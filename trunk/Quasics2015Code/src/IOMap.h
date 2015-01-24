6e99bc38aa1f2bde5aebf37c995ffc505d4c78fc
matt.j.healy@gmail.com <matt.j.healy@gmail.com@cb48be66-10d5-953f-2d12-17467ccc9a8c>
2015-01-24 19:16:21 +0000

    Added some comments, cleaned up some code formatting (as an example commit).

--- trunk/Quasics2015Code/src/IOMap.h
+++ trunk/Quasics2015Code/src/IOMap.h
@@ -8,19 +8,23 @@
 #ifndef SRC_IOMAP_H_
 #define SRC_IOMAP_H_
 
-#define FrontLeftTalonPort 1
-#define RearLeftTalonPort 2
+// Motor port definitions
+#define FrontLeftTalonPort  1
+#define RearLeftTalonPort   2
 #define FrontRightTalonPort 3
-#define RearRightTalonPort 4
+#define RearRightTalonPort  4
 
-#define LeftEncoderA 1
-#define LeftEncoderB 2
-#define RightEncoderA 3
-#define RightEncoderB 4
+// Encoder definitions
+#define LeftEncoderA        1
+#define LeftEncoderB        2
+#define RightEncoderA       3
+#define RightEncoderB       4
 
-#define GyroIn 1
+// Gyro input port(s)
+#define GyroIn              1
 
-#define GamePadIn 1
+// Driver controller port(s)
+#define GamePadIn           1
 
 
 #endif /* SRC_IOMAP_H_ */
--- trunk/Quasics2015Code/src/Includes.h
+++ trunk/Quasics2015Code/src/Includes.h
@@ -8,8 +8,13 @@
 #ifndef SRC_INCLUDES_H_
 #define SRC_INCLUDES_H_
 
+// System (standard) headers
 #include <math.h>
+
+// Libraries provided by FRC
 #include "WPILib.h"
+
+// Quasics-defined headers
 #include "IOMap.h"
 #include "Controller/Gamepad.h"
 #include "DriveTrain/DriveTrain.h"

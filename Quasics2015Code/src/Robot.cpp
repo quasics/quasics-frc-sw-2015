3e49303d7d7c48da030f2d10c538104f20a77df2
Yognaught2 <Yognaught2@gmail.com>
2015-01-27 22:12:17 +0000

    Fixed axis

--- Quasics2015Code/src/Robot.cpp
+++ Quasics2015Code/src/Robot.cpp
@@ -24,7 +24,7 @@ void Robot::TeleopInit() {
 }
 
 void Robot::TeleopPeriodic() {
-	driveBase.SetDrivePower(powerPad.GetRawAxis(0), -powerPad.GetRawAxis(3));
+	driveBase.SetDrivePower(powerPad.GetRawAxis(1), -powerPad.GetRawAxis(3));
 
 }
 

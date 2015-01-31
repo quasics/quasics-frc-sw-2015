de0a9b5ac7c037ba168e9e365425105b239039e8
joelman111@gmail.com <joelman111@gmail.com@cb48be66-10d5-953f-2d12-17467ccc9a8c>
2015-01-31 20:14:38 +0000

    Code for Grabber

---
+++ trunk/Quasics2015Code/src/Grabber/ChangeTimes.h
@@ -0,0 +1,17 @@
+/*
+ * ChangeTimes.h
+ *
+ *  Created on: Jan 31, 2015
+ *      Author: JOEL MILLER
+ */
+
+#ifndef SRC_ELEVATOR_CHANGETIMES_H_
+#define SRC_ELEVATOR_CHANGETIMES_H_
+
+const float NarrowToCanTime = 1;
+const float NarrowToWideTime = 1;
+const float CanToWideTime = 1;
+
+
+
+#endif /* SRC_ELEVATOR_CHANGETIMES_H_ */
--- trunk/Quasics2015Code/src/Grabber/Grabber.h
+++ trunk/Quasics2015Code/src/Grabber/Grabber.h
@@ -9,16 +9,31 @@
 #define SRC_GRABBER_GRABBER_H_
 
 #include "WPILib.h"
+#include "ChangeTimes.h"
 
 class Grabber {
 public:
+	enum Status {
+			kWide, kNarrow, kCan
+		};
 	//Constructor
 	Grabber (int screwMotorPort);
 
 	//Commands
+	void ChangeSizeStart (Status TargetSize);
+	void ChangeSizeProcess ();
+
+	bool IsMoving ();
+	bool Wide ();
+	bool Narrow ();
+	bool Can ();
 private:
 	Relay screwMotor;
+	Timer timer;
 
+	bool moving;
+	float targetDuration;
+	Status currentStatus;
 };
 
 #endif /* SRC_GRABBER_GRABBER_H_ */

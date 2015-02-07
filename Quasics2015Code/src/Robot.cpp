c37c85f7a032a8eeab52b8dd136d17116b98135e
QuasicsProgramming <QuasicsProgramming@gmail.com>
2015-02-07 21:40:52 +0000

    Relays co to RELAY PORTS

--- Quasics2015Code/src/Robot.cpp
+++ Quasics2015Code/src/Robot.cpp
@@ -4,28 +4,26 @@ GLaDOS::GLaDOS() :
 		driveBase(FrontLeftTalonPort, FrontRightTalonPort, RearLeftTalonPort,
 				RearRightTalonPort, LeftEncoderA, LeftEncoderB, RightEncoderA,
 				RightEncoderB, GyroIn), powerPad(GamePadIn, 0.05), logicPad(
-				GamePad2In, 0.05), camera(CameraHost), elevator(
-				LeftElevatorMotorPort, RightElevatorMotorPort), PissPoorDriveOn(
-				false), LogicSwitchButtonPrevious(false), PS4SwitchButtonPrevious(
-				false) {
+				GamePad2In, 0.05), PissPoorDriveOn(false), LogicSwitchButtonPrevious(
+				false), PS4SwitchButtonPrevious(false) {
 
 }
 
 void GLaDOS::RobotInit() {
 	printf("Loading GLaDOS: 0/100\n");
-	Wait (.1);
+	Wait(.1);
 	printf("Loading GLaDOS: 10/100\n");
-	Wait (.1);
+	Wait(.1);
 	printf("Loading GLaDOS: 23/100\n");
-	Wait (.1);
+	Wait(.1);
 	printf("Loading GLaDOS: 42/100\n");
-	Wait (.1);
+	Wait(.1);
 	printf("Loading GLaDOS: 50/100\n");
-	Wait (.1);
+	Wait(.1);
 	printf("Loading GLaDOS: 84/100\n");
-	Wait (.1);
+	Wait(.1);
 	printf("Loading GLaDOS: 100/100\n");
-	Wait (.1);
+	Wait(.1);
 	printf("GLaDOS Initialized\n");
 	printf("--------------------------\n");
 	printf("Morality Core: ");

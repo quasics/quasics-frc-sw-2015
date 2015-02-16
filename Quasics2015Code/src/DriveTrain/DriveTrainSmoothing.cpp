ca0288223a72fe38c96d9383ebae99eee8dea063
Yognaught2 <Yognaught2@gmail.com>
2015-02-16 18:54:24 +0000

    Linear Interpolation fix and comments

--- Quasics2015Code/src/DriveTrain/DriveTrainSmoothing.cpp
+++ Quasics2015Code/src/DriveTrain/DriveTrainSmoothing.cpp
@@ -1,129 +1,117 @@
 #include "DriveTrainSmoothing.h"
 #include <algorithm>
-
 using namespace std;
 
-/// "Power multipliers", used to convert a pair of values used to convert a
-/// joystick reading into the (desired) output power setting for the
-/// corresponding motor.
-struct JoystickToPowerMultipliers {
-  float leftMultiplier;
-  float rightMultiplier;
+//Data Pertaining to multiplier Table Start
+
+// Data type for multiplier table
+struct MultiplierDataType {
+	float leftMultiplier;
+	float rightMultiplier;
 };
-
-const float joystickStep = 0.5;
-
-/// Table of "power multipliers" for use by the TranslateValues() function.
-/// The values are spaced out by "joystickStep" increments on the joystick
-/// readings, so that if they are spaced by 0.05, then the first entry
-/// represents the multipliers to be used for joystick reading of -1.00, the
-/// second for readings of -0.95, etc., on up to readings for +1.00.
-const JoystickToPowerMultipliers joystickToPowerMultipliers[] = {
-  { 1, .97 },   // for joystick readings of -1.00
-  { .99, 1 },   // for joystick readings of -0.95
-  { .96, 1 },   // for joystick readings of -0.90
-  { 1, .96 },   // for joystick readings of -0.85
-  // More values go here....
-  { 1, .96 }    // for joystick readings of +1.00
+// Interval of data table
+const float joystickStep = 0.05;
+// Power Multiplier table. IMPORTANT: Keep scale consistent and update joysticStep as well
+const MultiplierDataType multiplierData[] = { { 1, .97 }, // for joystick readings of -1.00
+		{ .99, 1 },   // for joystick readings of -0.95
+		{ .96, 1 },   // for joystick readings of -0.90
+		{ 1, .96 },   // for joystick readings of -0.85
+		// More values go here....
+		{ 1, .96 }    // for joystick readings of +1.00
 };
+//Number of entries in multiplierData (Total Bytes / Bytes Per Entry)
+const int sizeOfMultiplierDataTable = sizeof(multiplierData)
+		/ sizeof(multiplierData[0]);
 
-const int sizeOfJoystickToPowerMultipliersTable =
-    sizeof(joystickToPowerMultipliers) / sizeof(joystickToPowerMultipliers[0]);
+//Data Pertaining To Data Table End
 
-float LinearInterpolation(
-    float x0, float y0,   // first point on curve (x is input, y is result)
-    float x1, float y1,   // second point on curve (ditto)
-    float x)              // input being evaluated for the function
-{
-  if ( x1 == x0 ) {
-    // This would be bad: either a single point is being used, or else we
-    // are going to wind up with infinite slope.  Either way, it's bad, so
-    // we will "chicken out" and just return y0.
-    return y0;
-  }
-  
-  return (y0 + (y1 - y0) * (x - x0) / (x1 - x0));
+/*Linear Interpolation Calculator
+ *	Point below is (x0, y0), point above is (x1, y1)
+ *	plots a point between them and evaluates that equation for x
+ */
+float LinearInterpolation(float x0, float y0, float x1, float y1, float x) {
+	//To prevent a NAN, return y0
+	if (x1 == x0) {
+		return y0;
+	}
+
+	//Actual equation
+	return (y0 + (y1 - y0) * (x - x0) / (x1 - x0));
 }
 
-/// Note that this funtion assumes that joystick readings go from -1 to +1, and
-/// that the corresponding entries in the table will be spaced at intervals
-/// of 0.05 (so that there are 41 readings in the table).
-///
-/// This function could be improved to perform linear interpolation to compute
-/// approximate values for joystick readings between points used for the
-/// table, so that the drive responds in a more analog fashion to changes on
-/// the joystick, rather than "jumping" from one point to another.  (See
-/// https://en.wikipedia.org/wiki/Linear_interpolation for a discussion of
-/// linear interpolation, including the formula and how to use it.)
-void TrimJoystickValuesToPower(bool useLinearScaling,
-                               float leftIn, float rightIn,
-                               float& leftOut, float& rightOut) {
-  // "Sanitize" our inputs, making sure that they are always between -1.0 and
-  // +1.0, inclusive.
-  leftIn = min(max(leftIn, -1.0f), +1.0f);
-  rightIn = min(max(rightIn, -1.0f), +1.0f);
-  
-  const int indexOffset =
-  sizeOfJoystickToPowerMultipliersTable / 2;  // e.g., 41 / 2 ==> 20,
-  // since they are integers
-  
-  // Scaling the values to an integer multiple of (roughly) half our table
-  // size, so that we can convert them into an index into the table in the
-  // next step.  (For example, -1 ==> -20, +1 ==> +20, etc.)
-  //
-  // Note that this will always round down (e.g. .99 * 20 ==> 19.8 when scaled,
-  // but we will round that down to 19, which gives us safe values for use in
-  // figuring out the index).
-  const int leftScaled = int(leftIn * indexOffset);
-  const int rightScaled = int(rightIn * indexOffset);
-  
-  // OK, take the scaled size and do the conversion.
-  const int leftIndex = leftScaled + indexOffset;
-  const int rightIndex = rightScaled + indexOffset;
-  
-  float leftMultiplier = joystickToPowerMultipliers[leftIndex].leftMultiplier;
-  float rightMultiplier = joystickToPowerMultipliers[rightIndex].rightMultiplier;
-  
-  if ( useLinearScaling ) {
-    if ( leftIndex == sizeOfJoystickToPowerMultipliersTable - 1 ) {
-      // Last value in the table: can't plot any further out, so we'll stick
-      // with the value pulled when we initialized the leftMultiplier.
-    } else {
-      const float x0 = leftScaled;
-      const float y0 = joystickToPowerMultipliers[leftIndex].leftMultiplier;
-      const float x1 = leftScaled + indexOffset;
-      const float y1 = joystickToPowerMultipliers[leftIndex+1].leftMultiplier;
-      const float x = leftIn * indexOffset;
-      
-      leftMultiplier = LinearInterpolation(x0, y0, x1, y1, x);
-    }
+/* Base function for trim calculation
+ * Can either use stepped scaling or interpolated scaling, depending on the bool
+ */
+void TrimJoystickValuesToPower(bool useLinearScaling, float leftIn,
+		float rightIn, float& leftOut, float& rightOut) {
+	// bring inputs to between -1 and 1
+	leftIn = min(max(leftIn, -1.0f), +1.0f);
+	rightIn = min(max(rightIn, -1.0f), +1.0f);
 
-    if ( rightIndex == sizeOfJoystickToPowerMultipliersTable - 1 ) {
-      // Last value in the table: can't plot any further out, so we'll stick
-      // with the value pulled when we initialized the rightMultiplier.
-    } else {
-      const float x0 = rightScaled;
-      const float y0 = joystickToPowerMultipliers[rightIndex].rightMultiplier;
-      const float x1 = rightScaled + indexOffset;
-      const float y1 = joystickToPowerMultipliers[rightIndex+1].rightMultiplier;
-      const float x = rightIn * indexOffset;
-      
-      rightMultiplier = LinearInterpolation(x0, y0, x1, y1, x);
-    }
-  }
+	// Conversion from input to table
+	const int indexOffset = sizeOfMultiplierDataTable / 2; // e.g., 41 / 2 ==> 20,
+														   //since they are integers
 
-  leftOut = leftMultiplier * leftIn;
-  rightOut = rightMultiplier * rightIn;
+	/* Scaling the values to an integer multiple of (roughly) half our table
+	 size, so that we can convert them into an index into the table in the
+	 next step.  (For example, -1 ==> -20, +1 ==> +20, etc.)
+
+	 Note that this will always round down (e.g. .99 * 20 ==> 19.8 when scaled,
+	 but we will round that down to 19, which gives us safe values for use in
+	 figuring out the index).
+	 */
+	const int leftScaled = int(leftIn * indexOffset);
+	const int rightScaled = int(rightIn * indexOffset);
+
+	// OK, take the scaled size and do the conversion.
+	const int leftIndex = leftScaled + indexOffset;
+	const int rightIndex = rightScaled + indexOffset;
+
+	//Set default multipliers (final multipliers if not using interpolation)
+	float leftMultiplier = multiplierData[leftIndex].leftMultiplier;
+	float rightMultiplier = multiplierData[rightIndex].rightMultiplier;
+
+	if (useLinearScaling) {
+		if (leftIndex == sizeOfMultiplierDataTable - 1) {
+			//Since we can't pull a greater number, just return the default for max
+		} else {
+			// Define variables for interpolation
+			const float x0 = leftScaled;	// previously defined floor
+			const float y0 = multiplierData[leftIndex].leftMultiplier; //get Y value for x0
+			const float x1 = leftScaled + joystickStep; //grab next value from data table
+			const float y1 = multiplierData[leftIndex + 1].leftMultiplier; //evaluate for x1
+			const float x = leftIn * indexOffset; //non-integer left scaled value
+
+			leftMultiplier = LinearInterpolation(x0, y0, x1, y1, x); //run equation
+		}
+
+		if (rightIndex == sizeOfMultiplierDataTable - 1) {
+			//Since we can't pull a greater number, just return the default for max
+		} else {
+			// Define variables for interpolation
+			const float x0 = rightScaled;	// previously defined floor
+			const float y0 = multiplierData[rightIndex].rightMultiplier; //get Y value for x0
+			const float x1 = rightScaled + joystickStep; //grab next value from data table
+			const float y1 = multiplierData[rightIndex + 1].rightMultiplier; //evaluate for x1
+			const float x = rightIn * indexOffset; //non-integer left scaled value
+
+			rightMultiplier = LinearInterpolation(x0, y0, x1, y1, x); //run equation
+		}
+	}
+
+	//output trimmed values
+	leftOut = leftMultiplier * leftIn;
+	rightOut = rightMultiplier * rightIn;
 }
 
-void TrimJoystickValuesToPowerWithoutLinearScaling(
-    float leftIn, float rightIn,
-    float& leftOut, float& rightOut) {
-  return TrimJoystickValuesToPower(false, leftIn, rightIn, leftOut, rightOut);
+//run stepped scaling
+void TrimJoystickValuesToPowerWithoutLinearScaling(float leftIn, float rightIn,
+		float& leftOut, float& rightOut) {
+	return TrimJoystickValuesToPower(false, leftIn, rightIn, leftOut, rightOut);
 }
 
-void TrimJoystickValuesToPowerWithLinearScaling(
-    float leftIn, float rightIn,
-    float& leftOut, float& rightOut) {
-  return TrimJoystickValuesToPower(true, leftIn, rightIn, leftOut, rightOut);
+//run interpolated scaling
+void TrimJoystickValuesToPowerWithLinearScaling(float leftIn, float rightIn,
+		float& leftOut, float& rightOut) {
+	return TrimJoystickValuesToPower(true, leftIn, rightIn, leftOut, rightOut);
 }

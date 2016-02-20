87bac2c4f16425401d2991282b62c7f8e9e4c7b5
mjhealy <mjhealy@users.noreply.github.com>
2016-02-20 08:45:43 -0800

    Fixing a bug where the autonomous command owned by the selector was being handed over to the Robot to stuff into a unique_ptr, giving rise to a dangling pointer (memory corruption) issue.

---
+++ Quasics2016Code/SimpleRobot/src/Commands/CloneableCommand.h
@@ -0,0 +1,21 @@
+/*
+ * CloneableCommand.h
+ *
+ *  Created on: Feb 20, 2016
+ *      Author: healym
+ */
+
+#ifndef SRC_COMMANDS_CLONEABLECOMMAND_H_
+#define SRC_COMMANDS_CLONEABLECOMMAND_H_
+
+#include <Commands/Command.h>
+
+class CloneableCommand: public Command {
+public:
+	CloneableCommand() {}
+	virtual ~CloneableCommand() {}
+
+	virtual std::unique_ptr<Command> clone() const = 0;
+};
+
+#endif /* SRC_COMMANDS_CLONEABLECOMMAND_H_ */

8f72b53dba1b1c3c3699903facda5672966c11cd
mjhealy <mjhealy@users.noreply.github.com>
2016-02-20 11:03:10 -0800

    Misc. cleanup

--- Quasics2016Code/Arduino/Samples/SerialConsole/SerialConsole.ino
+++ Quasics2016Code/Arduino/Samples/SerialConsole/SerialConsole.ino
@@ -1,7 +1,7 @@
 /*
-  Multple Serial test
+ "Console" application for a device connected to Serial1
 
- Receives from the main serial port, sends to the others.
+ Receives from the main serial port, sends to Serial1.
  Receives from serial port 1, sends to the main serial (Serial 0).
 
  This example works only with boards with more than one serial like Arduino Mega, Due, Zero etc
@@ -9,15 +9,6 @@
  The circuit:
  * Any serial device attached to Serial port 1
  * Serial monitor open on Serial port 0:
-
- created 30 Dec. 2008
- modified 20 May 2012
- by Tom Igoe & Jed Roach
- modified 27 Nov 2015
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
  */
 
 
@@ -25,18 +16,18 @@ void setup() {
   // initialize both serial ports:
   Serial.begin(115200);
   Serial1.begin(115200);
-  Serial.println("MultiSerial now running....");
+  Serial.println("SerialConsole now running....");
 }
 
 void loop() {
   // read from port 1, send to port 0:
-  if (Serial1.available()) {
+  while (Serial1.available()) {
     int inByte = Serial1.read();
     Serial.write(inByte);
   }
 
   // read from port 0, send to port 1:
-  if (Serial.available()) {
+  while (Serial.available()) {
     int inByte = Serial.read();
     Serial1.write(inByte);
   }

7c9513f8f16fc807abe13f25d7884bb0a823958a
R-Man213 <Armanf2000@gmail.com>
2017-02-04 12:43:39 -0500

    linear actuator command is done

---
+++ Quasics2017Code/Ralph/src/Commands/ActuatorAuto.cpp
@@ -0,0 +1,31 @@
+#include "ActuatorAuto.h"
+#include "../Robot.h"
+
+ActuatorAuto::ActuatorAuto(bool doorOpen) {
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(Robot::chassis.get());
+	Requires(Robot::fuelexhaustgate.get());
+	openDoor = doorOpen;
+}
+
+// Called just before this Command runs the first time
+void ActuatorAuto::Initialize() {
+	Robot::fuelexhaustgate->Set(openDoor);
+}
+
+
+// Make this return true when this Command no longer needs to run execute()
+bool ActuatorAuto::IsFinished() {
+	return true;
+}
+
+// Called once after isFinished returns true
+void ActuatorAuto::End() {
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void ActuatorAuto::Interrupted() {
+
+}
---
+++ Quasics2017Code/Ralph/src/Commands/ActuatorAuto.h
@@ -0,0 +1,19 @@
+#ifndef ActuatorAuto_H
+#define ActuatorAuto_H
+
+#include "WPILib.h"
+#include "../Subsystems/FuelExhaustGate.h"
+
+class ActuatorAuto : public Command {
+public:
+	ActuatorAuto(bool doorOpen);
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+private:
+	bool openDoor;
+};
+
+#endif  // ActuatorAuto_H

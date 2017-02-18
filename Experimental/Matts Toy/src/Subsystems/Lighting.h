4737172be63d9e02f11fbf4d41bf68a559683e0b
mjhealy <mjhealy@users.noreply.github.com>
2017-02-18 14:42:12 -0500

    Documentation

--- Experimental/Matts Toy/src/Subsystems/Lighting.h
+++ Experimental/Matts Toy/src/Subsystems/Lighting.h
@@ -13,6 +13,8 @@ public:
 	Lighting();
 	void InitDefaultCommand();
 
+	//
+	// Functions for manual configuration of lighting in Commands
 	enum Alliance {
 		eRed = 0, eBlue = 1, eDemo = 2
 	};
@@ -23,9 +25,20 @@ public:
 	};
 	void setMode(Mode mode);
 
+	//
+	// Functions for automatic configuration of lighting in Commands
+	// (e.g., in the AutomaticLightingCommand class).
 	void updateState(bool force = false);
 
 protected:
+	/**
+	 * Sends a command to the Arduino, indicating the lighting mode to
+	 * be used.
+	 *
+	 * @param force if false, the command will only be sent if we think that
+	 *              the lighting mode has changed from the last time that it
+	 *              was sent out.
+	 */
 	void sendLightingCommand(bool force);
 
 private:

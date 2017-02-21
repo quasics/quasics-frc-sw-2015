1c82d5897e776dbe8305bab72f6b1bb79761338f
mjhealy <mjhealy@users.noreply.github.com>
2017-02-21 17:36:07 -0500

    Code review comments, and fixing (path-related) compile-time issues.

--- Quasics2017Code/Ralph/src/Commands/AuxiliaryCommands.h
+++ Quasics2017Code/Ralph/src/Commands/AuxiliaryCommands.h
@@ -2,10 +2,14 @@
 #define AuxiliaryCommands_H
 
 #include <Commands/CommandGroup.h>
+// CODE_REVIEW(mjh): All of these should be moved into the .cpp file, since
+// the underlying types the define aren't used here.  (Including them here
+// will increase build time, since everyone that pulls in this header will
+// then also pull *them* in, even though they aren't used by client code.)
 #include "IntakeTeleop.h"
-#include <Commands/FuelExhaustTeleop.h>
-#include <Commands/GearTeleop.h>
-#include <Commands/OutputTele.h>
+#include "FuelExhaustTeleop.h"
+#include "GearTeleop.h"
+#include "OutputTele.h"
 
 class AuxiliaryCommands : public CommandGroup {
 public:

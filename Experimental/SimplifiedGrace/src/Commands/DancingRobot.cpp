b33f7bbfc8d67ecab99dfa73e043d49cbb05b1e6
ScottHealy <32375761+ScottHealy@users.noreply.github.com>
2017-12-02 15:33:15 -0500

    Made DancingRobot make the robot dance again.

--- Experimental/SimplifiedGrace/src/Commands/DancingRobot.cpp
+++ Experimental/SimplifiedGrace/src/Commands/DancingRobot.cpp
@@ -1,6 +1,55 @@
 #include "TurnRightInput.h"
 #include "DancingRobot.h"
+#include "TurnLeftInput.h"
+#include "MoveForward.h"
 
 DancingRobot::DancingRobot() {
-	AddSequential(new TurnRightInput(90));
+	AddSequential(new TurnRightInput(20));
+	AddSequential(new TurnLeftInput(40));
+	AddSequential(new TurnRightInput(20));
+	AddSequential(new TurnLeftInput(40));
+	AddSequential(new TurnRightInput(80));
+	AddSequential(new TurnLeftInput(40));
+	AddSequential(new TurnRightInput(60));
+	AddSequential(new TurnLeftInput(120));
+	AddSequential(new TurnRightInput(60));
+	AddSequential(new TurnLeftInput(80));
+	AddSequential(new TurnRightInput(160));
+	AddSequential(new TurnLeftInput(80));
+	AddSequential(new TurnRightInput(100));
+	AddSequential(new TurnLeftInput(200));
+	AddSequential(new TurnRightInput(100));
+	AddSequential(new TurnLeftInput(120));
+	AddSequential(new TurnRightInput(240));
+	AddSequential(new TurnLeftInput(120));
+	AddSequential(new TurnRightInput(140));
+	AddSequential(new TurnLeftInput(280));
+	AddSequential(new TurnRightInput(140));
+	AddSequential(new TurnLeftInput(160));
+	AddSequential(new TurnRightInput(320));
+	AddSequential(new TurnLeftInput(160));
+	AddSequential(new TurnRightInput(180));
+	AddSequential(new TurnLeftInput(360));
+	AddSequential(new TurnRightInput(180));
+	AddSequential(new TurnLeftInput(200));
+	AddSequential(new TurnRightInput(400));
+	AddSequential(new TurnLeftInput(200));
+	AddSequential(new MoveForward(.25,2));
+	AddSequential(new TurnLeftInput(720));
+	AddSequential(new TurnRightInput(720));
+	AddSequential(new TurnLeftInput(200));
+	AddSequential(new MoveForward(-.25,2));
+	AddSequential(new TurnRightInput(720));
+	AddSequential(new TurnLeftInput(720));
+	AddSequential(new MoveForward(.25,1));
+	AddSequential(new MoveForward(-.25,1));
+	AddSequential(new TurnRightInput(180));
+	AddSequential(new TurnLeftInput(360));
+	AddSequential(new TurnRightInput(180));
+	AddSequential(new MoveForward(-.25,1));
+	AddSequential(new MoveForward(.25,1));
+	AddSequential(new TurnLeftInput(180));
+	AddSequential(new TurnRightInput(360));
+	AddSequential(new TurnLeftInput(180));
+	AddSequential(new TurnRightInput(1080));
 }

c5e796812a45226568e645ca185b75f0f804739f
mjhealy <mjhealy@users.noreply.github.com>
2018-02-02 22:25:18 -0500

    Switched away from using inline functions

--- Quasics2018Code/Hedy/src/Subsystems/Navigation.h
+++ Quasics2018Code/Hedy/src/Subsystems/Navigation.h
@@ -1,16 +1,8 @@
-/*
- * Navigation.h
- *
- *  Created on: Nov 4, 2017
- *      Author: Developer
- */
-
 #ifndef SRC_SUBSYSTEMS_NAVIGATION_H_
 #define SRC_SUBSYSTEMS_NAVIGATION_H_
 
 #include <Commands/Subsystem.h>
 #include "../ThirdParty/NavX/include/AHRS.h"
-#include <iostream>
 
 class Navigation: public frc::Subsystem {
 ////////////////////////////////////////////////////////////////////////////////
@@ -27,11 +19,7 @@ public:
 	/**
 	 * @return true iff the sensor is available for use
 	 */
-	bool isReady() {
-		return (ahrs.get() != nullptr)
-				&& ahrs->IsConnected()
-				&& !ahrs->IsCalibrating();
-	}
+	bool isReady();
 
 	/**
 	 * Resets the base value for the bearing, so that future queries will
@@ -39,35 +27,16 @@ public:
 	 *
 	 * @see getBearing()
 	 */
-	void resetBearing() {
-		if (ahrs.get() != nullptr) {
-			ahrs->ZeroYaw();
-		}
-	}
+	void resetBearing();
 
 	/**
 	 * @return a value in the range of -180 to +180; if the sensor is
 	 *         unavailable, a value of 0 will be returned.
 	 * @see resetBearing()
 	 */
-	float getBearing() {
-		if (isReady()) {
-			return ahrs->GetYaw();
-		} else {
-			std::cerr << "*** Warning: NavX not ready ("  << __PRETTY_FUNCTION__ << "....\n";
-			return 0;
-		}
-	}
+	float getBearing();
 
-	float getCompassHeading() {
-		if (isReady()) {
-			return ahrs->GetCompassHeading();
-		}
-		else {
-			std::cerr << "*** Warning: NavX not ready ("  << __PRETTY_FUNCTION__ << "....\n";
-			return 0;
-		}
-	}
+	float getCompassHeading();
 
 	/**
 	 * Returns the cumulative angle that the robot has passed through.
@@ -79,15 +48,7 @@ public:
      * first 2 circles canceling out the ones to the right, and then
      * -360 for the 3rd circle).
 	 */
-	float getAngle() {
-		if (isReady()) {
-			return ahrs->GetAngle();
-		}
-		else {
-			std::cerr << "*** Warning: NavX not ready ("  << __PRETTY_FUNCTION__ << "....\n";
-			return 0;
-		}
-	}
+	float getAngle();
 
 };
 

aa3a9a9207944a0694459496a057b84b23dd7d6e
mjhealy <mjhealy@users.noreply.github.com>
2018-03-17 17:39:40 -0400

    Minor tweak

--- Quasics2018Code/Hedy/src/Commands/LightingTestCommand.cpp
+++ Quasics2018Code/Hedy/src/Commands/LightingTestCommand.cpp
@@ -16,7 +16,7 @@ LightingTestCommand::LightingTestCommand(): frc::Command() {
 
 // Called just before this Command runs the first time
 void LightingTestCommand::Initialize() {
-	Robot::lighting->WriteOff();
+	Robot::lighting->SendTestString();
 }
 
 // Make this return true when this Command no longer needs to run execute()

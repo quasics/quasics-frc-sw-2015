9460f282aa4c68bde75e090f26ad45b3ac05c448
quasics <quasics@boojumhunt.com>
2018-03-24 12:05:40 -0400

    Refactored code to allow robot to move different distances to cross the line from middle or side positions. Disabled "go for the switch" options for now (since vision code isn't working).

---
+++ Quasics2018Code/Hedy/src/Commands/AutoModeCrossTheLineCommand.h
@@ -0,0 +1,20 @@
+/*
+ * AutoModeCrossTheLineCommand.h
+ *
+ *  Created on: Mar 24, 2018
+ *      Author: Developer
+ */
+
+#ifndef SRC_COMMANDS_AUTOMODECROSSTHELINECOMMAND_H_
+#define SRC_COMMANDS_AUTOMODECROSSTHELINECOMMAND_H_
+
+#include <Commands/ConditionalCommand.h>
+
+class AutoModeCrossTheLineCommand: public frc::ConditionalCommand {
+public:
+	AutoModeCrossTheLineCommand();
+
+	bool Condition() override;
+};
+
+#endif /* SRC_COMMANDS_AUTOMODECROSSTHELINECOMMAND_H_ */

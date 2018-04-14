de82e7f5c047d9fc1514dd3e055a186cec0af20e
mjhealy <mjhealy@users.noreply.github.com>
2018-04-14 15:04:13 -0400

    Fixed some bugs in GyroTurn; still not running when tested

--- Quasics2018Code/Hedy/src/Commands/GyroTurn.cpp
+++ Quasics2018Code/Hedy/src/Commands/GyroTurn.cpp
@@ -1,15 +1,34 @@
-#include <Commands/GyroTurn.h>
+#include "GyroTurn.h"
 #include "../Robot.h"
+
+#include <iostream>
+#include <iomanip>
+
+double normalizeAngle(double angleDegrees) {
+	double result = 0;
+	if (angleDegrees >= 0 && angleDegrees < 360) {
+		result = angleDegrees;
+	} else if (angleDegrees < 0) {
+		// Angle is negative (and for now, we'll only turn to the left), so
+		// convert it.
+		result = angleDegrees - (((int(angleDegrees) / 360) - 1) * 360);
+	} else {
+		// Angle is too big: reduce it
+		result = angleDegrees - ((int(angleDegrees) / 360) * 360);
+	}
+	return result;
+}
 
 GyroTurn::GyroTurn(float angle, double power): frc::Command() {
 	Requires (Robot::gyroADXRS.get());
 
-	m_angle = angle;
+	m_angle = normalizeAngle(angle);
 	m_power = power;
 }
 
 // Called just before this Command runs the first time
 void GyroTurn::Initialize() {
+	std::cerr << "Initializing GyroTurn(" << m_angle << ", " << m_power << ")\n";
 	Robot::gyroADXRS->Reset();
 	Robot::driveBase->SetPowerToMotors(m_power, m_power);
 }
@@ -21,17 +40,15 @@ void GyroTurn::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool GyroTurn::IsFinished() {
-	if (Robot::gyroADXRS->GetAngle() >= m_angle){
+	const double currentAngle = Robot::gyroADXRS->GetAngle();
+	std::cerr << "Target angle: " << std::setw(6) << m_angle
+			  << "\tCurrent angle: " << std::setw(6) << currentAngle
+			  << std::endl;
+
+	if (currentAngle >= m_angle){
 		return true;
 	}
-	else if(Robot::gyroADXRS->GetAngle() < 0){
-		m_angle = (int(Robot::gyroADXRS->GetAngle()) / 360 + 1) * 360 + Robot::gyroADXRS->GetAngle();
-
-	}
-	else{
-		m_angle = Robot::gyroADXRS->GetAngle() - ((int(Robot::gyroADXRS->GetAngle()) / 360) * 360);
-	}
-
+	return false;
 }
 
 // Called once after isFinished returns true

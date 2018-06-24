0360fbf2aae5d320f28c38c4a47b690054776138
mjhealy <mjhealy@users.noreply.github.com>
2018-06-24 13:06:08 -0400

    More cleanup

--- Arduino/Control Code/4oj_lights/4oj_lights.ino
+++ Arduino/Control Code/4oj_lights/4oj_lights.ino
@@ -10,6 +10,12 @@ const int NEOPIXEL_PIN = 6;           ///< Digital pin used to communicate with 
 const int PIXEL_STRIP_LENGTH = 14;    ///< Length of the NeoPixel strip.
 const int BUTTON_PIN = 2;             ///< Pin connected to the button to cycle through lighting modes.
 
+const uint32_t WHITE  = Adafruit_NeoPixel::Color(255, 255, 255, 0);
+const uint32_t BLACK  = Adafruit_NeoPixel::Color(0, 0, 0, 0);
+const uint32_t RED    = Adafruit_NeoPixel::Color(255, 0, 0, 0);
+const uint32_t BLUE   = Adafruit_NeoPixel::Color(0, 0, 255, 0);
+const uint32_t QUASICS_GREEN = Adafruit_NeoPixel::Color(40, 255, 0, 0);
+
 /// NeoPixel strip control.
 Adafruit_NeoPixel strip(PIXEL_STRIP_LENGTH, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -37,25 +43,30 @@ void loop() {
     Serial.println();
   }
 
-  if (counter % 4 == 0){
-    colorWipe(strip.Color(0, 0, 255), 17);
-    colorWipe(strip.Color(0, 0, 0), 17);
-  }
-  else if (counter % 4 == 1){
-    colorWipe(strip.Color(255, 0, 0), 17);
-    colorWipe(strip.Color(0, 0, 0), 17);
-  }
-  else if (counter % 4 == 2){
-    colorWipe(strip.Color(255, 255, 255), 17);
-    colorWipe(strip.Color(0, 0, 0), 17);
-  }
-  else if (counter % 4 == 3){
-    colorWipe(strip.Color(255, 0, 0), 17);
-    colorWipe(strip.Color(0, 0, 255), 17);
-    colorWipe(strip.Color(255, 255, 255), 17);
-    colorWipe(strip.Color(0, 0, 0), 17);
-  }
+  const int standardDelay = 17;
 
+  switch(counter) {
+    case 0:
+      simplePattern(BLUE, standardDelay);
+      break;
+    case 1:
+      simplePattern(RED, standardDelay);
+      break;
+    case 2:
+      simplePattern(WHITE, standardDelay);
+      break;
+    case 3:
+      // Cycling red, blue, and white (for 4th of July)
+      colorWipe(RED, standardDelay);
+      colorWipe(BLUE, standardDelay);
+      colorWipe(WHITE, standardDelay);
+      colorWipe(BLACK, standardDelay);
+      break;
+    default:
+      // If it's not a color being used, then reset back to 0.
+      counter = 0;
+      break;
+  }
 }
 
 void colorWipe(uint32_t c, uint8_t wait) {
@@ -64,5 +75,10 @@ void colorWipe(uint32_t c, uint8_t wait) {
     strip.show();
     delay(wait);
   }
+}
+
+void simplePattern(uint32_t color, uint8_t wait) {
+  colorWipe(color, wait);
+  colorWipe(BLACK, wait);
 }
 

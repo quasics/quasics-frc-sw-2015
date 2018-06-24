ab2f90306e25f0a8b51e0d953cd62c0eb6f2d62e
mjhealy <mjhealy@users.noreply.github.com>
2018-06-24 12:48:18 -0400

    Misc cleanup and documentation

--- Arduino/Control Code/4oj_lights/4oj_lights.ino
+++ Arduino/Control Code/4oj_lights/4oj_lights.ino
@@ -1,80 +1,20 @@
+#include "TransitionButton.h"
 #include <Adafruit_NeoPixel.h>
-
-// Based on https://www.arduino.cc/en/Tutorial/Debounce
-
-int counter = 0;
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
-#define PIN 6
+const int NEOPIXEL_PIN = 6;           ///< Digital pin used to communicate with the NeoPixel strip.
+const int PIXEL_STRIP_LENGTH = 14;    ///< Length of the NeoPixel strip.
+const int BUTTON_PIN = 2;             ///< Pin connected to the button to cycle through lighting modes.
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(14, PIN, NEO_GRB + NEO_KHZ800);
+/// NeoPixel strip control.
+Adafruit_NeoPixel strip(PIXEL_STRIP_LENGTH, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
-
-const int BUTTON_PIN = 2;
-const int LED_PIN = 13;      // the number of the LED pin
-#define INITIAL_LED_STATE   true
-
-// Defines a simple class that will monitor a push-button, and can be used to
-// decide when it's been pressed and released.  This is meant to allow the
-// button to be used to move through a sequence of some sort (e.g., from one
-// light pattern to another, to another, etc.).
-class TransitionButton {
-  private:
-    const int buttonPin;
-    const bool transitionOnPress;
-    const unsigned long debounceDelayMsec;
-
-    int lastButtonState = LOW;           // the previous (transient) reading from the input pin
-    int buttonState = LOW;               // the last (stable) reading from the input pin
-    unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-  
-  public:
-    /**
-     * @param buttonPin         the pin being monitored for button presses
-     * @param transitionOnPress if true, advance on button being pressed down; otherwise, on button release
-     * @param debounceDelayMsec delay used to make sure that the button is in a stable state
-     */
-    TransitionButton(int buttonPin, bool transitionOnPress = true, unsigned long debounceDelayMsec = 50)
-      : buttonPin(buttonPin), transitionOnPress(transitionOnPress), debounceDelayMsec(debounceDelayMsec)
-    {
-      pinMode(buttonPin, INPUT);
-    }
-
-    bool advancePosition() {
-      const unsigned long now = millis();
-      int reading = digitalRead(buttonPin);
-      
-      if (reading != lastButtonState) {
-        lastDebounceTime = now;
-        lastButtonState = reading;
-      }
-    
-      if ((now - lastDebounceTime) > debounceDelayMsec) {
-        // whatever the reading is at, it's been there for longer than the debounce
-        // delay, so take it as the actual current state.
-        if (reading != buttonState) {
-          buttonState = reading;
-    
-          // We'll assume that we transition states on button down.
-          return (buttonState == transitionOnPress);
-        }
-      }
-    
-      return false;
-    }
-};
-
-//////////////////////////////////////////////////////////////////////////////
-// The following sample program for the TransitionButton class uses it to move
-// between 2 simple states (LED on and LED off).
-
+/// Button to control stepping through the different lighting modes.
 TransitionButton button(BUTTON_PIN);
-
-
 
 void setup() {
   Serial.begin(115200);
@@ -83,26 +23,12 @@ void setup() {
   }
   Serial.print("Starting up!\n");
 
-  pinMode(LED_PIN, OUTPUT);
-
-  // set initial LED state
-  digitalWrite(LED_PIN, INITIAL_LED_STATE);
-
- strip.begin();
+  strip.begin();
   strip.show();  
 }
 
-void x_loop() {
-  static bool lit = INITIAL_LED_STATE;
-  if (button.advancePosition()) {
-    Serial.println("Advancing");
-    lit = !lit;
-    digitalWrite(LED_PIN, lit);
-  }
-}
-
 void loop() {
-  static bool lit = INITIAL_LED_STATE;
+  static int counter = 0;
   if (button.advancePosition()) {
     Serial.println("Advancing...");
     counter = (counter + 1) % 4;

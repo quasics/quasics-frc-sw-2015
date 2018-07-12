e431e762c1f54fbd31d873b5725159de137e0e86
quasics <quasics@boojumhunt.com>
2018-07-12 19:28:59 -0400

    Adjusted power to left base motors to compensate for problems. -Gavin

--- Quasics2017Code/Nike-2018/src/Commands/BaseCommands/Teleop/TankDrive.cpp
+++ Quasics2017Code/Nike-2018/src/Commands/BaseCommands/Teleop/TankDrive.cpp
@@ -33,7 +33,7 @@ void TankDrive::Execute() {
 	}
 
 	float leftStick = Robot::oi->getDriveStick()->GetRawAxis(LeftYAxis)
-			* multiplier;
+			* multiplier * 1.5;
 	float rightStick = Robot::oi->getDriveStick()->GetRawAxis(RightYAxis)
 			* multiplier;
 

e2795f95bbcce5d1bec609bc9e1378bc142fbc53
mjhealy <mjhealy@users.noreply.github.com>
2019-02-08 09:14:47 -0500

    Formatting cleanup

--- Experimental/JavaSamples/Networking/BroadcastingClient.java
+++ Experimental/JavaSamples/Networking/BroadcastingClient.java
@@ -3,29 +3,30 @@ import java.net.*;
 import java.util.*;
 
 public class BroadcastingClient {
- 
+
     public static void main(String[] args) throws IOException {
         broadcast("on\000", 10900);
     }
 
     public static void broadcast(
-      String message, int port) throws IOException {
-	    broadcast(message, port, InetAddress.getByName("0.0.0.0"));
-	}
- 
+        String message, int port) throws IOException {
+        broadcast(message, port, InetAddress.getByName("0.0.0.0"));
+    }
+
     public static void broadcast(
-      String message, int port, InetAddress address) throws IOException {
+        String message, int port, InetAddress address) throws IOException {
         DatagramSocket socket = new DatagramSocket();
-		try {
+        try {
             socket.setBroadcast(true);
- 
+
             byte[] buffer = message.getBytes();
- 
-            DatagramPacket packet 
-              = new DatagramPacket(buffer, buffer.length, address, port);
+
+            DatagramPacket packet
+                = new DatagramPacket(buffer, buffer.length, address, port);
             socket.send(packet);
-		} finally {
-			socket.close();
-		}
+        }
+        finally {
+            socket.close();
+        }
     }
 }

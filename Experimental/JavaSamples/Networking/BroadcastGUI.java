9db2f9b78c81a7c37a20480a8de9ce98d0b61857
mjhealy <mjhealy@users.noreply.github.com>
2019-02-09 12:55:00 -0500

    Formatting and tweaks to network spec

--- Experimental/JavaSamples/Networking/BroadcastGUI.java
+++ Experimental/JavaSamples/Networking/BroadcastGUI.java
@@ -7,23 +7,20 @@ import javax.swing.*;
 
 public class BroadcastGUI {
 
-    final static String BROADCAST_ADDRESS = "0.0.0.0";
+    final static String BROADCAST_ADDRESS = "255.255.255.255";
     final static int BROADCAST_PORT = 10900;
 
     public static void broadcast(String message) throws IOException {
         DatagramSocket socket = new DatagramSocket();
         try {
-            InetAddress address = InetAddress.getByName("0.0.0.0");
+            InetAddress address = InetAddress.getByName(BROADCAST_ADDRESS);
             socket.setBroadcast(true);
 
             byte[] buffer = message.getBytes();
 
-            DatagramPacket packet = 
-				new DatagramPacket(
-					buffer, buffer.length, address, BROADCAST_PORT);
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, BROADCAST_PORT);
             socket.send(packet);
-        }
-        finally {
+        } finally {
             socket.close();
         }
     }
@@ -34,10 +31,16 @@ public class BroadcastGUI {
         //
         // Create our main frame
         //
-        final JFrame win = new JFrame( title ) {
+        final JFrame win = new JFrame(title) {
             private static final long serialVersionUID = 1;
-            public Dimension getMinimumSize() { return getPreferredSize(); }
-            public Dimension getMaximumSize() { return getPreferredSize(); }
+
+            public Dimension getMinimumSize() {
+                return getPreferredSize();
+            }
+
+            public Dimension getMaximumSize() {
+                return getPreferredSize();
+            }
         };
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container content = win.getContentPane();
@@ -46,22 +49,19 @@ public class BroadcastGUI {
         final JLabel label = new JLabel("Message:");
         final JTextField message = new JTextField(20);
         final JButton send = new JButton("Send");
-		final ActionListener action = event -> {
+        final ActionListener action = event -> {
             String text = message.getText();
             // Note: we'll tack a null character on at the end as a marker
             // to make it easier for any C/C++ code.
             text += '\000';
             try {
                 broadcast(text);
-            }
-            catch (Exception e) {
-                JOptionPane.showMessageDialog(
-                    win, e.getMessage(),
-                    "Error", JOptionPane.ERROR_MESSAGE);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(win, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             }
         };
-		send.addActionListener(action);
-		message.addActionListener(action);
+        send.addActionListener(action);
+        message.addActionListener(action);
 
         content.add(label, BorderLayout.WEST);
         content.add(message, BorderLayout.CENTER);
@@ -71,12 +71,10 @@ public class BroadcastGUI {
             public void run() {
                 win.pack();
 
-				// Make the window show up centered on the (primary) monitor.
-				Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-				win.setLocationRelativeTo(null);
-				win.setLocation(
-					dim.width/2-win.getSize().width/2, 
-					dim.height/2-win.getSize().height/2);
+                // Make the window show up centered on the (primary) monitor.
+                Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+                win.setLocationRelativeTo(null);
+                win.setLocation(dim.width / 2 - win.getSize().width / 2, dim.height / 2 - win.getSize().height / 2);
                 win.setVisible(true);
             }
         });

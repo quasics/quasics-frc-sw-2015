f8e418a553cad21306e92518aa7f1dc1b54ff2de
ScottHealy <32375761+ScottHealy@users.noreply.github.com>
2019-02-09 17:15:09 -0500

    Added some comments to make the code more readable.

--- Quasics2019Code/HedyLamarr/src/main/cpp/Commands/AdjustElevator.cpp
+++ Quasics2019Code/HedyLamarr/src/main/cpp/Commands/AdjustElevator.cpp
@@ -4,13 +4,14 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+//getting all the files it needs
 #include "Commands/AdjustElevator.h"
 #include "OI.h"
 #include "Robot.h"
 #include "Subsystems/Elevator.h"
 
 AdjustElevator::AdjustElevator() {
+  //gets the elevator subsystem
   Requires(Robot::elevator.get());
 }
 
@@ -20,28 +21,37 @@ void AdjustElevator::Initialize() {
 
 // Called repeatedly when this Command is scheduled to run
 void AdjustElevator::Execute() {
+  //tests whether the elevator is signaled to go up, and if it isn't at the top
   if (Robot::oi->isElevatorMoveUpSignaled() && !Robot::lifter->atTop()) {
+    //continues if both states are true
     Robot::elevator->moveSlowlyUp();
-  } else if (Robot::oi->isElevatorMoveDownSignaled() &&
-             !Robot::lifter->atBottom()) {
+  } 
+  //tests whether the elevator is signaled to go up, and if it isn't at the top
+  else if (Robot::oi->isElevatorMoveDownSignaled() && !Robot::lifter->atBottom()) {
+    //continues if both states are true
     Robot::elevator->moveSlowlyDown();
-  } else {
+  } 
+  else {
+    //if neither is true, it stops
     Robot::lifter->stop();
   }
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool AdjustElevator::IsFinished() {
+   //this command is default for the elevator and never stops, it is only interrupted
   return false;
 }
 
 // Called once after isFinished returns true
 void AdjustElevator::End() {
+  //putting in an end state to stop because I'm paranoid
   Robot::elevator->stop();
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void AdjustElevator::Interrupted() {
+  //when it's interrupted it needs to stop
   Robot::elevator->stop();
 }

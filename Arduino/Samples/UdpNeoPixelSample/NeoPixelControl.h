4fab846432c2e0594ff03708e592f84184615bf9
mjhealy <mjhealy@users.noreply.github.com>
2019-02-09 15:21:06 -0500

    Documentation updates

--- Arduino/Samples/UdpNeoPixelSample/NeoPixelControl.h
+++ Arduino/Samples/UdpNeoPixelSample/NeoPixelControl.h
@@ -3,14 +3,24 @@
 
 #include <Adafruit_NeoPixel.h>
 
+//////////////////////////////////////////////////////////////////////////////
+// Some customized NeoPixel type definitions, with self-descriptive names.
+
 constexpr neoPixelType NEOPIXEL_RING_RGBW = NEO_GRBW + NEO_KHZ800;
 constexpr neoPixelType NEOPIXEL_STRIP_RGB = NEO_GRB;
 constexpr neoPixelType NEOPIXEL_WS2811_RGB = NEO_RGB;
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Lighting modes supported by the controlling code.
 
 enum class NeoPixelMode {
   eOff,
   eOn
 };
+
+//////////////////////////////////////////////////////////////////////////////
+// Basic controlling functions for the lights.
 
 void initializeNeoPixels(int pin, int length, neoPixelType type);
 

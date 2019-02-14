636146bfc92d665dc1a1798f5f77a271daacfb51
mjhealy <mjhealy@users.noreply.github.com>
2019-02-14 18:22:07 -0500

    Added missing header.

---
+++ Quasics2019Code/HedyLamarr/src/main/include/ConfigurationFlags.h
@@ -0,0 +1,8 @@
+#ifndef CONFIGURATION_FLAGS_H
+#define CONFIGURATION_FLAGS_H
+
+// If DEFINED, don't enable default commands for the Elevator/Lifter subsystems.
+// This is intended for use during integration testing.
+#define DISABLE_ELEVATOR_DEFAULTS
+
+#endif  // CONFIGURATION_FLAGS_H

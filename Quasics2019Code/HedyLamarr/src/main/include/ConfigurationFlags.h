c7a9605a7900a05edf58b88cb8834b5207d49773
mjhealy <mjhealy@users.noreply.github.com>
2019-02-14 19:47:39 -0500

    Disabled limit switches (which aren't installed)

--- Quasics2019Code/HedyLamarr/src/main/include/ConfigurationFlags.h
+++ Quasics2019Code/HedyLamarr/src/main/include/ConfigurationFlags.h
@@ -2,7 +2,14 @@
 #define CONFIGURATION_FLAGS_H
 
 // If DEFINED, don't enable default commands for the Elevator/Lifter subsystems.
+//
 // This is intended for use during integration testing.
 #define DISABLE_ELEVATOR_DEFAULTS
+
+// If DEFINED, disable (ignore) the limit switches on the Elevator/Lifter
+// subsystems.
+//
+// This is intended for use when the limit switches aren't installed.
+#define DISABLE_ELEVATOR_LIMIT_SWITCHES
 
 #endif  // CONFIGURATION_FLAGS_H

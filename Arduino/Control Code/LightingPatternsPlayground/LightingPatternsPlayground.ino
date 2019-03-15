c8015a12dafbb8b241a035130f546a5be5c2dbc1
mjhealy <mjhealy@users.noreply.github.com>
2019-03-15 10:40:55 -0400

    Updated code to use an enum to consistently specify colors

--- Arduino/Control Code/LightingPatternsPlayground/LightingPatternsPlayground.ino
+++ Arduino/Control Code/LightingPatternsPlayground/LightingPatternsPlayground.ino
@@ -2,6 +2,7 @@
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
+#include "PatternColor.h"
 
 // Which pin on the Arduino is connected to the NeoPixels?
 // On a Trinket or Gemma we suggest changing this to 1
@@ -37,18 +38,54 @@ void setup() {
 }
 
 void loop() {
-  chase(RED);
-  chase(BLUE);
-  chase(GREEN);
-  pulse("RED");
-  pulse("BLUE");
-  pulse("GREEN");
-  blinking(RED);
-  blinking(BLUE);
-  blinking(GREEN);
+  chase(eRed);
+  chase(eBlue);
+  chase(eGreen);
+  
+  pulse(eRed);
+  pulse(eBlue);
+  pulse(eGreen);
+  
+  blinking(eRed);
+  blinking(eBlue);
+  blinking(eGreen);
 }
 
-void chase(uint32_t c) {
+uint32_t getColorValue(PatternColor patternColor) {
+  if (patternColor == eRed)
+    return RED;
+  if (patternColor == eGreen)
+    return GREEN;
+  if (patternColor == eBlue)
+    return BLUE;
+  if (patternColor == eYellow)
+    return YELLOW;
+
+  // This shouldn't ever happen, but if it does, we'll be able to get some warning.
+  Serial.print("Unrecognized PatternColor: ");
+  Serial.print(int(patternColor));
+  Serial.println(" --- Falling back on YELLOW.");
+  return YELLOW;
+}
+
+uint32_t getColorValueUsingIntensity(PatternColor patternColor, int intensity) {
+  if (patternColor == eGreen) {
+    return pixels.Color(0, intensity, 0);
+  }
+  else if (patternColor == eRed) {
+    return pixels.Color(intensity, 0, 0);
+  }
+  else if (patternColor == eBlue) {
+    return pixels.Color(0, 0, intensity);
+  }
+  else {
+    // Just assume it's yellow
+    return pixels.Color(intensity, intensity, 0);
+  }
+}
+
+void chase(PatternColor patternColor) {
+  const uint32_t c = getColorValue(patternColor);
   const int delayval = 30; // delay for 3/100ths of a second
 
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
@@ -65,28 +102,12 @@ void chase(uint32_t c) {
   }
 }
 
-uint32_t getColorFromNameAndIntensity(String colorName, int intensity) {
-    if (colorName == "GREEN") {
-      return pixels.Color(0, intensity, 0);
-    }
-    else if (colorName == "RED") {
-      return pixels.Color(intensity, 0, 0);
-    }
-    else if (colorName == "BLUE") {
-      return pixels.Color(0, 0, intensity);
-    }
-    else {
-      // Yellow
-      return pixels.Color(intensity, intensity, 0);
-    }
-}
-
-void pulse(String colorName) {
+void pulse(PatternColor patternColor) {
   const int delayval = 5; // delay for 0.005 seconds
 
   // Ramp the lights up
   for (int b = 0; b <= 150; b = b + 10) {
-    const uint32_t newColor = getColorFromNameAndIntensity(colorName, b);
+    const uint32_t newColor = getColorValueUsingIntensity(patternColor, b);
     for (int i = 0; i < NUMPIXELS; i++) {
       pixels.setPixelColor(i, newColor);
       pixels.show();
@@ -96,7 +117,7 @@ void pulse(String colorName) {
 
   // Ramp them back down
   for (int b = 150; b >= 0; b = b - 10) {
-    const uint32_t newColor = getColorFromNameAndIntensity(colorName, b);
+    const uint32_t newColor = getColorValueUsingIntensity(patternColor, b);
     for (int i = 0; i < NUMPIXELS; i++) {
       pixels.setPixelColor(i, newColor);
       pixels.show();
@@ -105,7 +126,8 @@ void pulse(String colorName) {
   }
 }
 
-void blinking(uint32_t c) {
+void blinking(PatternColor patternColor) {
+  const uint32_t c = getColorValue(patternColor);
   const int delayval = 400;   // Delay time in milliseconds (4/10ths sec)
 
   // Turn them all on
---
+++ Arduino/Control Code/LightingPatternsPlayground/PatternColor.h
@@ -0,0 +1,12 @@
+#ifndef PATTERN_COLOR_H
+#define PATTERN_COLOR_H
+
+// Due to a problem with how the Arduino IDE processes code, enumerated types need to be
+// defined in a header.
+
+enum PatternColor {
+  eRed, eBlue, eGreen, eYellow
+};
+
+#endif  // PATTERN_COLOR_H
+

2a0ffc03b4db9071f4133909e2847d796b0a65c6
ScottHealy <32375761+ScottHealy@users.noreply.github.com>
2019-08-15 18:51:02 -0400

    Added NewElevatorToBottom command.

---
+++ Quasics2019Code/HedyLamarr/src/main/cpp/Commands/NewElevatorToBottom.cpp
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Commands/NewElevatorToBottom.h"
+#include "Robot.h"
+#include "Subsystems/NewElevator.h"
+
+NewElevatorToBottom::NewElevatorToBottom() {
+  // Use Requires() here to declare subsystem dependencies
+  Requires(Robot::newElevator.get());
+}
+
+// Called just before this Command runs the first time
+void NewElevatorToBottom::Initialize() {
+  if(Robot::newElevator->atBottom()){
+    Robot::newElevator->stop();
+  }
+  else{
+    Robot::newElevator->move(-.6);
+  }
+}
+
+// Called repeatedly when this Command is scheduled to run
+void NewElevatorToBottom::Execute() {}
+
+// Make this return true when this Command no longer needs to run execute()
+bool NewElevatorToBottom::IsFinished() {
+  if(Robot::newElevator->atBottom()){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+// Called once after isFinished returns true
+void NewElevatorToBottom::End() {
+  Robot::newElevator->stop();
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void NewElevatorToBottom::Interrupted() {
+  End();
+}

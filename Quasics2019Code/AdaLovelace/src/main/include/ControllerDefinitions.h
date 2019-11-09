058686247866f05a6b8c8076004017754883c2b0
quasics-driver <47063590+quasics-driver@users.noreply.github.com>
2019-11-09 16:11:29 -0500

    Mapped the joysticks differently to be compatible with Logitech F310.

--- Quasics2019Code/AdaLovelace/src/main/include/ControllerDefinitions.h
+++ Quasics2019Code/AdaLovelace/src/main/include/ControllerDefinitions.h
@@ -11,10 +11,10 @@
 #define LogitechGamePad_BButton 3
 #define LogitechGamePad_XButton 4
 #define LogitechGamePad_YButton 1
-#define LogitechGamePad_LeftShoulder 5    
+#define LogitechGamePad_LeftShoulder 5
 #define LogitechGamePad_RightShoulder 6
-#define LogitechGamePad_LeftTrigger 7    
-#define LogitechGamePad_RightTrigger 8   
+#define LogitechGamePad_LeftTrigger 7
+#define LogitechGamePad_RightTrigger 8
 #define LogitechGamePad_LeftStickPress 11
 #define LogitechGamePad_RightStickPress 12
 #define LogitechGamePad_StartButton 9
@@ -22,28 +22,28 @@
 
 // Axes - Used with the "getRawAxis()" function to access the data for the
 // individual sticks on the controller (e.g., for "tank drive" coding).
-#define LogitechGamePad_LeftXAxis 0
-#define LogitechGamePad_LeftYAxis 1
-#define LogitechGamePad_RightXAxis 2
-#define LogitechGamePad_RightYAxis 3
+#define LogitechGamePad_LeftXAxis 2
+#define LogitechGamePad_LeftYAxis 5
+#define LogitechGamePad_RightXAxis 0
+#define LogitechGamePad_RightYAxis 1
 
 //--------------------X-Box
 //Controller-----------------------------------------------------------------
 // Buttons
-#define XBox_ButtonA 1  
-#define XBox_ButtonB 2  
-#define XBox_ButtonX 3  
-#define XBox_ButtonY 4      
-#define XBox_LeftButton 5   
-#define XBox_RightButton 6  
+#define XBox_ButtonA 1
+#define XBox_ButtonB 2
+#define XBox_ButtonX 3
+#define XBox_ButtonY 4
+#define XBox_LeftButton 5
+#define XBox_RightButton 6
 
 // Axes
 #define XBox_LeftXAxis 0
 #define XBox_LeftYAxis 1
 #define XBox_RightXAxis 2
-#define XBox_RightYAxis 5  
+#define XBox_RightYAxis 5
 
-#define XBox_LeftTrigger 2   
-#define XBox_RightTrigger 3  
+#define XBox_LeftTrigger 2
+#define XBox_RightTrigger 3
 
 #endif /* SRC_CONTROLLERDEFINITIONS_H_ */

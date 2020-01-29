ca82756568b6ee528836bf630d448f18dfea64fa
KatHofmann <38544408+KatHofmann@users.noreply.github.com>
2020-01-28 20:30:36 -0500

    More drivebase stuff... Tank Drive command

---
+++ Quasics2020Code/Gladys/src/main/cpp/commands/TankDrive.cpp
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/TankDrive.h"
+
+TankDrive::TankDrive(Drivebase*drivebase, std::function<double()> right, std::function<double()> left)
+:drivebase(drivebase), right(right), left(left) { 
+  // Use addRequirements() here to declare subsystem dependencies.
+
+}
+
+// Called when the command is initially scheduled.
+void TankDrive::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void TankDrive::Execute() {
+  drivebase->SetPower(right(), left());
+}
+
+// Called once the command ends or is interrupted.
+void TankDrive::End(bool interrupted) {
+  drivebase->Stop();
+}
+
+// Returns true when the command should end.
+bool TankDrive::IsFinished() { return false; }
---
+++ Quasics2020Code/Gladys/src/main/include/commands/TankDrive.h
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+#include "subsystems/Drivebase.h"
+
+/**
+ * An example command.
+ *
+ * <p>Note that this extends CommandHelper, rather extending CommandBase
+ * directly; this is crucially important, or else the decorator functions in
+ * Command will *not* work!
+ */
+class TankDrive
+    : public frc2::CommandHelper<frc2::CommandBase, TankDrive> {
+ public:
+  TankDrive(Drivebase*drivebase, std::function<double()> right, std::function<double()> left);
+
+  void Initialize() override;
+
+  void Execute() override;
+
+  void End(bool interrupted) override;
+  
+  bool IsFinished() override;
+
+
+private:
+ Drivebase*drivebase;
+ std::function<double()> right;
+ std::function<double()> left;
+
+
+
+};

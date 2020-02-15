40329f051a9dd502be3dda11f52455a47ef6b5f9
quasics-driver <47063590+quasics-driver@users.noreply.github.com>
2020-02-14 21:12:06 -0500

    Fixed bugs in color detection (bad probability spec, etc.).

--- Quasics2020Code/Gladys/src/main/cpp/subsystems/CommandPanel.cpp
+++ Quasics2020Code/Gladys/src/main/cpp/subsystems/CommandPanel.cpp
@@ -11,6 +11,11 @@
 
 CommandPanel::CommandPanel() : motor(CANBusIds::VictorSpx::SpinMotor) {
   SetSubsystem("CommandPanel");
+
+  m_colorMatcher.AddColorMatch(CommandPanelConstants::kBlueTarget);
+  m_colorMatcher.AddColorMatch(CommandPanelConstants::kGreenTarget);
+  m_colorMatcher.AddColorMatch(CommandPanelConstants::kRedTarget);
+  m_colorMatcher.AddColorMatch(CommandPanelConstants::kYellowTarget);
 }
 
 
@@ -34,30 +39,25 @@ void CommandPanel::TurnWheelMotorOff() {
 CommandPanel::Color CommandPanel::getCurrentColor() {
   Color colorID = UNKNOWN;
 
-  m_colorMatcher.AddColorMatch(CommandPanelConstants::kBlueTarget);
-  m_colorMatcher.AddColorMatch(CommandPanelConstants::kGreenTarget);
-  m_colorMatcher.AddColorMatch(CommandPanelConstants::kRedTarget);
-  m_colorMatcher.AddColorMatch(CommandPanelConstants::kYellowTarget);
-
-  frc::Color detectedColor = m_colorSensor.GetColor();
+  const frc::Color detectedColor = m_colorSensor.GetColor();
 
   std::string colorString;
   double confidence = 0.0;
-  frc::Color matchedColor =
+  const frc::Color matchedColor =
       m_colorMatcher.MatchClosestColor(detectedColor, confidence);
 
-  // TODO(RJ): Turn the constant value for the confidence into a named constant.
+\  constexpr double kMinimumConfidence = 0.90;
 
-  if (matchedColor == CommandPanelConstants::kBlueTarget && confidence > 90) {
+  if (matchedColor == CommandPanelConstants::kBlueTarget && confidence > kMinimumConfidence) {
     colorID = BLUE;
   } else if (matchedColor == CommandPanelConstants::kRedTarget &&
-             confidence > 90) {
+             confidence > kMinimumConfidence) {
     colorID = RED;
   } else if (matchedColor == CommandPanelConstants::kGreenTarget &&
-             confidence > 90) {
+             confidence > kMinimumConfidence) {
     colorID = GREEN;
   } else if (matchedColor == CommandPanelConstants::kYellowTarget &&
-             confidence > 90) {
+             confidence > kMinimumConfidence) {
     colorID = YELLOW;
   } else {
     colorID = UNKNOWN;

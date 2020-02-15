266a6c1180014f4a1018e146f20b8844d95b82fb
Matt Healy <mjhealy@users.noreply.github.com>
2020-02-15 16:48:47 -0500

    Added copyright info.

--- Experimental/MattsToy2020/src/main/include/utils/ValueScaler.h
+++ Experimental/MattsToy2020/src/main/include/utils/ValueScaler.h
@@ -1,3 +1,12 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 Quasics Robotics and Matthew J. Healy                   */
+/* All Rights Reserved.                                                       */
+/*                                                                            */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the BSD license file in the root directory of the   */
+/* project repository.                                                        */
+/*----------------------------------------------------------------------------*/
+
 #pragma once
 
 #include <cmath>

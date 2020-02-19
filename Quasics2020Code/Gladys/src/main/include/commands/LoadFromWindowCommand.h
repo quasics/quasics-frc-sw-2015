ac96b726deb8203ad4d9de3e14aab8bde8ea7b10
nurfr <40575718+nurfr@users.noreply.github.com>
2020-02-18 20:34:50 -0500

    Added LoadFromWindowCommand

---
+++ Quasics2020Code/Gladys/src/main/include/commands/LoadFromWindowCommand.h
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+
+/**
+ * An example command.
+ *
+ * <p>Note that this extends CommandHelper, rather extending CommandBase
+ * directly; this is crucially important, or else the decorator functions in
+ * Command will *not* work!
+ */
+class LoadFromWindowCommand
+    : public frc2::CommandHelper<frc2::CommandBase, LoadFromWindowCommand> {
+ public:
+  LoadFromWindowCommand();
+
+  void Initialize() override;
+
+  void Execute() override;
+
+  void End(bool interrupted) override;
+
+  bool IsFinished() override;
+};

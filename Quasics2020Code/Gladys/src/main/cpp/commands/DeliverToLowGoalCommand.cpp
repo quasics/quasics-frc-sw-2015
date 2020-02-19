61285acd601656b2225534f82f55d1ff6c0a2a87
nurfr <40575718+nurfr@users.noreply.github.com>
2020-02-18 20:54:37 -0500

    Added DeliverToLowGoalCommand

---
+++ Quasics2020Code/Gladys/src/main/cpp/commands/DeliverToLowGoalCommand.cpp
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/DeliverToLowGoalCommand.h"
+
+DeliverToLowGoalCommand::DeliverToLowGoalCommand(Exhaust*exhaust):exhaust(exhaust) {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void DeliverToLowGoalCommand::Initialize() {
+  exhaust->PushBallDown();
+  exhaust->ShootBallOn();
+}
+
+// Called once the command ends or is interrupted.
+void DeliverToLowGoalCommand::End(bool interrupted) {
+  exhaust->PushBallOff();
+  exhaust->ShootBallOff();
+}
+
+

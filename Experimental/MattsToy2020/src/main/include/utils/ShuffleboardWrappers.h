7c0ea2394452b96da01a87922897e9d478c0c9f9
Matt Healy <mjhealy@users.noreply.github.com>
2020-02-22 13:58:39 -0500

    Added SimpleDisplay class; tweaks to BooleanToggle

--- Experimental/MattsToy2020/src/main/include/utils/ShuffleboardWrappers.h
+++ Experimental/MattsToy2020/src/main/include/utils/ShuffleboardWrappers.h
@@ -4,20 +4,49 @@
 #include <frc/shuffleboard/ShuffleboardTab.h>
 #include <networktables/NetworkTableEntry.h>
 
+#include <iostream>
+#include <sstream>
 #include <string>
 
 namespace ShuffleboardWrappers {
 
-  inline frc::ShuffleboardTab& getTab(const std::string& tabName) {
-    return frc::Shuffleboard::GetTab(tabName.empty() ? "SmartDashboard"
-                                                     : tabName);
-  }
+  class SimpleDisplay {
+   public:
+    explicit SimpleDisplay(const std::string& title,
+                           const std::string& tabName = std::string())
+        : SimpleDisplay("", title, tabName) {
+    }
+    SimpleDisplay(const std::string defaultValue, const std::string& title,
+                  const std::string& tabName) {
+      frc::ShuffleboardTab& tab = frc::Shuffleboard::GetTab(tabName);
+      entry = tab.Add(title, "")
+                  .WithWidget(frc::BuiltInWidgets::kTextView)
+                  .GetEntry();
+    }
+
+    /**
+     * Sets a new value to be displayed.
+     *
+     * @param value  the new value to be displayed
+     */
+    template <typename T>
+    void SetValue(T value) {
+      std::ostringstream sout;
+      sout << value;
+      std::string text = sout.str();
+      entry.SetString(sout.str());
+    }
+
+   private:
+    /// NetworkTable entry associated with the slider.
+    nt::NetworkTableEntry entry;
+  };
 
   // class NumberBar {
   //  public:
   //   NumberBar(double min, double max, const std::string& title,
   //                const std::string& tabName = std::string()) {
-  //     frc::ShuffleboardTab& tab = getTab(tabName);
+  //     frc::ShuffleboardTab& tab = frc::Shuffleboard::GetTab(tabName);
   //     entry = tab.Add(title, false)
   //                 .WithWidget(frc::BuiltInWidgets::kNumberBar)
   //                 .GetEntry();
@@ -43,9 +72,23 @@ namespace ShuffleboardWrappers {
      *                "SmartDashboard" tab
      */
     explicit BooleanToggle(const std::string& title,
-                           const std::string& tabName = std::string()) {
-      frc::ShuffleboardTab& tab = getTab(tabName);
-      entry = tab.Add(title, false)
+                           const std::string& tabName = std::string())
+        : BooleanToggle(false, title, tabName) {
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param defaultValue the default/initial value for the control
+     * @param title   title shown for the toggle control on the dashboard
+     * @param tabName name of the tab on which the toggle should be placed; if
+     *                unspecified, it will be put on the default
+     *                "SmartDashboard" tab
+     */
+    BooleanToggle(bool defaultValue, const std::string& title,
+                  const std::string& tabName) {
+      frc::ShuffleboardTab& tab = frc::Shuffleboard::GetTab(tabName);
+      entry = tab.Add(title, defaultValue)
                   .WithWidget(frc::BuiltInWidgets::kToggleSwitch)
                   .GetEntry();
     }

4f6359f6f16d0c00ee705650f57aed3cd5ad0bd3
Matt Healy <mjhealy@users.noreply.github.com>
2020-02-28 14:11:21 -0500

    Documentation updates.

--- Experimental/MattsToy2020/src/main/include/utils/ShuffleboardWrappers.h
+++ Experimental/MattsToy2020/src/main/include/utils/ShuffleboardWrappers.h
@@ -36,37 +36,94 @@ namespace ShuffleboardWrappers {
    */
   class Collection {
    public:
+    /**
+     * Constructs a list to contain other widgets on a Shuffleboard tab.
+     *
+     * @param title   the title shown for the list of components (e.g., "Encoder
+     *                Values")
+     * @param tabName the name of the tab on which the list should be placed
+     */
     Collection(const std::string& title,
                const std::string& tabName = std::string())
         : layout(frc::Shuffleboard::GetTab(tabName).GetLayout(
               title, frc::BuiltInLayouts::kList)) {
     }
 
+    /// Returns the underlying Shuffleboard widget.
     frc::ShuffleboardLayout& Get() {
       return layout;
     }
 
    private:
+    /// The underlying Shuffleboard widget used to manage the list.
     frc::ShuffleboardLayout& layout;
   };
+
+  /**
+   * A class to display read-only (textual/numeric) values on a Shuffleboard
+   * tab.
+   */
   class SimpleDisplay {
    public:
+    /**
+     * Constructor, initially displaying an empty string.
+     *
+     * @param title   the title shown for the data (e.g., "Left Encoder
+     *                (inches)")
+     * @param tabName the name of the tab on which the widget should be placed
+     */
     explicit SimpleDisplay(const std::string& title,
                            const std::string& tabName = std::string())
         : SimpleDisplay("", title, tabName) {
     }
+
+    /**
+     * Constructor.
+     *
+     * @param defaultValue the initial data to be displayed
+     * @param title   the title shown for the data (e.g., "Left Encoder
+     *                (inches)")
+     * @param tabName the name of the tab on which the widget should be placed
+     */
     SimpleDisplay(const std::string& defaultValue, const std::string& title,
                   const std::string& tabName)
         : SimpleDisplay(defaultValue, title,
                         frc::Shuffleboard::GetTab(tabName)) {
     }
+
+    /**
+     * Constructor.
+     *
+     * @param title   the title shown for the data (e.g., "Left Encoder
+     *                (inches)")
+     * @param collection the collection in which the data should be placed
+     */
     SimpleDisplay(const std::string& title, Collection& collection)
         : SimpleDisplay("", title, collection) {
     }
+
+    /**
+     * Constructor.
+     *
+     * @param defaultValue the initial data to be displayed
+     * @param title   the title shown for the data (e.g., "Left Encoder
+     *                (inches)")
+     * @param collection the collection in which the data should be placed
+     */
     SimpleDisplay(const std::string& defaultValue, const std::string& title,
                   Collection& collection)
         : SimpleDisplay(defaultValue, title, collection.Get()) {
     }
+
+    /**
+     * Constructor.
+     *
+     * @param defaultValue the initial data to be displayed
+     * @param title   the title shown for the data (e.g., "Left Encoder
+     *                (inches)")
+     * @param container the Shuffleboard container in which the data should be
+     *                  placed
+     */
     SimpleDisplay(const std::string& defaultValue, const std::string& title,
                   frc::ShuffleboardContainer& container) {
       entry = container.Add(title, "")
@@ -130,7 +187,8 @@ namespace ShuffleboardWrappers {
      * Constructor.
      *
      * @param defaultValue the default/initial value for the control
-     * @param title   title shown for the toggle control on the dashboard
+     * @param title   title shown for the toggle control on the dashboard (e.g.,
+     *                "Debug on")
      * @param tabName name of the tab on which the toggle should be placed; if
      *                unspecified, it will be put on the default
      *                "SmartDashboard" tab
@@ -143,13 +201,36 @@ namespace ShuffleboardWrappers {
                   .GetEntry();
     }
 
+    /**
+     * Constructor, setting the innitial state of the toggle to "off".
+     *
+     * @param title   the title shown for the toggle (e.g., "Debug on")
+     * @param collection the collection in which the data should be placed
+     */
     BooleanToggle(const std::string& title, Collection& container)
         : BooleanToggle(false, title, container) {
     }
+
+    /**
+     * Constructor.
+     *
+     * @param defaultValue the initial state for the toggle (true == on)
+     * @param title   the title shown for the toggle (e.g., "Debug on")
+     * @param collection the collection in which the data should be placed
+     */
     BooleanToggle(bool defaultValue, const std::string& title,
                   Collection& container)
         : BooleanToggle(defaultValue, title, container.Get()) {
     }
+
+    /**
+     * Constructor.
+     *
+     * @param defaultValue the initial state for the toggle (true == on)
+     * @param title   the title shown for the toggle (e.g., "Debug on")
+     * @param container the Shuffleboard container in which the data should be
+     *                  placed
+     */
     BooleanToggle(bool defaultValue, const std::string& title,
                   frc::ShuffleboardContainer& container) {
       entry = container.Add(title, defaultValue)

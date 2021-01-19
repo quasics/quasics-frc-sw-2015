51760e998adbdc695073a0a5faabf0544e3e0c0a
Matt Healy <mjhealy@users.noreply.github.com>
2021-01-18 23:43:25 -0500

    Implemented PID control, added documentation.

--- Experimental/RomiToy2021/src/main/cpp/commands/DriveForward.cpp
+++ Experimental/RomiToy2021/src/main/cpp/commands/DriveForward.cpp
@@ -8,45 +8,55 @@
 
 #include <iostream>
 
-// If true, use the on-board gyro to compute direction/error.
-constexpr bool useGyroForAngles = false;
-
 // Called when the command is initially scheduled.
 void DriveForward::Initialize() {
-  if (useGyroForAngles) {
+  if (m_useGyro) {
     m_driveTrain->ResetGyro();
   } else {
     m_driveTrain->ResetEncoders();
   }
+  m_integral = 0;
+  m_lastError = 0;
 }
 
 // Called repeatedly when this Command is scheduled to run
 void DriveForward::Execute() {
-  double turnPower;
-  if (useGyroForAngles) {
-    constexpr double kP = 0.02;
-
+  double error;
+  if (m_useGyro) {
     const double rawAngle = m_driveTrain->GetGyroAngleZ();
     const double clampedAngle = double(int(rawAngle) % 360);
     const double adjustedAngle =
         (clampedAngle <= 180) ? clampedAngle : clampedAngle - 360;
-    const double error =
-        -adjustedAngle;  // our target delta for the angle is zero
-    std::cout << "rawAngle: " << rawAngle << ", clamped: " << clampedAngle
-              << ", adjusted: " << adjustedAngle << ", error: " << error;
-    turnPower = kP * error;
+    error = -adjustedAngle;  // our target delta for the angle is zero
+    if (m_noisy) {
+        std::cout << "rawAngle: " << rawAngle << ", clamped: " << clampedAngle
+                  << ", adjusted: " << adjustedAngle;
+    }
   } else {
     const auto distanceLeft = units::math::abs(m_driveTrain->GetLeftDistance());
     const auto distanceRight =
         units::math::abs(m_driveTrain->GetRightDistance());
-    const double error =
+    error =
         -double(distanceLeft - distanceRight);  // our target delta for distance
                                                 // between the two sides is zero
-    std::cout << "distanceLeft: " << distanceLeft
-              << ", distanceRight: " << distanceRight << ", error: " << error;
-    turnPower = error;
+    if (m_noisy) {
+        std::cout << "distanceLeft: " << distanceLeft
+                  << ", distanceRight: " << distanceRight;
+    }
   }
-  std::cout << ", turnPower: " << turnPower << std::endl;
+
+  m_integral += (error * kCurveInterval);
+  double derivative = ((error - m_lastError) / kCurveInterval);
+
+  const double turnPower = (kP * error)          // Proportional
+                           + (kI * m_integral)   // Integral
+                           + (kD * derivative);  // Derivative
+
+  if (m_noisy) {
+    std::cout << ", error: " << error << ", integral: " << m_integral
+              << ", derivative: " << derivative << ", turnPower: " << turnPower
+              << std::endl;
+  }
 
   m_driveTrain->ArcadeDrive(m_power, turnPower, false);
 }
--- Experimental/RomiToy2021/src/main/include/commands/DriveForward.h
+++ Experimental/RomiToy2021/src/main/include/commands/DriveForward.h
@@ -4,34 +4,79 @@
 
 #pragma once
 
-#include "subsystems/Drivetrain.h"
-
 #include <frc2/command/CommandBase.h>
 #include <frc2/command/CommandHelper.h>
 
+#include <cmath>
+
+#include "subsystems/Drivetrain.h"
+
 /**
- * An example command.
- *
- * <p>Note that this extends CommandHelper, rather extending CommandBase
- * directly; this is crucially important, or else the decorator functions in
- * Command will *not* work!
+ * A command supporting forward motion at a fixed speed (until interrupted) that
+ * uses PID control to try to correct for errors.
+ * 
+ * This class is based on discussion at
+ * https://frc-pdr.readthedocs.io/en/latest/control/driving_straight.html.
  */
 class DriveForward
     : public frc2::CommandHelper<frc2::CommandBase, DriveForward>
 {
 public:
-  DriveForward(Drivetrain *driveTrain, double power) : m_driveTrain(driveTrain), m_power(power)
-  {
-    AddRequirements({m_driveTrain});
-  }
+ /**
+  * Different sensors we can use to detect deviation from straight-line
+  * movement.
+  */
+ enum class SensorMode { Gyro, Encoders };
 
-  void Initialize() override;
+ /**
+  * @param driveTrain pointer to the drive train substem
+  * @param power power to be applied (clamped to the range [0..1])
+  * @param sensorMode specifies the type of sensor to be used to detect errors
+  *                   in direction
+  * @param noisy if true, log debugging output while the command is running
+  */
+ DriveForward(Drivetrain *driveTrain, double power,
+              SensorMode sensorMode = SensorMode::Encoders, bool noisy = false)
+     : m_driveTrain(driveTrain),
+       m_power(std::max(0.0, std::min(power, 1.0))),
+       m_useGyro(sensorMode == SensorMode::Gyro),
+       m_noisy(noisy) {
+   AddRequirements({m_driveTrain});
+ }
 
-  void Execute() override;
+ //
+ // Overridden methods from base class.
+public:
+ void Initialize() override;
 
-  void End(bool interrupted) override;
+ // Note: at each iteration, we will compute error (from heading, or delta in
+ // left/right distance), then apply correction factor:
+ //    correction = (kP * error)
+ //                 + (kI * m_integral)
+ //                 + (kD * change in error)
+ void Execute() override;
 
+ void End(bool interrupted) override;
+
+ //
+ // Data members
 private:
   Drivetrain *const m_driveTrain;
-  double m_power;
+  const double m_power;
+  const bool m_useGyro;
+  const bool m_noisy;
+
+  double m_integral = 0;   ///< cumulative error sample (should trend to 0)
+  double m_lastError = 0;  ///< used to calculate change in error
+                           ///< between samples
+
+  // These values should really be tuned to the robot, per discussion cited
+  // above.  However, these seem to work well enough for now.
+  const double kP = 1;  ///< proportional gain (should always be >0)
+  const double kI = 1;  ///< integral gain (can be 0, if we're just doing P)
+  const double kD = 0;  ///< derivative gain (can be 0, if we're just doing P/PI)
+  
+  
+  const double kCurveInterval = 0.02;  ///< period over which we're sampling
+                                       ///< error (0.02sec, since it's 50Hz)
 };

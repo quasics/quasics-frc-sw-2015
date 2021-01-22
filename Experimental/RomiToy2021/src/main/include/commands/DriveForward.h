1908c4b87633f06f822aeacc9467c51c2ed0b9cc
Matt Healy <mjhealy@users.noreply.github.com>
2021-01-22 11:46:59 -0500

    Added support for defining a stop condition (vs. having the command run forever/until interrupted).

--- Experimental/RomiToy2021/src/main/include/commands/DriveForward.h
+++ Experimental/RomiToy2021/src/main/include/commands/DriveForward.h
@@ -31,14 +31,20 @@ public:
  /**
   * @param driveTrain pointer to the drive train substem
   * @param power power to be applied (clamped to the range [0..1])
+  * @param stopCondition used to signal when movement should stop (e.g.,
+  *                      after a specific distance has been traveled, time
+  *                      elapsed, etc.)
   * @param sensorMode specifies the type of sensor to be used to detect errors
   *                   in direction
   * @param noisy if true, log debugging output while the command is running
   */
- DriveForward(Drivetrain *driveTrain, double power,
-              SensorMode sensorMode = SensorMode::Encoders, bool noisy = false)
+ DriveForward(
+     Drivetrain *driveTrain, double power,
+     std::function<bool()> stopCondition = [] { return false; },
+     SensorMode sensorMode = SensorMode::Encoders, bool noisy = false)
      : m_driveTrain(driveTrain),
        m_power(std::max(0.0, std::min(power, 1.0))),
+       m_stopCondition(stopCondition),
        m_useGyro(sensorMode == SensorMode::Gyro),
        m_noisy(noisy) {
    AddRequirements({m_driveTrain});
@@ -56,6 +62,10 @@ public:
  //                 + (kD * change in error)
  void Execute() override;
 
+ bool IsFinished() override {
+   return m_stopCondition();
+ }
+
  void End(bool interrupted) override;
 
  //
@@ -63,6 +73,7 @@ public:
 private:
   Drivetrain *const m_driveTrain;
   const double m_power;
+  std::function<bool()> m_stopCondition;
   const bool m_useGyro;
   const bool m_noisy;
 

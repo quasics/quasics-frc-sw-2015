59e729e34dfa973c30071804980f0baff401f581
Matt Healy <mjhealy@users.noreply.github.com>
2021-01-31 19:28:38 -0500

    Very hacky.  Not really usable as yet.

--- Experimental/Vision/multiCameraServer.py
+++ Experimental/Vision/multiCameraServer.py
@@ -4,7 +4,9 @@
 # Open Source Software; you can modify and/or share it under the terms of
 # the WPILib BSD license file in the root directory of this project.
 
+import cv2
 import json
+import numpy as np
 import time
 import sys
 
@@ -61,6 +63,7 @@ server = False
 cameraConfigs = []
 switchedCameraConfigs = []
 cameras = []
+ntinst = None
 
 def parseError(str):
     """Report parse error."""
@@ -205,6 +208,110 @@ def startSwitchedCamera(config):
 
     return server
 
+def runMultiCamera():
+    # start cameras
+    for config in cameraConfigs:
+        cameras.append(startCamera(config))
+
+    # start switched cameras
+    for config in switchedCameraConfigs:
+        startSwitchedCamera(config)
+
+    # loop forever
+    while True:
+        print("Hanging out...")
+        time.sleep(10)
+
+def runCamera(cameraObject):
+   cameraConfig = cameraObject.config
+   print("Config:", cameraConfig)
+   width = cameraConfig["width"]
+   height = cameraConfig["height"]
+   print("Width:", width, "Height:", height)
+   
+   inst = CameraServer.getInstance()
+   camera = UsbCamera(cameraObject.name, cameraObject.path)
+   sink = inst.startAutomaticCapture(camera=camera, return_server=False) # Will return either the MjpegServer or the UsbCamera
+   print("Started server")
+
+   camera.setConfigJson(json.dumps(cameraObject.config))
+   camera.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen)
+   # if cameraObject.streamConfig is not None:
+       # server.setConfigJson(json.dumps(cameraObject.streamConfig))
+
+   input_stream = inst.getVideo()
+   print("Got input stream")
+   output_stream = inst.putVideo('Processed', width, height)
+   print("Got output stream")
+
+   # Table for vision output information
+   vision_nt = ntinst.getTable('Vision')
+   print("Got 'Vision' table")
+
+   # Wait for NetworkTables to start
+   time.sleep(0.5)
+   
+   # Preallocate the buffer used for the input image
+   input_img = np.zeros(shape=(width, height, 3), dtype=np.uint8)
+   print("Preallocated image")
+
+   while True:
+      start_time = time.time()
+
+      frame_time, input_img = input_stream.grabFrame(input_img)
+      output_img = np.copy(input_img)
+
+      # Notify output of error and skip iteration
+      if frame_time == 0:
+         output_stream.notifyError(input_stream.getError())
+         continue
+
+      # Convert to HSV and threshold image
+      hsv_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2HSV)
+      
+      # Evaluation of the balls used in Infinite Recharge suggests a
+      # bounding range of (19, 70, 104) to (30, 179, 255)
+      binary_img = cv2.inRange(hsv_img, (19, 55, 104), (45, 179, 255))
+
+      # Remove noise and small holes from image
+      # binary_img = cv2.morphologyEx(binary_img, cv2.MORPH_OPEN, kernel, iterations = 2)
+
+      _, contour_list, _ = cv2.findContours(binary_img, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
+
+      x_list = []
+      y_list = []
+
+      index = 0
+      for contour in contour_list:
+
+         # Ignore small contours that could be because of noise/bad thresholding
+         if cv2.contourArea(contour) < 15:
+            continue
+
+         cv2.drawContours(output_img, contour, index, color = (255, 255, 255), thickness = -1)
+
+         rect = cv2.minAreaRect(contour)
+         center, size, angle = rect
+         center = [int(dim) for dim in center] # Convert to int so we can draw
+
+         # Draw rectangle and circle
+         # cv2.drawContours(output_img, np.int0(cv2.boxPoints(rect)), -1, color = (0, 0, 255), thickness = 2)
+         # cv2.circle(output_img, center = center, radius = 3, color = (0, 0, 255), thickness = -1)
+
+         x_list.append((center[0] - width / 2) / (width / 2))
+         x_list.append((center[1] - width / 2) / (width / 2))
+         
+         index = index + 1
+
+      vision_nt.putNumberArray('target_x', x_list)
+      vision_nt.putNumberArray('target_y', y_list)
+
+      processing_time = time.time() - start_time
+      fps = 1 / processing_time
+      cv2.putText(output_img, str(round(fps, 1)), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
+      output_stream.putFrame(output_img)
+      # print("Updated frame")
+
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
         configFile = sys.argv[1]
@@ -223,14 +330,7 @@ if __name__ == "__main__":
         ntinst.startClientTeam(team)
         ntinst.startDSClient()
 
-    # start cameras
-    for config in cameraConfigs:
-        cameras.append(startCamera(config))
-
-    # start switched cameras
-    for config in switchedCameraConfigs:
-        startSwitchedCamera(config)
-
-    # loop forever
-    while True:
-        time.sleep(10)
+    if False:
+        runMultiCamera()
+    else:
+        runCamera(cameraConfigs[0])

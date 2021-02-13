60fd505c2c4f14386dc352aa82ad38d37adf1e89
ScottHealy <32375761+ScottHealy@users.noreply.github.com>
2021-02-13 15:43:16 -0500

    Added DoASpin

---
+++ Quasics2021Code/MaeTester/src/main/cpp/commands/DoASpin.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/DoASpin.h"
+
+DoASpin::DoASpin(Drivebase* drivebase)
+ : drivebase(drivebase){
+  // Use addRequirements() here to declare subsystem dependencies.
+  AddRequirements(drivebase);
+}
+
+// Called when the command is initially scheduled.
+void DoASpin::Initialize() {
+  drivebase->setMotorSpeed(-.3, .3);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void DoASpin::Execute() {}
+
+// Called once the command ends or is interrupted.
+void DoASpin::End(bool interrupted) {
+  drivebase-> Stop();
+}
+
+// Returns true when the command should end.
+bool DoASpin::IsFinished() {
+  return false;
+}

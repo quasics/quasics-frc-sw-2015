7a490d0aac4f8dc699bde1e6083fc06e2d138704
Matt Healy <mjhealy@users.noreply.github.com>
2021-02-20 14:31:17 -0500

    Added initial support for 'switch drive'

--- Experimental/2021/Common2021/TeleopTankDrive.h
+++ Experimental/2021/Common2021/TeleopTankDrive.h
@@ -11,21 +11,41 @@
 
 /**
  * A sample implementation of a "tank drive" command, for use in teleop mode.
+ * 
+ * This also provides support for "switch drive", where the front and rear of the
+ * robot are treated as being reversed (which also means that the logical
+ * left&right motors are both switched and inverted).
  */
 class TeleopTankDrive
     : public frc2::CommandHelper<frc2::CommandBase, TeleopTankDrive> {
  public:
+  /**
+   * Constructor.
+   * 
+   * @param driveBase   the drive base we're working with
+   * @param leftSpeedSupplier   function returning the requested speed for the left side
+   * @param rightSpeedSupplier  function returning the requested speed for the right side
+   * @param switchDriveEnabled  function returning "true" if "switch mode" should be active
+   *                            (if not provided, switch mode is never active)
+   */
   TeleopTankDrive(CommonDriveSubsystem* driveBase,
                   std::function<double()> leftSpeedSupplier,
-                  std::function<double()> rightSpeedSupplier)
+                  std::function<double()> rightSpeedSupplier,
+                  std::function<bool()> switchDriveEnabled = []{return false;})
       : m_driveBase(driveBase),
         m_leftSpeedSupplier(leftSpeedSupplier),
-        m_rightSpeedSupplier(rightSpeedSupplier) {
+        m_rightSpeedSupplier(rightSpeedSupplier),
+        m_switchDriveEnabled(switchDriveEnabled) {
     AddRequirements({m_driveBase});
   }
 
   void Execute() override {
-    m_driveBase->TankDrive(m_leftSpeedSupplier(), m_rightSpeedSupplier());
+    if (m_switchDriveEnabled()) {
+      // Swap (and invert) left and right motor control
+      m_driveBase->TankDrive(-m_rightSpeedSupplier(), -m_leftSpeedSupplier());
+    } else {
+      m_driveBase->TankDrive(m_leftSpeedSupplier(), m_rightSpeedSupplier());
+    }
   }
 
   void End(bool interrupted) override {
@@ -36,4 +56,5 @@ class TeleopTankDrive
   CommonDriveSubsystem* const m_driveBase;
   std::function<double()> m_leftSpeedSupplier;
   std::function<double()> m_rightSpeedSupplier;
+  std::function<bool()> m_switchDriveEnabled;
 };

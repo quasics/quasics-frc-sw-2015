a5325820962d3709a8df1d17e7ff90701d17cb25
Matt Healy <mjhealy@users.noreply.github.com>
2021-03-12 11:42:13 -0500

    Added LOG_EVERY_N_SECONDS()

--- Experimental/2021/Common2021/LoggingHelpers.h
+++ Experimental/2021/Common2021/LoggingHelpers.h
@@ -1,5 +1,11 @@
 #pragma once
 
+/// Generates logging output every N times that the code is executed.
+/// (This is intended to help throttle debugging output to a level that is
+/// reasonable/meaningful for developers/drivers.)
+///
+/// @param n    number of seconds between logging output
+/// @param outputCmd  command to be run, producing logging output
 #define LOG_EVERY_N_TIMES(n, outputCmd)       \
   {                                           \
     static int counter = -1;                  \
@@ -7,3 +13,12 @@
       outputCmd;                              \
     }                                         \
   }
+
+/// Generates logging output every N seconds.
+///
+/// Intended for use within "Periodic" functions, etc., with an assumed
+/// frequency of 50Hz (which is standard for WPILib).
+///
+/// @param n    number of seconds between logging output
+/// @param outputCmd  command to be run, producing logging output
+#define LOG_EVERY_N_SECONDS(n, outputCmd) LOG_EVERY_N_TIMES(((n)*50), outputCmd)

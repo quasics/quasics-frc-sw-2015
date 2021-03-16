99878404cf83a64d0a2b66456f9ddcb4665c8272
nurfr <40575718+nurfr@users.noreply.github.com>
2021-03-16 19:08:21 -0400

    Created commands for intake.

---
+++ Quasics2021Code/MaeTester/src/main/cpp/commands/IntakePowerCells.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/IntakePowerCells.h"
+
+IntakePowerCells::IntakePowerCells(Intake* intake) : intake(intake) {
+  // Use addRequirements() here to declare subsystem dependencies.
+  AddRequirements(intake);
+}
+
+// Called when the command is initially scheduled.
+void IntakePowerCells::Initialize() {
+  intake->IntakeBallOn();
+  intake->ConveyBallOn();
+}
+
+// Called repeatedly when this Command is scheduled to run
+void IntakePowerCells::Execute() {
+}
+
+// Called once the command ends or is interrupted.
+void IntakePowerCells::End(bool interrupted) {
+  intake->IntakeBallOff();
+  intake->ConveyBallOff();
+}
+
+// Returns true when the command should end.
+bool IntakePowerCells::IsFinished() {
+  return false;
+}

cd94548acfcee440f73b095511313a9fb108e927
Matt Healy <mjhealy@users.noreply.github.com>
2021-03-27 12:32:29 -0400

    Cleaning up obsolete code

--- Experimental/2021/RomiToy2021/src/main/cpp/RobotContainer.cpp
+++ Experimental/2021/RomiToy2021/src/main/cpp/RobotContainer.cpp
@@ -276,7 +276,6 @@ frc2::SequentialCommandGroup* RobotContainer::GenerateRamseteCommand(
     const frc::Pose2d& start,
     const std::vector<frc::Translation2d>& interiorWaypoints,
     const frc::Pose2d& end, bool resetTelemetryAtStart) {
-#if 1
   // Configure trajectory generation.  (Note that this could be
   // embedded in the RobotContainer -- or elsewhere -- and reused
   // as needed, including tweaks to max speed/acceleration.)
@@ -298,66 +297,6 @@ frc2::SequentialCommandGroup* RobotContainer::GenerateRamseteCommand(
 
   return generator.GenerateCommand(speedProfile, start, interiorWaypoints, end,
                                    resetTelemetryAtStart);
-#else
-  using namespace RobotData::DriveConstants;
-  using namespace RobotData::PathFollowingLimits;
-
-  const frc::DifferentialDriveKinematics kDriveKinematics{
-      m_drive.GetTrackWidth()};
-
-  // Set up config for trajectory
-  frc::SimpleMotorFeedforward<units::meter> feedForward(
-      ksVolts, kvVoltSecondsPerMeter, kaVoltSecondsSquaredPerMeter);
-  frc::DifferentialDriveVoltageConstraint voltageConstraints(
-      feedForward, kDriveKinematics, 10_V);
-  frc::TrajectoryConfig config(kMaxSpeed, kMaxAcceleration);
-
-  // Add kinematics to ensure max speed is actually obeyed
-  config.SetKinematics(kDriveKinematics);
-
-  // Apply the voltage constraint
-  config.AddConstraint(voltageConstraints);
-
-  auto trajectory = frc::TrajectoryGenerator::GenerateTrajectory(
-      start, interiorWaypoints, end, config);
-
-  frc2::RamseteCommand ramseteCommand(
-      /* trajectory to follow */
-      trajectory,
-      /* function that supplies the robot pose */
-      [this]() { return m_drive.GetPose(); },
-      /* RAMSETE controller used to follow the trajectory */
-      frc::RamseteController(kRamseteB, kRamseteZeta),
-      /* calculates the feedforward for the drive */
-      feedForward,
-      /* kinematics for the robot drivetrain */
-      kDriveKinematics,
-      /* function that supplies the left/right side speeds */
-      [this] { return m_drive.GetWheelSpeeds(); },
-      /* left controller */
-      frc2::PIDController(kPDriveVel, kIDriveVel, kDDriveVel),
-      /* right controller */
-      frc2::PIDController(kPDriveVel, kIDriveVel, kDDriveVel),
-      /* output function */
-      [this](auto left, auto right) { m_drive.TankDriveVolts(left, right); },
-      /* required subsystems */
-      {&m_drive});
-
-  return new frc2::SequentialCommandGroup(
-      frc2::PrintCommand("Starting trajectory code"),
-      frc2::InstantCommand(
-          [this, resetTelemetryAtStart, trajectory] {
-                if (resetTelemetryAtStart) {
-                  m_drive.ResetOdometry(trajectory.InitialPose());
-                }
-              },
-          {&m_drive}),
-      std::move(ramseteCommand),
-      // Shut the drive down.
-      frc2::PrintCommand("Shutting down trajectory code"),
-      frc2::InstantCommand([this] { m_drive.TankDriveVolts(0_V, 0_V); },
-                           {&m_drive}));
-#endif
 }
 
 frc2::SequentialCommandGroup* RobotContainer::GenerateSampleRamseteCommand(

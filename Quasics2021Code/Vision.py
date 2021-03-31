7f6d161d74ad1cfa50f08da0c9f7466c19e969d1
Annabelle <73316806+tsuneoid@users.noreply.github.com>
2021-03-31 18:52:18 -0400

    Temporarily put path determination code in after the main().

--- Quasics2021Code/Vision.py
+++ Quasics2021Code/Vision.py
@@ -350,3 +350,46 @@ def main():
                             # running the Romi image and in the "disabled" robot
                             # state, when testing on a lightly-loaded network.
 main()
+
+#temporary place for determination code until figured out where to place is in the code.
+
+if numBalls == 3:
+    # Must be Path A.  Just need to figure out which route within it.
+    if bestIndex == 0:
+        otherindx1 = 1
+        otherindx2 = 2
+
+    elif bestIndex == 1:
+        otherindx1 = 0
+        otherindx2 = 2
+
+    elif bestIndex == 2:
+        otherindx1 = 0 
+        otherindx2 = 1
+
+    if all_targets_left_list[otherindx1] < all_targets_left_list[bestIndex] and all_targets_left_list[otherindx2] < all_targets_left_list[bestIndex]:
+        print("It is Path 2A")
+
+    elif all_targets_left_list[otherindx1] > all_targets_left_list[bestIndex] and all_targets_left_list[otherindx2] > all_targets_left_list[bestIndex]:
+        print("Error")
+
+    else:
+        print("It is Path 1A")
+
+elif numBalls == 2:
+    # Must be path B
+    if bestIndex == 0:
+        otherindx = 1
+    else:
+        otherindx = 0
+
+    if all_targets_left_list[otherindx] > all_targets_left_list[bestIndex]:
+        print("It is Path 1B")
+    else:
+        print("It is Path 2B")
+    
+    
+
+else:
+    # Something weird, can't determine A or B
+    print("Can't identify primary path information!")

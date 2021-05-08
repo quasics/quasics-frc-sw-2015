cec46b763ff7adc2af362182c9f0617ad78d8c9a
Matt Healy <mjhealy@users.noreply.github.com>
2021-05-08 14:53:15 -0400

    Added green data range

--- Experimental/2021/Vision/SimpleMasker.py
+++ Experimental/2021/Vision/SimpleMasker.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import sys
 
+captureGreen = True
 captureBlue = False
 
 windowName = 'Simple Masking'
@@ -15,6 +16,17 @@ high_H = 30
 high_S = 250
 high_V = 255
 
+# Rough shades for green (from photoreflective tape and LED rings)
+# Pass 1: 38/20/212 - 75/86/255 (Good for Reflective-1)
+# Pass 2: 55/20/162 - 75/102/255 (Good for Reflective-2)
+# Pass 3: 42/20/162 - 75/102/255 (reasonably good across Reflective-[123])
+green_low_H = 42
+green_low_S = 20
+green_low_V = 162
+green_high_H = 75
+green_high_S = 102
+green_high_V = 255
+
 if len(sys.argv) > 1:
     filename = sys.argv[1]
 
@@ -24,7 +36,11 @@ print("filename=",filename)
 image = cv2.imread(filename)
 hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-if captureBlue:
+if captureGreen:
+    lower_green = np.array([green_low_H,green_low_S,green_low_V])
+    upper_green = np.array([green_high_H,green_high_S,green_high_V])
+    mask = cv2.inRange(hsv_image, lower_green, upper_green)
+elif captureBlue:
     # define range of blue color in HSV
     lower_blue = np.array([110,50,50])
     upper_blue = np.array([130,255,255])

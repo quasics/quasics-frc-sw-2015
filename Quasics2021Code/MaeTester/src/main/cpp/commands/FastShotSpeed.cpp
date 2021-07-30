b0580955b0dead422c807fabbb8de335c77286e1
ScottHealy <32375761+ScottHealy@users.noreply.github.com>
2021-07-29 20:32:46 -0400

    added slow and fast shot speeds. values for the speed have not been set yet

---
+++ Quasics2021Code/MaeTester/src/main/cpp/commands/FastShotSpeed.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/FastShotSpeed.h"
+/*
+FastShotSpeed::FastShotSpeed(Shooter* shooter, double power)
+    : shooter(shooter), power(power){
+  // Use addRequirements() here to declare subsystem dependencies.
+  addRequirements(shooter);
+}
+
+// Called when the command is initially scheduled.
+void FastShotSpeed::Initialize() {
+  Shooter->setSpeed(power);
+}
+// Called repeatedly when this Command is scheduled to run
+void FastShotSpeed::Execute() {}
+
+// Called once the command ends or is interrupted.
+void FastShotSpeed::End(bool interrupted) {
+  Shooter->stop();
+}
+
+// Returns true when the command should end.
+bool FastShotSpeed::IsFinished() {
+  return false;
+}
---
+++ Quasics2021Code/MaeTester/src/main/cpp/commands/SlowShotSpeed.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/SlowShotSpeed.h"
+/*
+SlowShotSpeed::SlowShotSpeed(Shooter* shooter, double power) 
+    : shooter(shooter), power(power) {
+  // Use addRequirements() here to declare subsystem dependencies.
+  addRequirements(shooter);
+}
+
+// Called when the command is initially scheduled.
+void SlowShotSpeed::Initialize() {
+  shooter->setSpeed(power);  // makes ball go yeet
+}
+
+// Called repeatedly when this Command is scheduled to run
+void SlowShotSpeed::Execute() {}
+
+// Called once the command ends or is interrupted.
+void SlowShotSpeed::End(bool interrupted) {
+  shooter->Stop();
+}
+
+// Returns true when the command should end.
+bool SlowShotSpeed::IsFinished() {
+  return false;
+}

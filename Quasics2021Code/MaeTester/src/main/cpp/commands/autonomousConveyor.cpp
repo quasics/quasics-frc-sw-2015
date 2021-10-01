ffb76717b3636eb9652156cd7a3499b70ac2b5e7
quasics-driver <47063590+quasics-driver@users.noreply.github.com>
2021-09-30 20:07:08 -0400

    added empty autonomous commands for conveyor, drivebase, and shooter.

---
+++ Quasics2021Code/MaeTester/src/main/cpp/commands/autonomousConveyor.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/autonomousConveyor.h"
+
+autonomousConveyor::autonomousConveyor() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void autonomousConveyor::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void autonomousConveyor::Execute() {}
+
+// Called once the command ends or is interrupted.
+void autonomousConveyor::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool autonomousConveyor::IsFinished() {
+  return false;
+}
---
+++ Quasics2021Code/MaeTester/src/main/cpp/commands/autonomousShooter.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/autonomousShooter.h"
+
+autonomousShooter::autonomousShooter() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void autonomousShooter::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void autonomousShooter::Execute() {}
+
+// Called once the command ends or is interrupted.
+void autonomousShooter::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool autonomousShooter::IsFinished() {
+  return false;
+}

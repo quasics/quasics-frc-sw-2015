6a5e670e42c47bfdc763c2dd05c1aa23e5af4da4
Matt Healy <mjhealy@users.noreply.github.com>
2021-10-04 11:59:05 -0400

    Initial (untested) LED lighting example

---
+++ Experimental/2021/LightingExample/src/main/java/frc/robot/subsystems/LightingSubsystem.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LightingSubsystem extends SubsystemBase {
+  AddressableLED m_led;
+  AddressableLEDBuffer m_ledBuffer;
+
+  /** Creates a new ExampleSubsystem. */
+  public LightingSubsystem(int pwmPort, int numLights) {
+    m_led = new AddressableLED(pwmPort);
+
+    m_ledBuffer = new AddressableLEDBuffer(numLights);
+    m_led.setLength(m_ledBuffer.getLength());
+
+    // Set the data
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+  public void SetStripColor(int red, int green, int blue) {
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      m_ledBuffer.setRGB(i, red, green, blue);
+   }
+   
+   m_led.setData(m_ledBuffer);
+  }
+}

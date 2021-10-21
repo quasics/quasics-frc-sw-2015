ee802f70417efc50c56032cc485a07d27a99f742
Matt Healy <mjhealy@users.noreply.github.com>
2021-10-21 14:21:04 -0400

    Minor cleanup.

--- Quasics2021Code/MaeTester/src/main/cpp/subsystems/Lights.cpp
+++ Quasics2021Code/MaeTester/src/main/cpp/subsystems/Lights.cpp
@@ -14,17 +14,15 @@ Lights::Lights() {
   // that will fail.)
   m_led.SetLength(kLength);
 
-  for (int i = 0; i < kLength; ++i) {
-    // By default, turn all of the lights off.
-    m_ledBuffer[i].SetRGB(0, 0, 0);
-  }
+  // Start with all of the lights off.
+  TurnStripOff();
 }
 
 // This method will be called once per scheduler run
 void Lights::Periodic() {}
 
 void Lights::SetStripColor(int red, int green, int blue){
-  std::cout << "Setting strip solid color" << std::endl;
+  std::cout << "Setting strip to solid color" << std::endl;
   for (int i = 0; i < kLength; i++) {
     m_ledBuffer[i].SetRGB(red, green, blue);
   }

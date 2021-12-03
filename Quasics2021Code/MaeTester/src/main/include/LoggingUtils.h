8f6062e7320a209f4444d61e7dde86ff90f50af4
Matt Healy <mjhealy@users.noreply.github.com>
2021-12-03 14:36:20 -0500

    Minor simplification of logging macro.

--- Quasics2021Code/MaeTester/src/main/include/LoggingUtils.h
+++ Quasics2021Code/MaeTester/src/main/include/LoggingUtils.h
@@ -4,13 +4,9 @@
 
 #define LOG_EVERY_N_TIMES(n, msg)    \
   do {                               \
-    static int counter = -1;         \
-    if (n > 1) {                     \
-      counter = (counter + 1) % n;   \
-    } else {                         \
-      counter = 0;                   \
-    }                                \
+    static int counter = 0;          \
     if (counter == 0) {              \
       std::cerr << msg << std::endl; \
     }                                \
+    counter = (counter + 1) % n;     \
   } while (false)

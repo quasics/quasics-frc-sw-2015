bb73d7c80544984c247ca653ba9fdcd1d4a5cba1
Matt Healy <mjhealy@users.noreply.github.com>
2022-01-20 20:33:13 -0500

    Initial implementation. * Added motor and encoder bindings. * Added bodies for methods.

--- Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/subsystems/DriveBase.java
+++ Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/subsystems/DriveBase.java
@@ -7,22 +7,31 @@ package frc.robot.subsystems;
 import frc.robot.Constants;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveBase extends SubsystemBase {
 
-  private MotorController leftRear = new CANSparkMax(Constants.MotorIds.LEFT_REAR_DRIVE_MOTOR_ID, MotorType.kBrushless);
-  private MotorController rightRear = new CANSparkMax(Constants.MotorIds.RIGHT_REAR_DRIVE_MOTOR_ID,
+  private CANSparkMax leftRear = new CANSparkMax(Constants.MotorIds.LEFT_REAR_DRIVE_MOTOR_ID, MotorType.kBrushless);
+  private CANSparkMax rightRear = new CANSparkMax(Constants.MotorIds.RIGHT_REAR_DRIVE_MOTOR_ID,
       MotorType.kBrushless);
-  private MotorController leftFront = new CANSparkMax(Constants.MotorIds.LEFT_FRONT_DRIVE_MOTOR_ID,
+  private CANSparkMax leftFront = new CANSparkMax(Constants.MotorIds.LEFT_FRONT_DRIVE_MOTOR_ID,
       MotorType.kBrushless);
-  private MotorController rightFront = new CANSparkMax(Constants.MotorIds.RIGHT_FRONT_DRIVE_MOTOR_ID,
+  private CANSparkMax rightFront = new CANSparkMax(Constants.MotorIds.RIGHT_FRONT_DRIVE_MOTOR_ID,
       MotorType.kBrushless);
+
+  private RelativeEncoder leftEncoder = leftRear.getEncoder();
+  private RelativeEncoder rightEncoder = rightRear.getEncoder();
+
+  private MotorControllerGroup leftMotors = new MotorControllerGroup(leftFront, leftRear);
+  private MotorControllerGroup rightMotors = new MotorControllerGroup(rightFront, rightRear);
 
   /** Creates a new DriveBase. */
   public DriveBase() {
     this.setName("DriveBase");
+    // TODO: Add code to configure encoders.
   }
 
   public void stop() {
@@ -30,17 +39,16 @@ public class DriveBase extends SubsystemBase {
   }
 
   public void setPower(double leftPercent, double rightPercent) {
-    // TODO: Implement this.
+    leftMotors.set(leftPercent);
+    rightRear.set(rightPercent);
   }
 
   public double getLeftEncoderPosition() {
-    // TODO: Implement this.
-    return 0;
+    return leftEncoder.getPosition();
   }
 
   public double getRightEncoderPosition() {
-    // TODO: Implement this.
-    return 0;
+    return rightEncoder.getPosition();
   }
 
   @Override

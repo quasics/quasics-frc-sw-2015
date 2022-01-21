6dd5fa33b90b312e289a8f1e7e7ef676248c15b4
Matt Healy <mjhealy@users.noreply.github.com>
2022-01-21 18:44:24 -0500

    Wiring in tank drive as default for drive base.

--- Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/RobotContainer.java
+++ Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/RobotContainer.java
@@ -5,15 +5,21 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.TankDrive;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DriveBase;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
@@ -22,19 +28,35 @@ public class RobotContainer {
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private final DriveBase driveBase = new DriveBase();
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
-    // Configure the button bindings
+    // Allocate the joystick for the driver.
+    Joystick driverStick = new Joystick(Constants.OperatorInterface.DRIVER_JOYSTICK);
+
+    // Configure the button bindings.
     configureButtonBindings();
+
+    // Configure tank drive command.
+    TankDrive tankDrive = new TankDrive(driveBase,
+        () -> driverStick.getRawAxis(Constants.OperatorInterface.LogitechGamePad.LEFT_Y_AXIS),
+        () -> driverStick.getRawAxis(Constants.OperatorInterface.LogitechGamePad.RIGHT_Y_AXIS));
+    driveBase.setDefaultCommand(tankDrive);
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.

b7b1f592df3229105781acdfa83f36255548951a
Matt Healy <mjhealy@users.noreply.github.com>
2022-01-21 18:43:59 -0500

    Initial implementation of TankDrive command.

---
+++ Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/commands/TankDrive.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.DriveBase;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class TankDrive extends CommandBase {
+
+  public interface DrivePowerSupplier {
+    public double get();
+  }
+
+  final DriveBase driveBase;
+  final DrivePowerSupplier leftPowerSupplier;
+  final DrivePowerSupplier rightPowerSupplier;
+
+  /** Creates a new TankDrive. */
+  public TankDrive(DriveBase driveBase, DrivePowerSupplier leftPowerSupplier, DrivePowerSupplier rightPowerSupplier) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveBase);
+
+    this.driveBase = driveBase;
+    this.leftPowerSupplier = leftPowerSupplier;
+    this.rightPowerSupplier = rightPowerSupplier;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    driveBase.setPower(leftPowerSupplier.get(), rightPowerSupplier.get());
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    driveBase.setPower(leftPowerSupplier.get(), rightPowerSupplier.get());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveBase.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}

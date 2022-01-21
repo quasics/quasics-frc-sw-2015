48b4b73966a242ce9871d25eb3082e3a63e8ab7d
Matt Healy <mjhealy@users.noreply.github.com>
2022-01-21 18:43:35 -0500

    Added constants for driver joystick (Logitech).

--- Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/Constants.java
+++ Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/Constants.java
@@ -30,4 +30,37 @@ public final class Constants {
         public static final int LEFT_REAR_DRIVE_MOTOR_ID = 3;
         public static final int RIGHT_REAR_DRIVE_MOTOR_ID = 4;
     }
+
+    public static final class OperatorInterface {
+        public static final int DRIVER_JOYSTICK = 0;
+
+        public static final class LogitechGamePad {
+            // Axes - Used with the "getRawAxis()" function to access the data for the
+            // individual sticks on the controller (e.g., for "tank drive" coding).
+            //
+            // Note that the left and right triggers aren't treated as buttons: they
+            // report to the driver's station software as if they're joysticks (with a
+            // range of [0.0, 1.0], unlike regular joysticks).
+            public static final int LEFT_X_AXIS = 0;
+            public static final int LEFT_Y_AXIS = 1;
+            public static final int RIGHT_X_AXIS = 2;
+            public static final int RIGHT_Y_AXIS = 3;
+
+            // TODO: These are *clearly* wrong, and need to be fixed.
+            public static final int LEFT_TRIGGER_AXIS = 2;
+            public static final int RIGHT_TRIGGER_AXIS = 3;
+
+            // Buttons
+            public static final int A_BUTTON = 1;
+            public static final int B_BUTTON = 2;
+            public static final int X_BUTTON = 3;
+            public static final int Y_BUTTON = 4;
+            public static final int LEFT_SHOULDER = 5;
+            public static final int RIGHT_SHOULDER = 6;
+            public static final int BACK_BUTTON = 7;
+            public static final int START_BUTTON = 8;
+            public static final int LEFT_STICK_PRESS = 9;
+            public static final int RIGHT_STICK_PRESS = 10;
+        }
+    }
 }

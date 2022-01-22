ed8a0ee29070b1b33274253bfb3c3bb2016e63da
quasics-driver <47063590+quasics-driver@users.noreply.github.com>
2022-01-21 23:17:45 -0500

    Beginning to implement TankDrive command. (Untested)

---
+++ Experimental/2022/MattsToy2022/src/main/cpp/commands/TankDrive.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/TankDrive.h"
+
+TankDrive::TankDrive(DriveBase& driveBase,
+                     std::function<double()> leftPowerSupplier,
+                     std::function<double()> rightPowerSupplier)
+    : m_driveBase(driveBase),
+      m_leftPowerSupplier(leftPowerSupplier),
+      m_rightPowerSupplier(rightPowerSupplier) {
+  AddRequirements(&driveBase);
+}
+
+// Called when the command is initially scheduled.
+void TankDrive::Initialize() {
+  m_driveBase.TankDrive(m_leftPowerSupplier(), m_rightPowerSupplier());
+}
+
+// Called repeatedly when this Command is scheduled to run
+void TankDrive::Execute() {
+  m_driveBase.TankDrive(m_leftPowerSupplier(), m_rightPowerSupplier());
+}
+
+// Called once the command ends or is interrupted.
+void TankDrive::End(bool interrupted) {
+  m_driveBase.Stop();
+}
+
+// Returns true when the command should end.
+bool TankDrive::IsFinished() {
+  return false;
+}

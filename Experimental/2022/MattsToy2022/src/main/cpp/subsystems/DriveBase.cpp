a52c47752b7d61c00ea49dc4ad61e5d3574d71f4
quasics-driver <47063590+quasics-driver@users.noreply.github.com>
2022-01-21 23:49:25 -0500

    Documentation updates.

--- Experimental/2022/MattsToy2022/src/main/cpp/subsystems/DriveBase.cpp
+++ Experimental/2022/MattsToy2022/src/main/cpp/subsystems/DriveBase.cpp
@@ -29,10 +29,10 @@ DriveBase::DriveBase() {
  * Configures the encoders so that they will report back speeds and positions in
  * units that we generally care about (e.g., m/s and meters, rather than RPM and
  * fractions of a rotation).
+ *
+ * TODO: Check the math on all of this (at least empirically).
  */
 void DriveBase::ConfigureEncoders() {
-  // TODO: Check the math on all of this (at least empirically).
-
   // Compute the distance (in meters) for one rotation of the wheel, first.
   const units::meter_t wheelCircumference =
       WHEEL_DIAMETER_INCHES *  // Will auto-convert to meters! :-)
@@ -63,6 +63,13 @@ void DriveBase::ConfigureEncoders() {
   ResetEncoders();
 }
 
+/**
+ * Resets the encoders to 0.  (Similar to resetting the trip odometer in a car.)
+ *
+ * Warning: this can mess with odometry (once it's implemented), so please be
+ * very careful about invoking this, or else reset the odometry handling as
+ * well.
+ */
 void DriveBase::ResetEncoders() {
   m_leftFrontEncoder.SetPosition(0);
   m_rightFrontEncoder.SetPosition(0);
@@ -70,6 +77,13 @@ void DriveBase::ResetEncoders() {
   m_rightRearEncoder.SetPosition(0);
 }
 
+/**
+ * Enables/disables "coast mode".
+ *
+ * In "coast" mode", the motors will allow the wheels to roll freely when the
+ * power is 0; when it is disabled, they will effectively put on the brakes when
+ * power is 0.
+ */
 void DriveBase::SetCoastingEnabled(bool tf) {
   const rev::CANSparkMax::IdleMode mode =
       tf ? rev::CANSparkMax::IdleMode::kCoast
@@ -80,22 +94,41 @@ void DriveBase::SetCoastingEnabled(bool tf) {
   m_rightRear.SetIdleMode(mode);
 }
 
+/**
+ * Sets power via "tank mode", with the left and right sides controlled
+ * independently.
+ *
+ * @param leftSpeed  % power to be applied to left side (-1.0 to +1.0)
+ * @param rightSpeed % power to be applied to right side (-1.0 to +1.0)
+ */
 void DriveBase::TankDrive(double leftSpeed, double rightSpeed) {
   m_drive->TankDrive(leftSpeed, rightSpeed);
 }
 
+/** @return the current speed of the left side motors, in meters/sec. */
 units::meters_per_second_t DriveBase::GetLeftSpeed() {
   return units::meters_per_second_t(m_leftRearEncoder.GetVelocity());
 }
 
+/** @return the current speed of the right side motors, in meters/sec. */
 units::meters_per_second_t DriveBase::GetRightSpeed() {
   return units::meters_per_second_t(m_rightRearEncoder.GetVelocity());
 }
 
+/**
+ * @return the current distance traveled by the left side wheels, in meters.
+ *
+ * @see #ResetEncoders()
+ */
 units::meter_t DriveBase::GetLeftDistance() {
   return units::meter_t(m_leftRearEncoder.GetPosition());
 }
 
+/**
+ * @return the current distance traveled by the right side wheels, in meters.
+ *
+ * @see #ResetEncoders()
+ */
 units::meter_t DriveBase::GetRightDistance() {
   return units::meter_t(m_rightRearEncoder.GetPosition());
 }

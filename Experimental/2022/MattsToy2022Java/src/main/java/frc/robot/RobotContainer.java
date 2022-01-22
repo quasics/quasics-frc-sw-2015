49da50e6c2f3a872a66040cad97970a730489902
Matt Healy <mjhealy@users.noreply.github.com>
2022-01-21 19:04:18 -0500

    Cleaning up references to example subsystem/command.

--- Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/RobotContainer.java
+++ Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.TankDrive;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.DriveBase;
 import frc.robot.utils.DeadBandEnforcer;
 import frc.robot.Constants.OperatorInterface.LogitechGamePad;
@@ -26,11 +24,6 @@ import edu.wpi.first.wpilibj2.command.PrintCommand;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
   private final DriveBase driveBase = new DriveBase();
 
   /**

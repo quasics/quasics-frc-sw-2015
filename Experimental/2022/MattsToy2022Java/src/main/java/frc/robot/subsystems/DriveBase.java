51f535454c06cf0b7f5f284749b2e773b2b6c5a0
Matt Healy <mjhealy@users.noreply.github.com>
2022-01-25 20:35:11 -0500

    Beginning to add odometry/trajectory support. * Added gyro. * Added odometry calculations. * Added support functions for use with trajectories.

--- Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/subsystems/DriveBase.java
+++ Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/subsystems/DriveBase.java
@@ -9,6 +9,11 @@ import frc.robot.utils.BooleanSetter;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -18,8 +23,16 @@ public class DriveBase extends SubsystemBase {
   final private RelativeEncoder leftEncoder;
   final private RelativeEncoder rightEncoder;
 
+  final private MotorControllerGroup leftMotors;
+  final private MotorControllerGroup rightMotors;
+
   final private DifferentialDrive drive;
   final private BooleanSetter coastingEnabled;
+
+  // Assumes "Chip Select" jumper is set to CS0
+  final private ADXRS450_Gyro gyro = new ADXRS450_Gyro(edu.wpi.first.wpilibj.SPI.Port.kOnboardCS0);
+
+  final private DifferentialDriveOdometry odometry;
 
   /** Creates a new DriveBase. */
   public DriveBase() {
@@ -54,15 +67,15 @@ public class DriveBase extends SubsystemBase {
 
     /////////////////////////////////
     // Set up the differential drive.
-    MotorControllerGroup leftMotors = new MotorControllerGroup(leftFront, leftRear);
-    MotorControllerGroup rightMotors = new MotorControllerGroup(rightFront, rightRear);
+    leftMotors = new MotorControllerGroup(leftFront, leftRear);
+    rightMotors = new MotorControllerGroup(rightFront, rightRear);
 
     drive = new DifferentialDrive(leftMotors, rightMotors);
 
     ////////////////////////////////////////
     // Configure the encoders.
-    final double METERS_PER_INCH = 0.0254;
-    final double wheelCircumferenceMeters = (Constants.WHEEL_DIAMETER_INCHES * METERS_PER_INCH) * Math.PI;
+    final double wheelCircumferenceMeters = edu.wpi.first.math.util.Units
+        .inchesToMeters(Constants.WHEEL_DIAMETER_INCHES);
     System.out.println("Wheel circumference (m): " + wheelCircumferenceMeters);
 
     // Conversion factor from units in rotations (or RPM) to meters (or m/s).
@@ -80,6 +93,22 @@ public class DriveBase extends SubsystemBase {
     rightEncoder.setVelocityConversionFactor(velocityAdjustment);
 
     resetEncoders();
+
+    ////////////////////////////////////////
+    // Configure the gyro
+
+    // Gyro must be calibrated to initialize for use (generally immediately on
+    // start-up).
+    gyro.calibrate();
+
+    ////////////////////////////////////////
+    // Odometry setup.
+
+    // Notes:
+    // 1) This *must* be done after the encoders have been reset to 0.
+    // 2) We're building a "robot-oriented" view of the field (as in, our starting
+    // position will be treated as the origin), rather than a "field-oriented" view.
+    odometry = new DifferentialDriveOdometry(new Rotation2d(0), new Pose2d());
   }
 
   /**
@@ -115,6 +144,20 @@ public class DriveBase extends SubsystemBase {
   }
 
   /**
+   * @return the current speed for the left wheels (in meters/sec)
+   */
+  public double getLeftSpeed() {
+    return leftEncoder.getVelocity();
+  }
+
+  /**
+   * @return the current speed for the right wheels (in meters/sec)
+   */
+  public double getRightSpeed() {
+    return rightEncoder.getVelocity();
+  }
+
+  /**
    * Resets both the left and right encoders to 0.
    */
   public void resetEncoders() {
@@ -134,5 +177,48 @@ public class DriveBase extends SubsystemBase {
   // This method will be called once per scheduler run
   @Override
   public void periodic() {
+    updateOdometry();
   }
+
+  Rotation2d getGyroAngle() {
+    return gyro.getRotation2d();
+  }
+
+  //////////////////////////////////////////////////////////////////
+  // Trajectory-following support.
+
+  Pose2d GetPose() {
+    return odometry.getPoseMeters();
+  }
+
+  DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(getLeftSpeed(), getRightSpeed());
+  }
+
+  private void updateOdometry() {
+    // Get my gyro angle. We are negating the value because gyros return positive
+    // values as the robot turns clockwise. This is not standard convention that is
+    // used by the WPILib classes.
+    var gyroAngle = Rotation2d.fromDegrees(-gyro.getAngle());
+
+    // Update the pose
+    odometry.update(gyroAngle, getLeftEncoderPosition(), getRightEncoderPosition());
+  }
+
+  void ResetOdometry(Pose2d pose) {
+    resetEncoders();
+    odometry.resetPosition(pose, getGyroAngle());
+  }
+
+  void TankDriveVolts(double leftVolts, double rightVolts) {
+    leftMotors.setVoltage(leftVolts);
+    rightMotors.setVoltage(rightVolts);
+    drive.feed();
+  }
+
+  // TODO: Define these methods, once we have good data to work with.
+
+  // double GetTrackWidth() override {
+  // return kTrackWidthMeters;
+  // }
 }

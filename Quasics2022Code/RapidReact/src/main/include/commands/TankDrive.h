e2163eeff6d92a8447daaef2af2fe3bd1dd78340
Matt Healy <mjhealy@users.noreply.github.com>
2022-01-30 12:58:49 -0500

    Fixed broken (windows-specific) #include.
    Also added some documentation.

--- Quasics2022Code/RapidReact/src/main/include/commands/TankDrive.h
+++ Quasics2022Code/RapidReact/src/main/include/commands/TankDrive.h
@@ -5,23 +5,24 @@
 #pragma once
 
 #include <frc/Joystick.h>
-#include <subsystems\Drivebase.h>
 #include <frc2/command/CommandBase.h>
 #include <frc2/command/CommandHelper.h>
+#include <subsystems/Drivebase.h>
 
 /**
- * An example command.
- *
- * <p>Note that this extends CommandHelper, rather extending CommandBase
- * directly; this is crucially important, or else the decorator functions in
- * Command will *not* work!
+ * Command providing support for "tank drive" operations during teleop mode.
  */
-class TankDrive
-    : public frc2::CommandHelper<frc2::CommandBase, TankDrive> {
+class TankDrive : public frc2::CommandHelper<frc2::CommandBase, TankDrive> {
  public:
-  TankDrive(Drivebase* drivebase, frc::Joystick* driverStick);
+  /**
+   * Constructor.
+   *
+   * @param drivebase   pointer to the Drivebase subsystem being used
+   * @param driverStick pointer to the Logitech controller used by the driver
+   */
+  TankDrive(Drivebase *drivebase, frc::Joystick *driverStick);
 
- // Methods defined for Commands, which we're overriding.
+  // Methods defined for Commands, which we're overriding.
  public:
   void Initialize() override;
 
@@ -29,13 +30,19 @@ class TankDrive
 
   void End(bool interrupted) override;
 
- // Private utility functions
+  // Private utility functions
  private:
+  /**
+   * Updates the current left/right motor speeds on the drive base, in response
+   * to the joystick positions.
+   */
   void UpdateSpeeds();
 
- // Data members 
+  // Data members
  private:
-  Drivebase* m_drivebase;
-  frc::Joystick* m_driverStick;
+  /** Drive base we're working with. */
+  Drivebase *m_drivebase;
 
+  /** Logitech controller providing feedback for speeds. */
+  frc::Joystick *m_driverStick;
 };

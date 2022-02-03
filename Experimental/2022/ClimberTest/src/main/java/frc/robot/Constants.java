4b119b927021040724ee0f1973ca9177bf42ee60
Matt Healy <mjhealy@users.noreply.github.com>
2022-02-03 11:30:49 -0500

    Adding initial (untested) code for motor control.

--- Experimental/2022/ClimberTest/src/main/java/frc/robot/Constants.java
+++ Experimental/2022/ClimberTest/src/main/java/frc/robot/Constants.java
@@ -5,11 +5,20 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final class MotorIds {
+        public static final class SparkMax {
+            public static final int LEFT_CLIMBER_MOTOR_ID = 6;
+            public static final int RIGHT_CLIMBER_MOTOR_ID = 7;
+        }
+    }
+}
--- Experimental/2022/ClimberTest/src/main/java/frc/robot/subsystems/Climber.java
+++ Experimental/2022/ClimberTest/src/main/java/frc/robot/subsystems/Climber.java
@@ -4,7 +4,14 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 /**
  * Simple design for control of the 2022 climber hardware, which is expected to
@@ -22,21 +29,57 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * @see https://www.thethriftybot.com/bearings/Round-2-Pre-Order-Thrifty-Telescoping-Tube-Kit-p416413760
  */
 public class Climber extends SubsystemBase {
+
+  private enum Motion {
+    kNone, kExtending, kRetracting
+  }
+
   // TODO: Make this a non-arbitrary value. :-)
   public static final double MAX_EXTENSION_INCHES = 100.0;
 
+  private static final double EXTENDING_SPEED_PERCENT = 0.25;
+  private static final double RETRACTING_SPEED_PERCENT = 0.25;
+
+  private final CANSparkMax m_leftMotor = new CANSparkMax(Constants.MotorIds.SparkMax.LEFT_CLIMBER_MOTOR_ID,
+      MotorType.kBrushless);
+  private final CANSparkMax m_rightMotor = new CANSparkMax(Constants.MotorIds.SparkMax.RIGHT_CLIMBER_MOTOR_ID,
+      MotorType.kBrushless);
+
+  private final RelativeEncoder m_leftEncoder;
+  private final RelativeEncoder m_rightEncoder;
+
+  private final MotorControllerGroup m_motors;
+
+  private Motion m_currentMode = Motion.kNone;
+
   /** Creates a new Climber. */
   public Climber() {
+    // TODO: Figure out which of the motors actually *is* inverted (if any).
+    m_leftMotor.setInverted(true);
+    m_rightMotor.setInverted(false);
+
+    m_leftEncoder = m_leftMotor.getEncoder();
+    m_rightEncoder = m_rightMotor.getEncoder();
+
+    // TODO: Add code to convert encoder positions from revolutions to inches.
+    // (Though this may not be possible, since the cable will wrap around itself
+    // somewhat unpredicably: we may need to consider averaging, or else using some
+    // other type of sensor, such as Hall effect, to detect arm positions.)
+
+    m_motors = new MotorControllerGroup(m_leftMotor, m_rightMotor);
 
     resetPosition();
   }
 
-  public void stop() {
-    // TODO: Implement this method.
+  public void enableBrakingMode(boolean tf) {
+    IdleMode mode = tf ? IdleMode.kBrake : IdleMode.kCoast;
+    m_rightMotor.setIdleMode(mode);
+    m_rightMotor.setIdleMode(mode);
   }
 
   public void holdPosition() {
-    // TODO: Implement this method.
+    enableBrakingMode(true);
+    stop();
 
     /*
      * [Notes from discussion among coaches on 28Jan:]
@@ -64,24 +107,65 @@ public class Climber extends SubsystemBase {
   }
 
   private void resetPosition() {
-    // TODO: Implement this method.
+    m_leftEncoder.setPosition(0);
+    m_rightEncoder.setPosition(0);
   }
 
   public double getPosition() {
-    // TODO: Implement this method.
-    return 0;
+    return m_leftEncoder.getPosition();
   }
 
   public void extendArms() {
-    // TODO: Implement this method.
+    if (armsInBounds()) {
+      m_motors.set(EXTENDING_SPEED_PERCENT);
+      m_currentMode = Motion.kExtending;
+    }
   }
 
+  /**
+   * 
+   */
   public void retractArms() {
-    // TODO: Implement this method.
+    if (armsInBounds()) {
+      m_motors.set(RETRACTING_SPEED_PERCENT);
+      m_currentMode = Motion.kRetracting;
+    }
+  }
+
+  /**
+   * Stops the arms moving.
+   * 
+   * Note that this does *not* alter braking behavior: if the motors are set to
+   * "coast", then the arms will not be stopped from moving in response to
+   * external forces.
+   * 
+   * @see #holdPosition()
+   */
+  public void stop() {
+    m_motors.stopMotor();
+    m_currentMode = Motion.kNone;
+  }
+
+  /**
+   * @return true iff the motors are stopped (either via client request, or
+   *         because we've hit a soft limit)
+   */
+  public boolean isStopped() {
+    return (m_currentMode == Motion.kNone);
+  }
+
+  private boolean armsInBounds() {
+    // TODO: Add code to support "soft stops" on the motors.
+    // These decisions will be based on m_currentMode and either data from position,
+    // or via alternate signals (e.g., limit switches, Hall effect sensors, etc.).
+    return true;
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if (!armsInBounds()) {
+      stop();
+    }
   }
 }

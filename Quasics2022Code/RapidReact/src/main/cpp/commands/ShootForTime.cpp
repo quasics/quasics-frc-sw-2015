ba6e20a313c99c8e4492f7730e98fa1d31238d67
RoboticsMB <83793330+RoboticsMB@users.noreply.github.com>
2022-02-05 15:39:54 -0500

    Added a Shoot For time Command

---
+++ Quasics2022Code/RapidReact/src/main/cpp/commands/ShootForTime.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/ShootForTime.h"
+
+ShootForTime::ShootForTime() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void ShootForTime::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void ShootForTime::Execute() {}
+
+// Called once the command ends or is interrupted.
+void ShootForTime::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool ShootForTime::IsFinished() {
+  return false;
+}

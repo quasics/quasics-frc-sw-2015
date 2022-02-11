a9f5ca7359048ba97a4e35d1d5b361532c68f0ca
Matt Healy <mjhealy@users.noreply.github.com>
2022-02-11 16:35:42 -0500

    Documentation updates.

--- Quasics2022Code/RapidReact/src/main/cpp/subsystems/Drivebase.cpp
+++ Quasics2022Code/RapidReact/src/main/cpp/subsystems/Drivebase.cpp
@@ -3,15 +3,15 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "subsystems/Drivebase.h"
+
 #include <rev/CANSparkMax.h>
+
 #include <iostream>
 #include <wpi/numbers>
 
 Drivebase::Drivebase() {
   m_rightFront.SetInverted(true);
   m_rightBack.SetInverted(true);
-
-
 
   m_leftSide.reset(new frc::MotorControllerGroup(m_leftFront, m_leftBack));
   m_rightSide.reset(new frc::MotorControllerGroup(m_rightFront, m_rightBack));
@@ -24,15 +24,25 @@ Drivebase::Drivebase() {
 // This method will be called once per scheduler run
 
 void Drivebase::ConfigureEncoders() {
+  // Calculate wheel circumference (distance travelled per wheel revolution).
   const units::meter_t wheelCircumference = WHEEL_DIAMETER * wpi::numbers::pi;
-  const units::meter_t gearingConversion = wheelCircumference / DRIVEBASE_GEAR_RATIO;
+
+  // Compute distance traveled per rotation of the motor.
+  const units::meter_t gearingConversion =
+      wheelCircumference / DRIVEBASE_GEAR_RATIO;
+
+  // Compute conversion factor (used to change "(motor) RPM" to "m/sec").
   const units::meter_t velocityCorrection = gearingConversion / 60;
 
+  // Update encoders so that they will report distance as meters traveled,
+  // rather than rotations.
   m_leftFrontEncoder.SetPositionConversionFactor(gearingConversion.value());
   m_leftBackEncoder.SetPositionConversionFactor(gearingConversion.value());
   m_rightFrontEncoder.SetPositionConversionFactor(gearingConversion.value());
   m_rightBackEncoder.SetPositionConversionFactor(gearingConversion.value());
 
+  // Update encoders so that they will report velocity as m/sec, rather than
+  // RPM.
   m_leftFrontEncoder.SetVelocityConversionFactor(velocityCorrection.value());
   m_leftBackEncoder.SetVelocityConversionFactor(velocityCorrection.value());
   m_rightFrontEncoder.SetVelocityConversionFactor(velocityCorrection.value());
@@ -40,36 +50,41 @@ void Drivebase::ConfigureEncoders() {
 
   ResetEncoders();
 }
-void Drivebase::Periodic() {
-
-}
-//Todo - Write code for the following 4 commands
-
-void Drivebase::SetMotorPower(double leftPower, double rightPower) {
-  m_drive->TankDrive(leftPower, rightPower);
-}
-
-units::meter_t  Drivebase::GetLeftDistance() {
-  //TODO: the returned value is in revolution need to fix to meters
-  return units::meter_t (m_leftFrontEncoder.GetPosition());
-}
-
-units::meter_t  Drivebase::GetRightDistance() {
-  //TODO: the returned value is in revolution need to fix to meters
-  return units::meter_t (m_rightFrontEncoder.GetPosition());
-}
-
-units::meters_per_second_t Drivebase::GetLeftVelocity() {
-  return units::meters_per_second_t(m_leftFrontEncoder.GetVelocity());
-}
-
-units::meters_per_second_t Drivebase::GetRightVelocity() {
-  return units::meters_per_second_t(m_rightFrontEncoder.GetVelocity());
-}
 
 void Drivebase::ResetEncoders() {
   m_leftFrontEncoder.SetPosition(0);
   m_rightFrontEncoder.SetPosition(0);
   m_leftBackEncoder.SetPosition(0);
   m_rightBackEncoder.SetPosition(0);
+}
+
+void Drivebase::Periodic() {
+}
+
+void Drivebase::SetMotorPower(double leftPower, double rightPower) {
+  m_drive->TankDrive(leftPower, rightPower);
+}
+
+units::meter_t Drivebase::GetLeftDistance() {
+  // Note that the conversion factor configured earlier means that we're getting
+  // position in meters.
+  return units::meter_t(m_leftFrontEncoder.GetPosition());
+}
+
+units::meter_t Drivebase::GetRightDistance() {
+  // Note that the conversion factor configured earlier means that we're getting
+  // position in meters.
+  return units::meter_t(m_rightFrontEncoder.GetPosition());
+}
+
+units::meters_per_second_t Drivebase::GetLeftVelocity() {
+  // Note that the conversion factor configured earlier means that we're getting
+  // velocity in m/sec.
+  return units::meters_per_second_t(m_leftFrontEncoder.GetVelocity());
+}
+
+units::meters_per_second_t Drivebase::GetRightVelocity() {
+  // Note that the conversion factor configured earlier means that we're getting
+  // velocity in m/sec.
+  return units::meters_per_second_t(m_rightFrontEncoder.GetVelocity());
 }

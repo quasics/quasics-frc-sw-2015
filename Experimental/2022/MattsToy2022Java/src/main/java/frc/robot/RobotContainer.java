3be802c91cd13c96d7e00eb2b1993bb507023ef2
Matt Healy <mjhealy@users.noreply.github.com>
2022-02-11 16:43:25 -0500

    Formatting updates.

--- Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/RobotContainer.java
+++ Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/RobotContainer.java
@@ -22,12 +22,10 @@ import edu.wpi.first.wpilibj2.command.PrintCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and button mappings) should be declared here.
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
 

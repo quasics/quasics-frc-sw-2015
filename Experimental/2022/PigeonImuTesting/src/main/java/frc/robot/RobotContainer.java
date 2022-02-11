23b318bd34c9e0b233708239f0d0f626d8c9dbb2
quasics-driver <47063590+quasics-driver@users.noreply.github.com>
2022-02-10 19:28:30 -0500

    Adding "Reset" command. (mjh)

--- Experimental/2022/PigeonImuTesting/src/main/java/frc/robot/RobotContainer.java
+++ Experimental/2022/PigeonImuTesting/src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,8 @@ package frc.robot;
 import frc.robot.subsystems.Navigation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -26,6 +28,7 @@ public class RobotContainer {
    */
   public RobotContainer() {
     // Configure the button bindings
+    SmartDashboard.putData("Reset", new InstantCommand(()->{m_exampleSubsystem.getGyro().reset();}, m_exampleSubsystem));
   }
 
   /**

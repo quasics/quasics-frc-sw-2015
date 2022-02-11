296fc4bd24833c8b4fcdab50ad801ce8e84b6b3e
Matt Healy <mjhealy@users.noreply.github.com>
2022-02-11 15:51:56 -0500

    Documentation updates.

--- Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/utils/SpeedModifier.java
+++ Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/utils/SpeedModifier.java
@@ -5,9 +5,16 @@
 package frc.robot.utils;
 
 /**
- * General interface for things that will manipulate motor speeds specified by
- * the driver joysticks.
+ * General interface for things that will manipulate motor speeds (e.g., as
+ * specified by the driver joysticks).
  */
 public interface SpeedModifier {
+  /**
+   * Takes the specified input %age, and applies some type of modifier to it
+   * (e.g., for "dead band" handling, or speed caps, etc.).
+   * 
+   * @param inputPercentage input %age of motor speed, to be adjusted
+   * @return the modified value for the speed to be applied to a motor
+   */
   double adjustSpeed(double inputPercentage);
 }

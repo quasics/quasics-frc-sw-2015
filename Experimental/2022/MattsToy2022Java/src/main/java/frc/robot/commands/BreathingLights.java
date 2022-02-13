e22cfe9d2d8acfadbf9c17688d07ed36ff061c85
Matt Healy <mjhealy@users.noreply.github.com>
2022-02-12 20:26:40 -0500

    Doc updates; switch to using "toWpiColor" vs. direct calculation.

--- Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/commands/BreathingLights.java
+++ Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/commands/BreathingLights.java
@@ -9,70 +9,78 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Lighting;
 
 public class BreathingLights extends CommandBase {
+  /** Reference to the Lighting subsystem. */
   final private Lighting m_lighting;
+  /** The base color we're using. */
   final private Lighting.StockColor m_color;
+  /** Intensity change to be used on each "tick" (50x/sec). */
   final private double m_step;
 
+  /** Intensity used for the last "tick". */
   private double m_currentIntensity = 0;
+  /** Iff true, the intensity is increasing on the next "tick". */
   private boolean m_rising = true;
 
-  /** Creates a new BreathingLights. */
+  /**
+   * Constructs a command using the specified color and a 1% change on each "tick"
+   * (50x/sec).
+   * 
+   * @param lighting the lighting subsystem
+   * @param color    the base color for the lights (adjusted by the intensity)
+   * @param step     intensity change to be used on each "tick" (50x/sec)
+   */
   public BreathingLights(Lighting lighting, Lighting.StockColor color) {
     this(lighting, color, 0.01);
   }
 
   /**
+   * Constructor.
    * 
-   * @param lighting
-   * @param color
+   * @param lighting the lighting subsystem
+   * @param color    the base color for the lights (adjusted by the intensity)
+   * @param step     intensity change to be used on each "tick" (50x/sec)
    */
   public BreathingLights(Lighting lighting, Lighting.StockColor color, double step) {
     this.m_lighting = lighting;
     this.m_color = color;
     this.m_step = step;
 
-    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_lighting);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    // Start out with lights fully off.
-    this.m_currentIntensity = 0;
-    this.m_rising = true;
+    // Start out with lights fully off, and start increasing on the first "tick".
+    m_currentIntensity = 0;
+    m_rising = true;
+
     m_lighting.SetStripColor(Lighting.StockColor.Black);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // if (!timer.hasElapsed(0.1)) {
-    // return;
-    // }
-
     // Calculate new intensity (and update direction for next pass, if needed.)
-    if (this.m_rising) {
-      this.m_currentIntensity += this.m_step;
-      if (this.m_currentIntensity >= 1.0) {
+    if (m_rising) {
+      m_currentIntensity += m_step;
+      if (m_currentIntensity >= 1.0) {
         // Start falling on next round
-        this.m_rising = false;
-        this.m_currentIntensity = 1.0;
+        m_rising = false;
+        m_currentIntensity = 1.0;
       }
     } else {
-      this.m_currentIntensity -= this.m_step;
-      if (this.m_currentIntensity <= 0) {
+      m_currentIntensity -= m_step;
+      if (m_currentIntensity <= 0) {
         // Start rising on next round
-        this.m_rising = true;
-        this.m_currentIntensity = 0;
+        m_rising = true;
+        m_currentIntensity = 0;
       }
     }
 
     // Update lights, based on current intensity.
-    final int r = (int) (m_currentIntensity * m_color.getR());
-    final int g = (int) (m_currentIntensity * m_color.getG());
-    final int b = (int) (m_currentIntensity * m_color.getB());
-    m_lighting.SetStripColor(r, g, b);
+    var useColor = m_color.toWpiColor(m_currentIntensity);
+    m_lighting.SetStripColor(useColor);
   }
 
   // Called once the command ends or is interrupted.

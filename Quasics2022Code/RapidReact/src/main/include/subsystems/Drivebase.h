49463fbc0949793942effd3e9020bd09d85c2551
Matt Healy <mjhealy@users.noreply.github.com>
2022-02-13 16:58:06 -0500

    Documentation updates.

--- Quasics2022Code/RapidReact/src/main/include/subsystems/Drivebase.h
+++ Quasics2022Code/RapidReact/src/main/include/subsystems/Drivebase.h
@@ -13,31 +13,60 @@
 #include "units/length.h"
 #include "units/velocity.h"
 
+/**
+ * Drive base subsystem, used for movement/navigation.
+ */
 class Drivebase : public frc2::SubsystemBase {
  public:
+  // Constructor.
   Drivebase();
 
-  void ConfigureEncoders();
+  /**
+   * Sets the speeds for the left and right motors to the specified percentages
+   * (-1.0 == full reverse, +1.0 == full forward, 0 = stopped).
+   *
+   * @param leftPower  desired % power for the left motors
+   * @param rightPower desired % power for the right motors
+   */
+  void SetMotorPower(double leftPower, double rightPower);
 
-  void Periodic() override;
-
+  /** Convenience method to stop the drive base. */
   void Stop() {
     SetMotorPower(0, 0);
   }
 
-  void SetMotorPower(double leftPower, double rightPower);
-
+  /**
+   * Returns the distance traveled by the left wheels since they were last
+   * reset.
+   */
   units::meter_t GetLeftDistance();
 
+  /**
+   * Returns the distance traveled by the right wheels since they were last
+   * reset.
+   */
   units::meter_t GetRightDistance();
 
+  /** Returns the current speed of the left-side wheels. */
   units::meters_per_second_t GetLeftVelocity();
 
+  /** Returns the current speed of the right-side wheels. */
   units::meters_per_second_t GetRightVelocity();
 
+  /** Resets the encoders used to report distances for left/right wheels. */
   void ResetEncoders();
 
+  // Standard functions for subsystems.
+ public:
+  void Periodic() override;
+
+  // Internal functions.
  private:
+  /** Sets up the encoders to use meaningful units for distance/speed. */
+  void ConfigureEncoders();
+
+ private:
+  // Individual motors.
   rev::CANSparkMax m_leftFront{MotorIds::SparkMax::LEFT_FRONT_DRIVE_MOTOR_ID,
                                rev::CANSparkMax::MotorType::kBrushless};
   rev::CANSparkMax m_rightFront{MotorIds::SparkMax::RIGHT_FRONT_DRIVE_MOTOR_ID,
@@ -53,14 +82,15 @@ class Drivebase : public frc2::SubsystemBase {
   rev::SparkMaxRelativeEncoder m_leftBackEncoder = m_leftBack.GetEncoder();
   rev::SparkMaxRelativeEncoder m_rightBackEncoder = m_rightBack.GetEncoder();
 
+  // Motor controller groups, used to "gang together" motors for a given side.
+  //
+  // Note that we need to use pointers (rather than direct creation here), since
+  // any motor inversions must be configured prior to adding them to a motor
+  // controller group.
   std::unique_ptr<frc::MotorControllerGroup> m_leftSide;
   std::unique_ptr<frc::MotorControllerGroup> m_rightSide;
 
+  // Standard WPILib differential drive, used to control left/right motors as a
+  // set (e.g., under tank or arcade drive).
   std::unique_ptr<frc::DifferentialDrive> m_drive;
 };
-
-/**
- * Will be called periodically whenever the CommandScheduler runs.
- */
-// Components (e.g. motor controllers and sensors) should generally be
-// declared private and exposed only through public methods.

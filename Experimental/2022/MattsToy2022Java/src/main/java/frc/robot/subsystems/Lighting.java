69198ad0caa13e67f2c39f3852cb8d23f9a5b812
Matt Healy <mjhealy@users.noreply.github.com>
2022-02-12 20:05:01 -0500

    Doc updates, code refactoring/cleanup.

--- Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/subsystems/Lighting.java
+++ Experimental/2022/MattsToy2022Java/src/main/java/frc/robot/subsystems/Lighting.java
@@ -69,16 +69,6 @@ public class Lighting extends SubsystemBase {
     m_led.start();
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
-
   /** Helper interface to use in populating the data for each LED on the strip. */
   public interface ColorFunctor {
     /**
@@ -87,6 +77,12 @@ public class Lighting extends SubsystemBase {
     public Color getColorForLed(int position);
   }
 
+  /**
+   * Sets the color for each LED in the strip, using the specified function to
+   * generate the values for each position.
+   * 
+   * @param function Function generating the color for each LED
+   */
   public void SetStripColor(ColorFunctor function) {
     for (var i = 0; i < m_ledBuffer.getLength(); i++) {
       m_ledBuffer.setLED(i, function.getColorForLed(i));
@@ -95,34 +91,59 @@ public class Lighting extends SubsystemBase {
     m_led.setData(m_ledBuffer);
   }
 
-  public void SetStripColor(StockColor color) {
+  /**
+   * Convenience function: sets the strip to a solid color.
+   * 
+   * Note: unlike with various other APIs, the component values for WPI's version
+   * of color specification are all as percentages (0.0-1.0).
+   * 
+   * @param color the color to make all of the lights in the strip
+   */
+  public void SetStripColor(Color color) {
     // Defines a "lambda" function that will be used to fulfill the
     // requirements of the ColorFunctor type. (It will return the
     // same color for each position in the strip.)
-    var useColor = color.toWpiColor();
-    ColorFunctor function = (var position) -> useColor;
+    ColorFunctor function = (var position) -> color;
 
     // Uses the lambda to set the color for the full strip.
-    SetStripColor((var position) -> useColor);
+    SetStripColor(function);
   }
 
+  /**
+   * Convenience function: sets the strip to a solid color from the stock set.
+   * 
+   * @param color the color to make all of the lights in the strip
+   */
+  public void SetStripColor(StockColor color) {
+    SetStripColor(color.toWpiColor());
+  }
+
+  /**
+   * Convenience function: sets the strip to a solid color, specified as an RGB
+   * tripet of values (each [0..255]).
+   * 
+   * @param red   red component (0-255)
+   * @param green green component (0-255)
+   * @param blue  blue component (0-255)
+   */
   public void SetStripColor(int red, int green, int blue) {
-    System.out.println("Setting lights: " + red + "/" + green + "/" + blue);
-    final var fixedColor = new edu.wpi.first.wpilibj.util.Color(red / 255.0, green / 255.0, blue / 255.0);
-    for (var i = 0; i < m_ledBuffer.getLength(); ++i) {
-      m_ledBuffer.setLED(i, fixedColor);
-    }
+    // Note: WPI expects color component values to be percentages in the range
+    // [0.0-1.0], so we need to convert to the right scale.
+    var color = new Color(
+        red / 255.0,
+        green / 255.0,
+        blue / 255.0);
 
-    // Set the data
-    m_led.setData(m_ledBuffer);
+    SetStripColor(color);
+  }
 
-    // // Defines a "lambda" function that will be used to fulfill the
-    // // requirements of the ColorFunctor type. (It will return the
-    // // same color for each position in the strip.)
-    // final var fixedColor2 = new edu.wpi.first.wpilibj.util.Color(red, green,
-    // blue);
-    // ColorFunctor function = (var position) -> fixedColor2;
-    // // Uses the lambda to set the color for the full strip.
-    // SetStripColor(function);
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
   }
 }

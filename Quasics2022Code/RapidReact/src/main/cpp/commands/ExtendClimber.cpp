ee5cf97a62780460464462a861671bc4ece6feca
RoboticsMB <83793330+RoboticsMB@users.noreply.github.com>
2022-02-15 18:47:33 -0500

    Created an ExtendClimber command

---
+++ Quasics2022Code/RapidReact/src/main/cpp/commands/ExtendClimber.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/ExtendClimber.h"
+
+ExtendClimber::ExtendClimber(Climber* climber) : m_climber(climber) {
+  // Use addRequirements() here to declare subsystem dependencies.
+
+  AddRequirements(m_climber);
+}
+
+// Called when the command is initially scheduled.
+void ExtendClimber::Initialize() {
+  m_climber->StartExtending();
+}
+
+// Called once the command ends or is interrupted.
+void ExtendClimber::End(bool interrupted) {
+  m_climber->EnableBraking(true);
+}
+
+// Returns true when the command should end.
+bool ExtendClimber::IsFinished() {
+  return m_climber->IsFullyExtended();
+}

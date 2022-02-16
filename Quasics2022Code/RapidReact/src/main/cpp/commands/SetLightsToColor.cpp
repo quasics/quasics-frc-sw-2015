bf04fde642a9bdc37d9a0e652e3d7f279906eaf7
RoboticsMB <83793330+RoboticsMB@users.noreply.github.com>
2022-02-15 21:00:51 -0500

    Created a command to set all lights to a color and populated it

---
+++ Quasics2022Code/RapidReact/src/main/cpp/commands/SetLightsToColor.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/SetLightsToColor.h"
+
+SetLightsToColor::SetLightsToColor(Lighting* lighting) : m_lighting(lighting) {
+  // Use addRequirements() here to declare subsystem dependencies.
+  AddRequirements(m_lighting);
+}
+
+// Called when the command is initially scheduled.
+void SetLightsToColor::Initialize() {
+  m_lighting->SetAllToColor(255, 0, 0);
+}
---
+++ Quasics2022Code/RapidReact/src/main/include/commands/SetLightsToColor.h
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+
+#include "subsystems/Lighting.h"
+
+/**
+ * An example command.
+ *
+ * <p>Note that this extends CommandHelper, rather extending CommandBase
+ * directly; this is crucially important, or else the decorator functions in
+ * Command will *not* work!
+ */
+class SetLightsToColor
+    : public frc2::CommandHelper<frc2::CommandBase, SetLightsToColor> {
+ public:
+  SetLightsToColor(Lighting* lighting);
+
+  void Initialize() override;
+
+ private:
+  Lighting* m_lighting;
+};

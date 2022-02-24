938b65a0914982d6f635b7b7c2a0dd2f58bba990
RoboticsMB <83793330+RoboticsMB@users.noreply.github.com>
2022-02-24 18:59:06 -0500

    Created and populated commands for extend and retract intake

---
+++ Quasics2022Code/RapidReact/src/main/cpp/commands/ExtendIntake.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/ExtendIntake.h"
+
+ExtendIntake::ExtendIntake(IntakeDeployment* IntakeDeployment, double speed)
+    : m_intakeDeployment(IntakeDeployment), intakeSpeed(speed) {
+  // Use addRequirements() here to declare subsystem dependencies.
+
+  AddRequirements(m_intakeDeployment);
+}
+
+// Called when the command is initially scheduled.
+void ExtendIntake::Initialize() {
+  m_intakeDeployment->SetMotorSpeed(intakeSpeed);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void ExtendIntake::Execute() {
+  m_intakeDeployment->SetMotorSpeed(intakeSpeed);
+}
+
+// Called once the command ends or is interrupted.
+void ExtendIntake::End(bool interrupted) {
+}
+
+// Returns true when the command should end.
+bool ExtendIntake::IsFinished() {
+  return false;
+}
---
+++ Quasics2022Code/RapidReact/src/main/cpp/commands/RetractIntake.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/RetractIntake.h"
+
+RetractIntake::RetractIntake(IntakeDeployment* intake, double speed)
+    : m_intakeDeployment(intake), intakeSpeed(speed) {
+  // Use addRequirements() here to declare subsystem dependencies.
+
+  AddRequirements(m_intakeDeployment);
+}
+
+// Called when the command is initially scheduled.
+void RetractIntake::Initialize() {
+  m_intakeDeployment->SetMotorSpeed(-1 * intakeSpeed);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void RetractIntake::Execute() {
+  m_intakeDeployment->SetMotorSpeed(-1 * intakeSpeed);
+}
+
+// Called once the command ends or is interrupted.
+void RetractIntake::End(bool interrupted) {
+}
+
+// Returns true when the command should end.
+bool RetractIntake::IsFinished() {
+  return false;
+}

6f18bfe2ae588065adae9872fda21d9b040f7def
RoboticsMB <83793330+RoboticsMB@users.noreply.github.com>
2022-07-07 19:10:10 -0400

    Created a Conveyor Tuning Command

---
+++ Quasics2022Code/RapidReact/src/main/cpp/commands/ConveyorTuningCommand.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/ConveyorTuningCommand.h"
+
+ConveyorTuningCommand::ConveyorTuningCommand() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void ConveyorTuningCommand::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void ConveyorTuningCommand::Execute() {}
+
+// Called once the command ends or is interrupted.
+void ConveyorTuningCommand::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool ConveyorTuningCommand::IsFinished() {
+  return false;
+}

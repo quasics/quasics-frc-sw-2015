0218475efc443bceb28cdf9255f288da9b18f5a6
RoboticsMB <83793330+RoboticsMB@users.noreply.github.com>
2022-07-07 19:34:48 -0400

    Created a ExtendOneClimberArm Command

---
+++ Quasics2022Code/RapidReact/src/main/cpp/commands/ExtendOneClimberArm.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/ExtendOneClimberArm.h"
+
+ExtendOneClimberArm::ExtendOneClimberArm() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void ExtendOneClimberArm::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void ExtendOneClimberArm::Execute() {}
+
+// Called once the command ends or is interrupted.
+void ExtendOneClimberArm::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool ExtendOneClimberArm::IsFinished() {
+  return false;
+}

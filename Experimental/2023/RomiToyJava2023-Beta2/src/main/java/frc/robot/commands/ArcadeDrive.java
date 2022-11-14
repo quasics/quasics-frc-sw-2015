2c2255a0bde6f2c1fd6db14b305fb07a8b993806
Matt Healy <mjhealy@users.noreply.github.com>
2022-11-14 13:45:05 -0500

    Removing trivial implementation duplicating defaults.

--- Experimental/2023/RomiToyJava2023-Beta2/src/main/java/frc/robot/commands/ArcadeDrive.java
+++ Experimental/2023/RomiToyJava2023-Beta2/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -31,23 +31,9 @@ public class ArcadeDrive extends CommandBase {
     addRequirements(drivetrain);
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     m_drivetrain.arcadeDrive(m_xaxisSpeedSupplier.get(), m_zaxisRotateSupplier.get());
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
   }
 }

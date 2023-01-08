d48d86c5c020116b7951e4d8ad08fa871a2f5ba0
Matt Healy <mjhealy@users.noreply.github.com>
2023-01-08 09:21:44 -0500

    Adding deadband generators to the SpeedModifier interface.

--- Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/utils/SpeedModifier.java
+++ Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/utils/SpeedModifier.java
@@ -7,6 +7,9 @@ package frc.robot.utils;
 /**
  * General interface for things that will manipulate motor speeds (e.g., as
  * specified by the driver joysticks).
+ * 
+ * Note: I expect that this interface will often just be implemented via a
+ * lambda, or by using convenience methods defined on the interface.
  */
 public interface SpeedModifier {
   /**
@@ -17,4 +20,56 @@ public interface SpeedModifier {
    * @return the modified value for the speed to be applied to a motor
    */
   double adjustSpeed(double inputPercentage);
+
+  ////////////////////////////////////////////////////////////////////////
+  // Convenience functions, to create commont types of SpeedControllers
+
+  /**
+   * Generates a speed modifier that implements a "deadband", where if
+   * lowValue < inputPercentage < highValue, then we'll treat it as "don't move";
+   * otherwise, we'll just return the input speed.
+   * 
+   * This is meant to compensate for things like a joystick not being fully
+   * calibrated, so that it will appear to be reporting a non-zero value when the
+   * driver isn't actually pushing the stick in one direction or another.
+   * 
+   * @param lowValue  the lower limit for the deadband (e.g., -0.05)
+   * @param highValue the upper limit for the deadband (e.g., -0.05)
+   * @return a SpeedModifier that handles deadband computations
+   * 
+   * @see https://en.wikipedia.org/wiki/Deadband
+   */
+  static SpeedModifier generateDeadbandSpeedModifier(double lowValue, double highValue) {
+    // Handle cases where folks accidentally swap the low and high range (e.g.,
+    // lowValue = +0.08 and highValue = -0.05). This way, we'll always use the
+    // lesser value for the low end, etc.
+    final double sanityCheckedLowValue = (lowValue < highValue ? lowValue : highValue);
+    final double sanityCheckedHighValue = (highValue > lowValue ? highValue : lowValue);
+
+    return (double inputPercentage) -> {
+      // Does the input percentage fall into the deadband?
+      if (inputPercentage > sanityCheckedLowValue
+          && inputPercentage < sanityCheckedHighValue) {
+        return 0;
+      }
+
+      // OK, it's safe to use the input as the speed.
+      return inputPercentage;
+    };
+  }
+
+  /**
+   * Generates a speed modifier that handles a "deadband", where if
+   * Math.abs(input) < Math.abs(deadband), then we'll treat it as "don't move";
+   * otherwise, we'll just return the input speed.
+   * 
+   * @param deadbandValue the value (+/-) for which an input speed should be
+   *                      treated as effectively 0
+   * @return a SpeedModifier that handles deadband computations
+   * 
+   * @see https://en.wikipedia.org/wiki/Deadband
+   */
+  static SpeedModifier generateDeadbandSpeedModifier(double deadbandValue) {
+    return generateDeadbandSpeedModifier(-Math.abs(deadbandValue), Math.abs(deadbandValue));
+  }
 }

9930fccce5842ea1f3440ab56db6c2e4651f8951
Matt Healy <mjhealy@users.noreply.github.com>
2023-01-13 17:37:24 -0500

    Fixed incorrect drive base call (copy/paste error)

--- Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/commands/TankDrive.java
+++ Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/commands/TankDrive.java
@@ -38,7 +38,7 @@ public class TankDrive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_driveBase.arcadeDrive(m_leftSpeedSupplier.get(), m_rightSpeedSupplier.get());
+    m_driveBase.tankDrive(m_leftSpeedSupplier.get(), m_rightSpeedSupplier.get());
   }
 
   // Called once the command ends or is interrupted.

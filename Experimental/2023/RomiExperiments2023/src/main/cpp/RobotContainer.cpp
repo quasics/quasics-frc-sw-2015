4e81b10c6770842a3db5a6607e6b8f375bfddcb0
RoboticsMB <83793330+RoboticsMB@users.noreply.github.com>
2023-01-21 15:33:32 -0500

    made the self balacning command a default autonomous command for ROMI

--- Experimental/2023/RomiExperiments2023/src/main/cpp/RobotContainer.cpp
+++ Experimental/2023/RomiExperiments2023/src/main/cpp/RobotContainer.cpp
@@ -8,28 +8,42 @@
 
 #include "commands/Autos.h"
 #include "commands/ExampleCommand.h"
+#include "commands/SelfBalancing.h"
 
-RobotContainer::RobotContainer() {
+RobotContainer::RobotContainer()
+{
   // Initialize all of your commands and subsystems here
 
   // Configure the button bindings
   ConfigureBindings();
+  // SelfBalancing SelfBalancing(&m_driveTrain);
+  //.SetDefaultCommand(SelfBalancing);
 }
 
-void RobotContainer::ConfigureBindings() {
+void RobotContainer::ConfigureBindings()
+{
   // Configure your trigger bindings here
+  // SelfBalancing selfBalancer(&m_driveTrain);
+  // m_driveTrain.SetDefaultCommand(selfBalancer);
 
   // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-  frc2::Trigger([this] {
-    return m_subsystem.ExampleCondition();
-  }).OnTrue(ExampleCommand(&m_subsystem).ToPtr());
+  frc2::Trigger([this]
+                { return m_subsystem.ExampleCondition(); })
+      .OnTrue(ExampleCommand(&m_subsystem).ToPtr());
 
   // Schedule `ExampleMethodCommand` when the Xbox controller's B button is
   // pressed, cancelling on release.
   m_driverController.B().WhileTrue(m_subsystem.ExampleMethodCommand());
 }
 
-frc2::CommandPtr RobotContainer::GetAutonomousCommand() {
+frc2::CommandPtr RobotContainer::GetAutonomousCommand()
+{
   // An example command will be run in autonomous
-  return autos::ExampleAuto(&m_subsystem);
+  // return autos::ExampleAuto(&m_subsystem);
+  // std::unique_ptr<frc2::CommandBase> ptr(new SelfBalancing(&m_driveTrain));
+  return frc2::CommandPtr(                // Build a CommandPtr object from....
+    std::unique_ptr<frc2::CommandBase>(   // a std::unique_ptr to CommandBase, which wraps....
+      new SelfBalancing(&m_driveTrain)    // the command we want to have executed.
+    )
+  );
 }

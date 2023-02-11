e716cf9d96e89a7c386125731d524f214b87ee28
Matt Healy <mjhealy@users.noreply.github.com>
2023-02-11 17:56:31 -0500

    Added enumeration for alliance and robot position.

--- Experimental/2023/MattsToys/MattsToy2023/src/main/java/frc/robot/Constants.java
+++ Experimental/2023/MattsToys/MattsToy2023/src/main/java/frc/robot/Constants.java
@@ -5,14 +5,12 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants. This class should not be used for any other
- * purpose. All constants should be declared globally (i.e. "public static
- * final"). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. "public static final"). Do not put anything functional in this class.
  *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the constants are needed, to reduce verbosity.
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
  */
 public final class Constants {
 
@@ -24,19 +22,22 @@ public final class Constants {
   public static final double TRACK_WIDTH_INCHES_SALLY = 22.0;
 
   // TODO(mjh): Confirm this value for Mae....
-  public static final double TRACK_WIDTH_INCHES_MAE = 47.134344149315914763; // 2021 robot's width, from in-code
-  // constants (which are in meters)
+  /** 2021 robot's width, from in-code constants (which are in meters) */
+  public static final double TRACK_WIDTH_INCHES_MAE = 47.134344149315914763;
 
   // TODO(mjh): Confirm this for Nike
   public static final double TRACK_WIDTH_INCHES_NIKE = TRACK_WIDTH_INCHES_MAE;
 
   public static final double WHEEL_DIAMETER_INCHES = 6.0;
 
-  public static final double DRIVE_BASE_GEAR_RATIO_NIKE = 10.71; ///< Gear ratio for 2017 robot, kept as spare drive base
-  public static final double DRIVE_BASE_GEAR_RATIO_MAE = 10.71;  ///< 2020/2021 robot gear ratio
-  public static final double DRIVE_BASE_GEAR_RATIO_SALLY = 8.45; ///< 2022 robot gear rato (swapped in new gearbox vs. 10.71 in KoP)
-  // TODO: Confirm gear ratio on 2023 robot
-  public static final double DRIVE_BASE_GEAR_RATIO_2023 = 8.45;  ///< 2023 robot gear ratio (KoP gearing is 8.45:1)
+  /** Gear ratio for 2017 robot, kept as spare drive base. */
+  public static final double DRIVE_BASE_GEAR_RATIO_NIKE = 10.71;
+  /** 2020/2021 robot gear ratio. */
+  public static final double DRIVE_BASE_GEAR_RATIO_MAE = 10.71;
+  /** 2022 robot gear rato (swapped in new gearbox vs. 10.71 in KoP) */
+  public static final double DRIVE_BASE_GEAR_RATIO_SALLY = 8.45;
+  /** 2023 robot gear ratio (KoP gearing is 8.45:1 this year) */
+  public static final double DRIVE_BASE_GEAR_RATIO_2023 = 8.45;
 
   public static final double DRIVE_BASE_GEAR_RATIO = DRIVE_BASE_GEAR_RATIO_SALLY;
 
@@ -69,18 +70,68 @@ public final class Constants {
 
   /**
    * Constants associated with "dead zones" on joysticks.
-   * 
+   *
    * @see https://en.wikipedia.org/wiki/Deadband
    */
   public static final class Deadbands {
-    /**
-     * Dead band value to be used for deciding when the driver joysticks are at
-     * neutral.
-     */
+    /** Dead band value to be used for deciding when the driver joysticks are at neutral. */
     public static final double DRIVING = 0.055;
   }
 
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  public enum Alliance {
+    Red,
+    Blue
+  }
+
+  public enum RobotPosition {
+    eRed1(Alliance.Red, 1),
+    eRed2(Alliance.Red, 2),
+    eRed3(Alliance.Red, 3),
+    eBlue1(Alliance.Blue, 1),
+    eBlue2(Alliance.Blue, 2),
+    eBlue3(Alliance.Blue, 3);
+
+    RobotPosition(Alliance alliance, int position) {
+      this.m_pos = position;
+      this.m_alliance = alliance;
+    }
+
+    public int position() {
+      return m_pos;
+    }
+
+    public Alliance alliance() {
+      return m_alliance;
+    }
+
+    public String toString() {
+      StringBuilder b = new StringBuilder();
+      b.append(m_alliance);
+      b.append(" ");
+      b.append(m_pos);
+      return b.toString();
+    }
+
+    private static final double CLOSE_COMMUNITY_LINE_METERS = 3.36;
+    private static final double FAR_COMMUNITY_LINE_METERS = 4.91;
+
+    public double distanceToCommunityLineInMeters() {
+      // TODO: Check that the position #s are right.
+      if ((m_alliance == Alliance.Red && m_pos == 3)
+          || (m_alliance == Alliance.Blue && m_pos == 1)) {
+        return CLOSE_COMMUNITY_LINE_METERS;
+      } else {
+        // Note that this doesn't take into account additional distance for getting over the
+        // charging station.  A *real* implementation would need to do so.
+        return FAR_COMMUNITY_LINE_METERS;
+      }
+    }
+
+    private final int m_pos;
+    private final Alliance m_alliance;
   }
 }

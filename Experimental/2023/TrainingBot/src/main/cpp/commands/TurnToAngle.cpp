dbb3deadf3b2a79c09e8f2ab51d60143e5ad205b
quasics-driver <47063590+quasics-driver@users.noreply.github.com>
2023-02-16 20:52:19 -0500

    Debugging output (mjh)

--- Experimental/2023/TrainingBot/src/main/cpp/commands/TurnToAngle.cpp
+++ Experimental/2023/TrainingBot/src/main/cpp/commands/TurnToAngle.cpp
@@ -5,6 +5,7 @@
 #include "commands/TurnToAngle.h"
 
 #include <cmath>
+#include <iostream>
 
 TurnToAngle::TurnToAngle(DriveBase* driveBase, units::degree_t rotationDegree, double speed) {
   m_driveBase = driveBase;
@@ -19,31 +20,39 @@ TurnToAngle::TurnToAngle(DriveBase* driveBase, units::degree_t rotationDegree, d
 // Called when the command is initially scheduled.
 void TurnToAngle::Initialize() {
   m_targetAngle = m_angle + m_driveBase->GetAngle();
+  std::cerr << "Initialized Target angle: " << m_targetAngle.value() << "\n";
 }
 
 // Called repeatedly when this Command is scheduled to run
 void TurnToAngle::Execute() {
   if(m_angle.value() > 0) {
+    std::cerr << "Set speed to " << m_speed << ", " << -m_speed << "\n";
     m_driveBase->TankDrive(m_speed, -m_speed);
   }
   else if(m_angle.value() < 0) {
+    std::cerr << "Set speed to " << -m_speed << ", " << m_speed << "\n";
     m_driveBase->TankDrive(-m_speed, m_speed);
   }       
 }
 
 // Called once the command ends or is interrupted.
 void TurnToAngle::End(bool interrupted) {
+    std::cerr << "Stopping\n";
   m_driveBase->Stop();
 }
 
 // Returns true when the command should end.
 bool TurnToAngle::IsFinished() {
+    std::cerr << "current angle " << m_driveBase->GetAngle().value() << ", target " << targetAngle.value() << "\n";
   if(m_angle == 0_deg) {
+    std::cerr << "0 deg stop\n";
     return true;
   } else if(m_angle.value() > 0 && m_driveBase->GetAngle() >= m_targetAngle) {
+    std::cerr << ">0 angle stop\n";
     return true;
   }
   else if(m_angle.value() < 0 && m_driveBase->GetAngle() <= m_targetAngle) {
+    std::cerr << "<0 angle stop\n";
     return true;
   }
   return false;

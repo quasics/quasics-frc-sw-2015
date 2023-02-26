220817cd527faeb473d63ddaafe31022eece2e56
Matt Healy <mjhealy@users.noreply.github.com>
2023-02-26 11:18:57 -0500

    Reframing ThreeAxisGyro as an interface

--- Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/sensors/ThreeAxisGyro.java
+++ Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/sensors/ThreeAxisGyro.java
@@ -5,9 +5,10 @@
 package frc.robot.sensors;
 
 import edu.wpi.first.wpilibj.interfaces.Gyro;
+import java.util.function.Supplier;
 
 /** Add your docs here. */
-public abstract class ThreeAxisGyro {
+public abstract interface ThreeAxisGyro {
   /** Returns a Gyro object that can track rotation on the X axis (roll). */
   public abstract Gyro getRollGyro();
 
@@ -16,4 +17,38 @@ public abstract class ThreeAxisGyro {
 
   /** Returns a Gyro object that can track rotation on the Z axis (yaw). */
   public abstract Gyro getYawGyro();
+
+  /** Simple wrapper class to be help implement the interface. */
+  public class SingleAxisWrapper implements Gyro {
+    /** Source for reporting the angle. */
+    final Supplier<Double> m_angleSupplier;
+
+    /** Used by "reset()" to establish a new baseline for the angle. */
+    double m_calibrationOffset = 0;
+
+    SingleAxisWrapper(Supplier<Double> supplier) {
+      m_angleSupplier = supplier;
+    }
+
+    @Override
+    public void close() throws Exception {}
+
+    @Override
+    public void calibrate() {}
+
+    @Override
+    public void reset() {
+      m_calibrationOffset = m_angleSupplier.get();
+    }
+
+    @Override
+    public double getAngle() {
+      return m_angleSupplier.get() - m_calibrationOffset;
+    }
+
+    @Override
+    public double getRate() {
+      throw new UnsupportedOperationException("getRate() isn't supported");
+    }
+  }
 }
--- Experimental/2023/MattsToys/MattsToy2023/src/main/java/frc/robot/sensors/PigeonThreeAxisGyroWrapper.java
+++ Experimental/2023/MattsToys/MattsToy2023/src/main/java/frc/robot/sensors/PigeonThreeAxisGyroWrapper.java
@@ -6,61 +6,9 @@ package frc.robot.sensors;
 
 import com.ctre.phoenix.sensors.Pigeon2;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
-import java.util.function.Supplier;
 
 /** Add your docs here. */
-public class PigeonThreeAxisGyroWrapper extends ThreeAxisGyro {
-  enum Axis {
-    ePitch,
-    eRoll,
-    eYaw
-  }
-
-  private class SingleAxisWrapper implements Gyro {
-    double m_calibrationOffset = 0;
-    final Supplier<Double> m_angleSupplier;
-
-    SingleAxisWrapper(Axis axis) {
-      m_angleSupplier = getAngleForAxis(axis);
-    }
-
-    @Override
-    public void close() throws Exception {}
-
-    @Override
-    public void calibrate() {}
-
-    @Override
-    public void reset() {
-      m_calibrationOffset = m_angleSupplier.get();
-    }
-
-    @Override
-    public double getAngle() {
-      return m_angleSupplier.get() - m_calibrationOffset;
-    }
-
-    @Override
-    public double getRate() {
-      throw new UnsupportedOperationException("getRate() isn't supported for Pigeon2");
-    }
-  }
-
-  private Supplier<Double> getAngleForAxis(Axis axis) {
-    switch (axis) {
-      case ePitch:
-        return m_pigeon::getPitch;
-      case eRoll:
-        return m_pigeon::getRoll;
-      case eYaw:
-        return m_pigeon::getYaw;
-      default:
-        return () -> {
-          return 0.0;
-        };
-    }
-  }
-
+public class PigeonThreeAxisGyroWrapper implements ThreeAxisGyro {
   private Pigeon2 m_pigeon;
 
   public PigeonThreeAxisGyroWrapper(int deviceNumber) {
@@ -69,16 +17,16 @@ public class PigeonThreeAxisGyroWrapper extends ThreeAxisGyro {
 
   @Override
   public Gyro getRollGyro() {
-    return new SingleAxisWrapper(Axis.eRoll);
+    return new SingleAxisWrapper(m_pigeon::getRoll);
   }
 
   @Override
   public Gyro getPitchGyro() {
-    return new SingleAxisWrapper(Axis.ePitch);
+    return new SingleAxisWrapper(m_pigeon::getPitch);
   }
 
   @Override
   public Gyro getYawGyro() {
-    return new SingleAxisWrapper(Axis.eYaw);
+    return new SingleAxisWrapper(m_pigeon::getYaw);
   }
 }

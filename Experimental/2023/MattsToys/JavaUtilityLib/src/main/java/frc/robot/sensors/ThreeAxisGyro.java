ad6c13ce925fc384e67f61feebcefb326249735f
Matt Healy <mjhealy@users.noreply.github.com>
2023-02-26 10:53:13 -0500

    Adding ThreeAxisGyro interface.

--- Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/sensors/ThreeAxisGyro.java
+++ Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/sensors/ThreeAxisGyro.java
@@ -4,24 +4,36 @@
 
 package frc.robot.sensors;
 
-import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 /** Add your docs here. */
 public abstract class ThreeAxisGyro {
-    public final class NullGyro implements Gyro {
-        // From Gyro
-        public void calibrate() {}
-        public double getAngle() { return 0; }
-        public double getRate() { return 0; }
-        public Rotation2d getRotation2d() { return new Rotation2d(); }
-        public void reset() {}
+  public final class NullGyro implements Gyro {
+    // From Gyro
+    public void calibrate() {}
 
-        // From AutoCloseable
-        public void close() {}
+    public double getAngle() {
+      return 0;
     }
 
-    public abstract Gyro getXAxisGyro();
-    public abstract Gyro getYAxisGyro();
-    public abstract Gyro getZAxisGyro();
+    public double getRate() {
+      return 0;
+    }
+
+    public Rotation2d getRotation2d() {
+      return new Rotation2d();
+    }
+
+    public void reset() {}
+
+    // From AutoCloseable
+    public void close() {}
+  }
+
+  public abstract Gyro getRollGyro();
+
+  public abstract Gyro getPitchGyro();
+
+  public abstract Gyro getYawGyro();
 }

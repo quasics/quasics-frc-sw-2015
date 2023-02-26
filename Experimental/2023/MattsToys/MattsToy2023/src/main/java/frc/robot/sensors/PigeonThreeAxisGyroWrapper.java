2978c1c4ada498b76b50db253fe83d044c6d7c02
Matt Healy <mjhealy@users.noreply.github.com>
2023-02-26 11:31:04 -0500

    Docs, and extended constructor

--- Experimental/2023/MattsToys/MattsToy2023/src/main/java/frc/robot/sensors/PigeonThreeAxisGyroWrapper.java
+++ Experimental/2023/MattsToys/MattsToy2023/src/main/java/frc/robot/sensors/PigeonThreeAxisGyroWrapper.java
@@ -7,12 +7,28 @@ package frc.robot.sensors;
 import com.ctre.phoenix.sensors.Pigeon2;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 
-/** Add your docs here. */
+/** Provides access to discrete Gyros for each of the axes supported by the Pigeon2. */
 public class PigeonThreeAxisGyroWrapper implements ThreeAxisGyro {
+  /** The wrapped Pigeon2. */
   private Pigeon2 m_pigeon;
 
+  /**
+   * Constructor.
+   *
+   * @param deviceNumber CAN ID for the Pigeon2 being wrapped.
+   */
   public PigeonThreeAxisGyroWrapper(int deviceNumber) {
     m_pigeon = new Pigeon2(deviceNumber);
+  }
+
+  /**
+   * Constructor.
+   *
+   * @param deviceNumber CAN ID for the Pigeon2 being wrapped.
+   * @param canbus Name of the CANbus on which the Pigeon2 is wired.
+   */
+  public PigeonThreeAxisGyroWrapper(int deviceNumber, String canbus) {
+    m_pigeon = new Pigeon2(deviceNumber, canbus);
   }
 
   @Override

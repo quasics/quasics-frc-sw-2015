68d912159982f03921990fa3c429a824d7c41fc3
Matt Healy <mjhealy@users.noreply.github.com>
2023-03-04 12:14:57 -0500

    Porting Lighting subsystem from 2022 code.

---
+++ Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/commands/SimpleLighting.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Lighting;
+
+/** An example command that uses the lighting subsystem to establish a single color. */
+public class SimpleLighting extends CommandBase {
+  private final Lighting m_subsystem;
+  private final Lighting.StockColor color;
+
+  /**
+   * Creates a new SimpleLighting.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public SimpleLighting(Lighting subsystem) {
+    this(subsystem, Lighting.StockColor.Green);
+  }
+
+  /**
+   * Creates a new SimpleLighting.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public SimpleLighting(Lighting subsystem, Lighting.StockColor color) {
+    setName("Lighting");
+
+    m_subsystem = subsystem;
+    this.color = color;
+    addRequirements(subsystem);
+  }
+
+  @Override
+  public void execute() {
+    // Do it every time, just in case the lights aren't plugged in @ start.
+    m_subsystem.SetStripColor(color);
+  }
+}

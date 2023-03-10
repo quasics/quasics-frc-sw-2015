604c739dc6724295f395c8a491c5cb5c25db93d0
rylieloughran <124110541+rylieloughran@users.noreply.github.com>
2023-03-09 20:57:38 -0500

    Added UNTESTED SetAllianceColor command

---
+++ Quasics2023Code/ChargedUp/src/main/cpp/commands/SetAllianceColor.cpp
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/SetAllianceColor.h"
+
+#include <frc/DriverStation.h>
+
+SetAllianceColor::SetAllianceColor(Lighting* lighting) {
+  m_lighting = lighting;
+  // Use addRequirements() here to declare subsystem dependencies.
+  AddRequirements(m_lighting);
+}
+
+// Called when the command is initially scheduled.
+void SetAllianceColor::Initialize() {
+  m_lighting->SetAllToColor(0, 0, 0);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void SetAllianceColor::Execute() {
+  const auto allianceData = frc::DriverStation::GetAlliance();
+  if (allianceData == frc::DriverStation::kRed) {
+    m_lighting->SetAllToColor(255, 0, 0);
+  } else if (allianceData == frc::DriverStation::kBlue) {
+    m_lighting->SetAllToColor(0, 0, 255);
+  } else {
+    m_lighting->SetAllToColor(0, 255, 0);
+  }
+}
+
+// Called once the command ends or is interrupted.
+void SetAllianceColor::End(bool interrupted) {
+}
+
+// Returns true when the command should end.
+bool SetAllianceColor::IsFinished() {
+  return false;
+}

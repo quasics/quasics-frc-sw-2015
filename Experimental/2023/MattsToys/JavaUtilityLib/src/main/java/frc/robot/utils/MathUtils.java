9443505aefe559524b84165dfeee66dd78320058
Matt Healy <mjhealy@users.noreply.github.com>
2023-03-11 17:28:09 -0500

    Added withinTolerance().

--- Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/utils/MathUtils.java
+++ Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/utils/MathUtils.java
@@ -3,12 +3,14 @@ package frc.robot.utils;
 public final class MathUtils {
   /**
    * Determine whether two numbers are "approximately equal" by seeing if they are within a certain
-   * "tolerance percentage," with `tolerancePercentage` given as a percentage (such as 10.0 meaning
+   * "tolerance percentage", with `tolerancePercentage` given as a percentage (such as 10.0 meaning
    * "10%").
    *
    * <p>This is also meant to help account for the fact that comparison of floating-point numbers
    * for precise equality is usually unsafe.
    *
+   * @param desiredValue the value that we'd like to have
+   * @param actualValue the measured value
    * @param tolerancePercentage 0.01 = 1%, 0.025 = 2.5%, etc.
    * @see https://alvinalexander.com/source-code/java-approximately-equals-method-function/
    */
@@ -17,5 +19,21 @@ public final class MathUtils {
     double diff = Math.abs(desiredValue - actualValue);
     double tolerance = desiredValue * tolerancePercentage;
     return diff < tolerance;
+  }
+
+  /**
+   * Determine whether two numbers are "approximately equal" by seeing if they are within a certain
+   * "tolerance range", with `maximumError` given as the number "+/-" that we're willing to accept.
+   *
+   * <p>This is also meant to help account for the fact that comparison of floating-point numbers
+   * for precise equality is usually unsafe.
+   *
+   * @param desiredValue the value that we'd like to have
+   * @param actualValue the measured value
+   * @param maximumError maximum error (in absolute units) that we're willing to tolerate
+   */
+  public static boolean withinTolerance(
+      double desiredValue, double actualValue, double maximumError) {
+    return Math.abs(desiredValue - actualValue) <= Math.abs(maximumError);
   }
 }

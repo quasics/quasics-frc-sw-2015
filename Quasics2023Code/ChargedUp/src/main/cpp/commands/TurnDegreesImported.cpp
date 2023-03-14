24122ff17079c1b48353c0b82fb02fe50de1f724
RoboticsMB <83793330+RoboticsMB@users.noreply.github.com>
2023-03-13 22:41:31 -0400

    Cleaned up and drafted possible ideas for fixing robot rotation

--- Quasics2023Code/ChargedUp/src/main/cpp/commands/TurnDegreesImported.cpp
+++ Quasics2023Code/ChargedUp/src/main/cpp/commands/TurnDegreesImported.cpp
@@ -6,6 +6,9 @@
 
 #include <iostream>
 
+#undef CURRENT_TURN_VERSION
+#undef FASTER_SLOW_DOWN
+#define NO_SLOW_DOWN
 TurnDegreesImported::TurnDegreesImported(Drivebase* drivebase, double speed,
                                          units::degree_t angle)
     : m_drivebase(drivebase),
@@ -17,33 +20,35 @@ TurnDegreesImported::TurnDegreesImported(Drivebase* drivebase, double speed,
 
 // Called when the command is initially scheduled.
 void TurnDegreesImported::Initialize() {
+#ifdef CURRENT_TURN_VERSION
   angleTest = m_angle * DecreaseForCompensation;
   std::cout << "AngleTestValue" << angleTest.value() << std::endl;
   std::cout << "Wanted Turn" << m_angle.value() << std::endl;
   multiplier = 1.0;
   turningleft = angleTest > 0_deg;  // substitution
-  // turningleft = m_angle > 0_deg;
-  //  angleShift = m_drivebase->GetAngle();
   startingposition = m_drivebase->GetAngle();
   m_drivebase->SetBrakingMode(true);
-  /*
-  if (turningleft) {
-    m_drivebase->TankDrive(-1 * m_speed, m_speed);
-  } else {
-    m_drivebase->TankDrive(-1 * m_speed, m_speed);
-  }*/
   m_drivebase->TankDrive(-1 * m_speed, m_speed);
+#endif
+#ifdef NO_SLOW_DOWN
+  startingposition = m_drivebase->GetAngle();
+  turningleft = m_angle > 0_deg;
+  m_drivebase->SetBrakingMode(true);
+  m_drivebase->TankDrive(-1 * m_speed, m_speed);
+#endif
 }
 
 // Called repeatedly when this Command is scheduled to run
 void TurnDegreesImported::Execute() {
+#ifdef CURRENT_TURN_VERSION
   m_drivebase->SetBrakingMode(true);
   if (turningleft) {
-    // units::degree_t currentPosition = m_drivebase->GetAngle() - angleShift;
-    units::degree_t currentPosition = m_drivebase->GetAngle();  // 200
-    if (currentPosition >
-            ((startingposition + angleTest) * 0.5) &&  // 200 > 200+90
+    units::degree_t currentPosition = m_drivebase->GetAngle();
+    if (currentPosition > ((startingposition + angleTest) * 0.5) &&
         (m_speed * multiplier > 0.26)) {
+#ifdef FASTER_SLOW_DOWN
+      multiplier = multiplier * 0.90;
+#endif
       multiplier = multiplier * 0.95;
     }
     m_drivebase->TankDrive(-1 * m_speed * multiplier, m_speed * multiplier);
@@ -51,10 +56,18 @@ void TurnDegreesImported::Execute() {
     units::degree_t currentPosition = m_drivebase->GetAngle();
     if (currentPosition < ((startingposition + angleTest) * 0.5) &&
         (m_speed * multiplier > 0.26)) {
+#ifdef FASTER_SLOW_DOWN
+      multiplier = multiplier * 0.90;
+#endif
       multiplier = multiplier * 0.95;
     }
     m_drivebase->TankDrive(-1 * m_speed * multiplier, m_speed * multiplier);
   }
+#endif
+#ifdef NO_SLOW_DOWN
+  m_drivebase->SetBrakingMode(true);
+  m_drivebase->TankDrive(-1 * m_speed, m_speed);
+#endif
 }
 
 // Called once the command ends or is interrupted.
@@ -64,9 +77,10 @@ void TurnDegreesImported::End(bool interrupted) {
 }
 // Returns true when the command should end.
 bool TurnDegreesImported::IsFinished() {
-  units::degree_t currentPosition = m_drivebase->GetAngle();  // 200
+#ifdef CURRENT_TURN_VERSION
+  units::degree_t currentPosition = m_drivebase->GetAngle();
   if (turningleft) {
-    if (currentPosition >= (startingposition + angleTest)) {  // 200
+    if (currentPosition >= (startingposition + angleTest)) {
       std::cout << "Difference Left "
                 << (currentPosition - (startingposition + angleTest)).value()
                 << std::endl;
@@ -86,4 +100,24 @@ bool TurnDegreesImported::IsFinished() {
   }
 
   return false;
+#endif
+
+#ifdef NO_SLOW_DOWN
+  units::degree_t currentPosition = m_drivebase->GetAngle();
+  if (turningleft) {
+    if (currentPosition >=
+        (startingposition + m_angle) -
+            15_deg) {  // arbitrary value to have time to stop turning
+      return true;
+    }
+    return false;
+  } else {
+    if (currentPosition <=
+        (startingposition + m_angle) +
+            15_deg) {  // arbitrary value to have time to stop turning
+      return true;
+    }
+    return false;
+  }
+#endif
 }

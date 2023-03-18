636400a442c19fb707486aec9806422dfc7ab492
Matt Healy <mjhealy@users.noreply.github.com>
2023-03-18 12:28:20 -0400

    Added code for end() method.

--- Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/commands/ArcadeDrive.java
+++ Experimental/2023/MattsToys/JavaUtilityLib/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -44,7 +44,9 @@ public class ArcadeDrive extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_driveBase.stop();
+  }
 
   // Returns true when the command should end.
   @Override

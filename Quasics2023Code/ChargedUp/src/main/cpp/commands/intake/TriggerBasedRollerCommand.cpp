f5b24ddead36001c6d3c511d2bc3cd37adc8f11d
quasics-driver <47063590+quasics-driver@users.noreply.github.com>
2023-04-01 12:56:54 -0400

    added a thing that reports if its intaking cones or cubes

--- Quasics2023Code/ChargedUp/src/main/cpp/commands/intake/TriggerBasedRollerCommand.cpp
+++ Quasics2023Code/ChargedUp/src/main/cpp/commands/intake/TriggerBasedRollerCommand.cpp
@@ -4,6 +4,8 @@
 
 #include "commands/intake/TriggerBasedRollerCommand.h"
 
+#include <frc/smartdashboard/SmartDashboard.h>
+
 TriggerBasedRollerCommand::TriggerBasedRollerCommand(
     IntakeRoller* intakeRoller, ConfigSettings* settings,
     std::function<bool()> intakeTriggered,
@@ -20,6 +22,8 @@ TriggerBasedRollerCommand::TriggerBasedRollerCommand(
 // Called repeatedly when this Command is scheduled to run
 void TriggerBasedRollerCommand::Execute() {
   bool intakingCubes = m_settings->intakingCubes;
+  frc::SmartDashboard::PutString("Intake Setting",
+                                 intakingCubes ? "Cubes" : "Cones");
   if (m_intakeTriggered()) {
     if (intakingCubes) {
       m_intakeRoller->SetRollerSpeed(IntakeConstants::RollerSpeeds::CUBES);

cd433c60ce6a1a24c72d84993855158f821cf25c
Matt Healy <mjhealy@users.noreply.github.com>
2023-04-11 12:42:13 -0400

    Code review and minor cleanup

--- Quasics2023Code/ChargedUp/src/main/cpp/commands/movement/StraightLineDriving.cpp
+++ Quasics2023Code/ChargedUp/src/main/cpp/commands/movement/StraightLineDriving.cpp
@@ -7,7 +7,7 @@
 #include <iostream>
 
 // When button pressed, read current angle, in execute
-// pid.calculate(currentAngle, to original reading) Use arcade drive give use
+// pid.calculate(m_currentAngle, to original reading) Use arcade drive give use
 // forward power as what is given by joystick
 // GetRawAxis(OperatorInterface::LogitechGamePad::LEFT_Y_AXIS)
 
@@ -27,54 +27,55 @@ StraightLineDriving::StraightLineDriving(Drivebase* drivebase, double speed,
 
 // Called when the command is initially scheduled.
 void StraightLineDriving::Initialize() {
-  accelerating = true;
-  subtraction = 0;
-  gradualreduction = std::abs(m_speed) - 0.35;
-  counter = 0;
-  originalDistance = m_drivebase->GetLeftDistance();
-  originalAngle = m_drivebase->GetYaw();
-  std::cout << "Starting Angle: " << originalAngle.value() << std::endl;
+  m_accelerating = true;
+  m_subtraction = 0;
+  m_gradualreduction = std::abs(m_speed) - 0.35;
+  m_counter = 0;
+  m_originalDistance = m_drivebase->GetLeftDistance();
+  m_originalAngle = m_drivebase->GetYaw();
+  std::cout << "Starting Angle: " << m_originalAngle.value() << std::endl;
   m_drivebase->ArcadeDrive(m_speed, 0);
   m_drivebase->SetBrakingMode(true);
 }
 
 // Called repeatedly when this Command is scheduled to run
 void StraightLineDriving::Execute() {
-  if (SlewRateLimiter.Calculate(m_speed) >= m_speed) {
-    accelerating = false;
+  if (m_slewRateLimiter.Calculate(m_speed) >= m_speed) {
+    m_accelerating = false;
   }
-  currentDistance = m_drivebase->GetLeftDistance();
-  distanceToDestination = originalDistance + m_distance - currentDistance;
-  currentAngle = m_drivebase->GetYaw();
+  m_currentDistance = m_drivebase->GetLeftDistance();
+  m_distanceToDestination = m_originalDistance + m_distance - m_currentDistance;
+  m_currentAngle = m_drivebase->GetYaw();
   double rotationCorrection =
-      pid.Calculate(currentAngle.value(), originalAngle.value());
-  if (std::abs(distanceToDestination.value()) < 1.5 &&
+      pid.Calculate(m_currentAngle.value(), m_originalAngle.value());
+  if (std::abs(m_distanceToDestination.value()) < 1.5 &&
       std::abs(m_speed) > 0.35) {
     // std::cout << "Applying Reduction" << std::endl;
-    subtraction = std::abs(m_speed) - 0.35 - gradualreduction;
-    counter++;
-    if (gradualreduction > 0 && counter % 5 == 0) {
-      if (gradualreduction <= 0) {
-        gradualreduction = 0;
+    m_subtraction = std::abs(m_speed) - 0.35 - m_gradualreduction;
+    m_counter++;
+    if (m_gradualreduction > 0 && m_counter % 5 == 0) {
+      if (m_gradualreduction <= 0) {
+        m_gradualreduction = 0;
       } else {
-        gradualreduction = gradualreduction - 0.1;
+        m_gradualreduction = m_gradualreduction - 0.1;
       }
     }
   }
 
   if (m_distance >= 0_m) {
-    if (accelerating) {
-      m_drivebase->ArcadeDrive(SlewRateLimiter.Calculate(m_speed),
+    if (m_accelerating) {
+      m_drivebase->ArcadeDrive(m_slewRateLimiter.Calculate(m_speed),
                                rotationCorrection);
     } else {
-      m_drivebase->ArcadeDrive(m_speed - subtraction, rotationCorrection);
+      m_drivebase->ArcadeDrive(m_speed - m_subtraction, rotationCorrection);
     }
   } else {
-    if (accelerating) {
-      m_drivebase->ArcadeDrive(SlewRateLimiter.Calculate(m_speed),
+    if (m_accelerating) {
+      m_drivebase->ArcadeDrive(m_slewRateLimiter.Calculate(m_speed),
                                -1 * rotationCorrection);
     } else {
-      m_drivebase->ArcadeDrive(m_speed + subtraction, -1 * rotationCorrection);
+      m_drivebase->ArcadeDrive(m_speed + m_subtraction,
+                               -1 * rotationCorrection);
     }
   }
 
@@ -83,7 +84,7 @@ void StraightLineDriving::Execute() {
 
 // Called once the command ends or is interrupted.
 void StraightLineDriving::End(bool interrupted) {
-  std::cout << "Ending Angle: " << currentAngle.value() << std::endl;
+  std::cout << "Ending Angle: " << m_currentAngle.value() << std::endl;
   m_drivebase->Stop();
   m_drivebase->SetBrakingMode(true);
 }
@@ -91,11 +92,11 @@ void StraightLineDriving::End(bool interrupted) {
 // Returns true when the command should end.
 bool StraightLineDriving::IsFinished() {
   if (m_distance > 0_m) {
-    if (currentDistance >= (originalDistance + m_distance)) {
+    if (m_currentDistance >= (m_originalDistance + m_distance)) {
       return true;
     }
   } else {
-    if (currentDistance <= (originalDistance + m_distance)) {
+    if (m_currentDistance <= (m_originalDistance + m_distance)) {
       return true;
     }
   }
--- Quasics2023Code/ChargedUp/src/main/include/commands/movement/StraightLineDriving.h
+++ Quasics2023Code/ChargedUp/src/main/include/commands/movement/StraightLineDriving.h
@@ -18,6 +18,13 @@
  * <p>Note that this extends CommandHelper, rather extending CommandBase
  * directly; this is crucially important, or else the decorator functions in
  * Command will *not* work!
+ *
+ * CODE_REVIEW(matthew): Please update the documentation for this command,
+ * including the comments above (which indicate that this is "an example
+ * command"), so that it's clear what the command does, and how it is expected
+ * to be used.  You should also indicate how this differs from
+ * "DriveAtPowerForMeters" or "DriveAtPowerForMetersWorkingVersion", and why
+ * they're all available.
  */
 class StraightLineDriving
     : public frc2::CommandHelper<frc2::CommandBase, StraightLineDriving> {
@@ -42,16 +49,18 @@ class StraightLineDriving
                           StraightDrivingConstants::PID::kI,
                           StraightDrivingConstants::PID::kD};
 
-  frc::SlewRateLimiter<units::scalar> SlewRateLimiter{0.5 / 1_s};
-  bool accelerating = true;
-
-  units::degree_t originalAngle = 0_deg;
-  units::degree_t currentAngle = 0_deg;
-  double robotSpeed = 0;
-  units::meter_t originalDistance = 0_m;
-  units::meter_t currentDistance = 0_m;
-  units::meter_t distanceToDestination = 0_m;
-  double subtraction = 0;
-  double gradualreduction = 0.5;
-  int counter = 0;
+  // CODE_REVIEW(matthew): The names don't make it clear what some of these
+  // variables do (e.g., "m_subtraction").  Please either update the names to
+  // better reflect purpose, or add comments to clarify (or both).
+  frc::SlewRateLimiter<units::scalar> m_slewRateLimiter{0.5 / 1_s};
+  bool m_accelerating = true;
+  units::degree_t m_originalAngle = 0_deg;
+  units::degree_t m_currentAngle = 0_deg;
+  double m_robotSpeed = 0;
+  units::meter_t m_originalDistance = 0_m;
+  units::meter_t m_currentDistance = 0_m;
+  units::meter_t m_distanceToDestination = 0_m;
+  double m_subtraction = 0;
+  double m_gradualreduction = 0.5;
+  int m_counter = 0;
 };

9b06d48b09b9dc712c17dac2a03a6e9f2ba6d6af
Matt Healy <mjhealy@users.noreply.github.com>
2023-11-22 17:48:45 -0500

    Adding sample lighting code, based on 2023 toy.

--- Experimental/2024/MattsToys/SimulationExp-Beta3/src/main/java/frc/robot/RobotContainer.java
+++ Experimental/2024/MattsToys/SimulationExp-Beta3/src/main/java/frc/robot/RobotContainer.java
@@ -8,12 +8,19 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.RainbowLighting;
 import frc.robot.subsystems.AbstractDrivebase;
 import frc.robot.subsystems.BrokenCanDrivebase;
+import frc.robot.subsystems.Lighting;
+import frc.robot.subsystems.LightingInterface;
 import frc.robot.subsystems.SimulationDrivebase;
 
 public class RobotContainer {
+  final static int LIGHTING_PWM_PORT = 9;
+  final static int NUM_LIGHTS = 40;
+
   private final XboxController m_controller = new XboxController(0);
+  private final LightingInterface m_lighting = new Lighting(LIGHTING_PWM_PORT, NUM_LIGHTS);
   private final AbstractDrivebase m_drivebase;
 
   public RobotContainer() {
@@ -25,6 +32,7 @@ public class RobotContainer {
     configureBindings();
 
     m_drivebase.setDefaultCommand(new ArcadeDrive(m_drivebase, m_controller));
+    m_lighting.setDefaultCommand(new RainbowLighting(m_lighting));
   }
 
   private void configureBindings() {
---
+++ Experimental/2024/MattsToys/SimulationExp-Beta3/src/main/java/frc/robot/commands/SimpleLighting.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.LightingInterface;
+
+/** An example command that uses a lighting subsystem implementation to establish a single color. */
+public class SimpleLighting extends Command {
+  /** The lighting subsystem we're talking to. */
+  private final LightingInterface m_subsystem;
+
+  /** The color to be set for the robot's LEDs. */
+  private final LightingInterface.StockColor color;
+
+  /**
+   * Creates a new SimpleLighting.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public SimpleLighting(LightingInterface subsystem) {
+    this(subsystem, LightingInterface.StockColor.Green);
+  }
+
+  /**
+   * Creates a new SimpleLighting.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public SimpleLighting(LightingInterface subsystem, LightingInterface.StockColor color) {
+    setName("Lighting");
+
+    m_subsystem = subsystem;
+    this.color = color;
+    addRequirements((Subsystem) subsystem);
+  }
+
+  @Override
+  public void execute() {
+    // Do it every time, just in case the lights aren't plugged in @ start.
+    m_subsystem.SetStripColor(color);
+  }
+}

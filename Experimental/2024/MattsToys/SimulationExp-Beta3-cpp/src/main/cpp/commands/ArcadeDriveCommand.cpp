de03bd3d28d39605fa55361858a88402e15302b5
Matt Healy <mjhealy@users.noreply.github.com>
2023-12-01 17:00:58 -0500

    Initial (untested) arcade drive command.

---
+++ Experimental/2024/MattsToys/SimulationExp-Beta3-cpp/src/main/cpp/commands/ArcadeDriveCommand.cpp
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/ArcadeDriveCommand.h"
+
+ArcadeDriveCommand::ArcadeDriveCommand(IDrivebase& drivebase,
+                                       frc::XboxController& controller)
+    : m_drivebase(drivebase), m_controller(controller) {
+  AddRequirements(dynamic_cast<frc2::Subsystem*>(&m_drivebase));
+}
+
+// Called when the command is initially scheduled.
+void ArcadeDriveCommand::Initialize() {
+  updateSpeeds();
+}
+
+// Called repeatedly when this Command is scheduled to run
+void ArcadeDriveCommand::Execute() {
+  updateSpeeds();
+}
+
+// Called once the command ends or is interrupted.
+void ArcadeDriveCommand::End(bool interrupted) {
+  m_drivebase.stop();
+}
+
+// Returns true when the command should end.
+bool ArcadeDriveCommand::IsFinished() {
+  return false;
+}
+
+void ArcadeDriveCommand::updateSpeeds() {
+  // Get the x speed. We are inverting this because Xbox controllers return
+  // negative values when we push forward.
+  const units::meters_per_second_t xSpeed =
+      -m_speedLimiter.Calculate(m_controller.GetLeftY()) *
+      IDrivebase::MAX_SPEED;
+
+  // Get the rate of angular rotation. We are inverting this because we want a
+  // positive value when we pull to the left (remember, CCW is positive in
+  // mathematics). Xbox controllers return positive values when you pull to
+  // the right by default.
+  const units::radians_per_second_t rot =
+      -m_rotLimiter.Calculate(m_controller.GetRightX()) *
+      IDrivebase::MAX_ANGULAR_SPEED;
+
+  m_drivebase.arcadeDrive(xSpeed, rot);
+}
---
+++ Experimental/2024/MattsToys/SimulationExp-Beta3-cpp/src/main/include/commands/ArcadeDriveCommand.h
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc/XboxController.h>
+#include <frc/filter/SlewRateLimiter.h>
+#include <frc2/command/Command.h>
+#include <frc2/command/CommandHelper.h>
+
+#include "subsystems/IDriveBase.h"
+
+class ArcadeDriveCommand
+    : public frc2::CommandHelper<frc2::Command, ArcadeDriveCommand> {
+ private:
+  IDrivebase& m_drivebase;
+  frc::XboxController& m_controller;
+
+  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0
+  // to 1.
+  frc::SlewRateLimiter<units::scalar> m_speedLimiter{3 / 1_s};
+  frc::SlewRateLimiter<units::scalar> m_rotLimiter{3 / 1_s};
+
+ public:
+  ArcadeDriveCommand(IDrivebase& drivebase, frc::XboxController& controller);
+
+  void Initialize() override;
+
+  void Execute() override;
+
+  void End(bool interrupted) override;
+
+  bool IsFinished() override;
+
+ private:
+  void updateSpeeds();
+};

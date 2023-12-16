7aebeb924d9c863ab9bbf82df4b91c0eb60c0fdc
Matt Healy <mjhealy@users.noreply.github.com>
2023-12-15 21:08:26 -0500

    Removing extraneous import.

--- Experimental/2024/MattsToys/SimulationExp-Beta3/src/main/java/frc/robot/RobotContainer.java
+++ Experimental/2024/MattsToys/SimulationExp-Beta3/src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;

bdf315ba7c3d08df040965ae61f438a30e8b1672
quasics-driver <47063590+quasics-driver@users.noreply.github.com>
2023-12-21 07:07:20 -0500

    Adding SpinInPlace command.

---
+++ Experimental/2024/MattsToys/SimulationExp-Beta3/src/main/java/frc/robot/commands/SpinInPlace.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.AbstractDrivebase;
+
+public class SpinInPlace extends Command {
+  final private AbstractDrivebase m_drivebase;
+
+  /** Creates a new SpinInPlace. */
+  public SpinInPlace(AbstractDrivebase drivebase) {
+    m_drivebase = drivebase;
+    addRequirements(m_drivebase);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_drivebase.arcadeDrive(0, 1);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_drivebase.arcadeDrive(0, 1);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_drivebase.stop();
+  }
+}

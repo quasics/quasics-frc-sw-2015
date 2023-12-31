9c6b37b7f1c9f79a21002a75a1820ad300111e6c
Matt Healy <mjhealy@users.noreply.github.com>
2023-12-30 20:31:09 -0500

    Documentation updates

--- Arduino/Samples/java-udpPacketSender/UdpPacketSender.java
+++ Arduino/Samples/java-udpPacketSender/UdpPacketSender.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 class UdpPacketSender {
   public static void main(String [] args) throws IOException {
+	// Parameter sanitization
     if (args.length != 3) {
       System.err.println("Usage: java UdpPacketSender host_or_ip port text");
 	  System.exit(1);
@@ -11,12 +12,23 @@ class UdpPacketSender {
 	final int port = Integer.decode(args[1]);
 	final String msg = args[2];
  
-    InetAddress address = InetAddress.getByName(hostname);
-    DatagramSocket socket = new DatagramSocket();
- 
+	// Translate the 1st parameter into an actual IP address.
+    final InetAddress address = InetAddress.getByName(hostname);
+
+	// Allocate a socket for use in sending data packets.
+	//
+	// Note that we don't need to specify any information about
+	// IP addresses or ports here; this is the equivalent of
+	// the "drop box" we'll use to put our packet onto the
+	// network; the *packet* needs to be fully "addressed" for
+	// delivery..
+    final DatagramSocket socket = new DatagramSocket();
+
+	// Build the "packet" to be sent on the network.
     final byte[] buffer = msg.getBytes();
+    final DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
  
-    DatagramPacket request = new DatagramPacket(buffer, buffer.length, address, port);
-    socket.send(request);
+	// Send the data.
+    socket.send(packet);
   }
 }

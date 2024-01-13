50960e7d043a9fc520049de4a7f05812c6edc444
Quasics FRC <quasics_driverStation@mersgroup.com>
2024-01-13 16:40:13 -0500

    MORE TEST ICPOMENT

--- Quasics2024Code/Crescendo-XrpEnabled/src/main/cpp/Robot.cpp
+++ Quasics2024Code/Crescendo-XrpEnabled/src/main/cpp/Robot.cpp
@@ -1,12 +1,13 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+// TEST COMMENT NUMBER ONE
 #include "Robot.h"
 
 #include <frc2/command/CommandScheduler.h>
 
-void Robot::RobotInit() {}
+void Robot::RobotInit() {
+}
 
 /**
  * This function is called every 20 ms, no matter the mode. Use
@@ -25,9 +26,11 @@ void Robot::RobotPeriodic() {
  * can use it to reset any subsystem information you want to clear when the
  * robot is disabled.
  */
-void Robot::DisabledInit() {}
+void Robot::DisabledInit() {
+}
 
-void Robot::DisabledPeriodic() {}
+void Robot::DisabledPeriodic() {
+}
 
 /**
  * This autonomous runs the autonomous command selected by your {@link
@@ -41,7 +44,8 @@ void Robot::AutonomousInit() {
   }
 }
 
-void Robot::AutonomousPeriodic() {}
+void Robot::AutonomousPeriodic() {
+}
 
 void Robot::TeleopInit() {
   // This makes sure that the autonomous stops running when
@@ -56,22 +60,26 @@ void Robot::TeleopInit() {
 /**
  * This function is called periodically during operator control.
  */
-void Robot::TeleopPeriodic() {}
+void Robot::TeleopPeriodic() {
+}
 
 /**
  * This function is called periodically during test mode.
  */
-void Robot::TestPeriodic() {}
+void Robot::TestPeriodic() {
+}
 
 /**
  * This function is called once when the robot is first started up.
  */
-void Robot::SimulationInit() {}
+void Robot::SimulationInit() {
+}
 
 /**
  * This function is called periodically whilst in simulation.
  */
-void Robot::SimulationPeriodic() {}
+void Robot::SimulationPeriodic() {
+}
 
 #ifndef RUNNING_FRC_TESTS
 int main() {

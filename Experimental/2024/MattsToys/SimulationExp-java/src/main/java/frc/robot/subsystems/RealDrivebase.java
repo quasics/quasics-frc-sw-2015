6c8305da251e21a7cc8768bc0b633e81d6fa94b3
Matt Healy <mjhealy@alum.rit.edu>
2024-01-14 10:55:12 -0500

    Removing MotorControllerGroup use and Gladys constants. Also addressing some other build-time warnings, and cleaning up "to-do" comments.

--- Experimental/2024/MattsToys/SimulationExp-java/src/main/java/frc/robot/subsystems/RealDrivebase.java
+++ Experimental/2024/MattsToys/SimulationExp-java/src/main/java/frc/robot/subsystems/RealDrivebase.java
@@ -5,14 +5,12 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.Pigeon2;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.AnalogGyro;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import frc.robot.sensors.IGyro;
 import frc.robot.sensors.OffsetGyro;
 import frc.robot.sensors.SparkMaxEncoderWrapper;
@@ -38,42 +36,33 @@ public class RealDrivebase extends AbstractDrivebase {
     // Characteristics from 2023 "ChargedUp" constants for Sally
     Sally(
         /* Track Width (m) */
-        0.381 * 2, // TODO: Confirm this
+        0.381 * 2, // TODO: Confirm track width for Sally
         /* Gear ratio */
         8.45,
         /* PID */
         0.29613, 0.0, 0.0,
         /* Gains */
         0.19529, 2.2329, 0.0),
-    // Characteristics from 2023 "ChargedUp" code constants for Gladys
-    Gladys(
-        /* Track Width (m) */
-        0.5588 /* 22in */, // TODO: Confirm this
-        /* Gear ratio */
-        8.45,
-        /* PID */
-        0.29613, 0, 0,
-        /* Gains */
-        0.25829, 4.5623, 1.608),
     // Characteristics from 2023 "ChargedUp" code constants for Mae
     Mae(
         /* Track Width (m) */
-        0.5588 /* 22in */, // TODO: Confirm this
+        0.5588 /* 22in */, // TODO: Confirm track width for Mae
         /* Gear ratio */
         8.45,
         /* PID */
-        0.001379, 0, 0, // TODO: Confirm this, since it seems *really* low
+        0.001379, 0, 0, // TODO: Confirm kP for Mae, since it seems *really* low
         /*
          * Gains
          *
-         * TODO: Confirm these, since they're very different from 2022 values. (Though
-         * we also
-         * changed the hardware significantly post-season.)
+         * TODO: Confirm Gains for Mae, since they're very different from 2022
+         * values. (Though we also changed the hardware significantly
+         * post-season.)
          */
         0.13895, 1.3143, 0.1935);
 
-    RobotCharacteristics(double trackWidthMeters, double gearRatio, double kP, double kI, double kD,
-        double kS, double kV, double kA) {
+    RobotCharacteristics(double trackWidthMeters, double gearRatio, double kP,
+                         double kI, double kD, double kS, double kV,
+                         double kA) {
       this.kTrackWidthMeters = trackWidthMeters;
       this.gearRatio = gearRatio;
       this.kP = kP;
@@ -94,7 +83,8 @@ public class RealDrivebase extends AbstractDrivebase {
     final double kA;
   }
 
-  // Motor IDs are based on those Quasics has used over the last couple of years.
+  // Motor IDs are based on those Quasics has used over the last couple of
+  // years.
   static final int LEFT_FRONT_CAN_ID = 1;
   static final int LEFT_REAR_CAN_ID = 2;
   static final int RIGHT_FRONT_CAN_ID = 3;
@@ -104,8 +94,10 @@ public class RealDrivebase extends AbstractDrivebase {
 
   // Common physical characteristics for Quasics' robots (and directly derived
   // values).
-  static final double ANDYMARK_6IN_PLACTION_DIAMETER_METERS = Units.inchesToMeters(6.0);
-  static final double WHEEL_CIRCUMFERENCE_METERS = Math.PI * ANDYMARK_6IN_PLACTION_DIAMETER_METERS;
+  static final double ANDYMARK_6IN_PLACTION_DIAMETER_METERS =
+      Units.inchesToMeters(6.0);
+  static final double WHEEL_CIRCUMFERENCE_METERS =
+      Math.PI * ANDYMARK_6IN_PLACTION_DIAMETER_METERS;
 
   // Hardware control/sensing.
   //
@@ -113,22 +105,27 @@ public class RealDrivebase extends AbstractDrivebase {
   private final IGyro m_iGyro = IGyro.wrapGyro(m_gyro);
   private final IGyro m_offsetGyro = new OffsetGyro(m_iGyro);
 
-  final CANSparkMax m_leftRear = new CANSparkMax(LEFT_REAR_CAN_ID, MotorType.kBrushless);
-  final CANSparkMax m_rightRear = new CANSparkMax(RIGHT_REAR_CAN_ID, MotorType.kBrushless);
-  final CANSparkMax m_leftFront = new CANSparkMax(LEFT_FRONT_CAN_ID, MotorType.kBrushless);
-  final CANSparkMax m_rightFront = new CANSparkMax(RIGHT_FRONT_CAN_ID, MotorType.kBrushless);
-
-  private final MotorControllerGroup m_leftGroup = new MotorControllerGroup(m_leftRear, m_leftFront);
-  private final MotorControllerGroup m_rightGroup = new MotorControllerGroup(m_rightRear, m_rightFront);
+  final CANSparkMax m_leftRear =
+      new CANSparkMax(LEFT_REAR_CAN_ID, MotorType.kBrushless);
+  final CANSparkMax m_rightRear =
+      new CANSparkMax(RIGHT_REAR_CAN_ID, MotorType.kBrushless);
+  final CANSparkMax m_leftFront =
+      new CANSparkMax(LEFT_FRONT_CAN_ID, MotorType.kBrushless);
+  final CANSparkMax m_rightFront =
+      new CANSparkMax(RIGHT_FRONT_CAN_ID, MotorType.kBrushless);
 
   private final RelativeEncoder m_leftEncoder = m_leftRear.getEncoder();
   private final RelativeEncoder m_rightEncoder = m_rightRear.getEncoder();
 
-  private final TrivialEncoder m_leftTrivialEncoder = new SparkMaxEncoderWrapper(m_leftEncoder);
-  private final TrivialEncoder m_rightTrivialEncoder = new SparkMaxEncoderWrapper(m_rightEncoder);
+  private final TrivialEncoder m_leftTrivialEncoder =
+      new SparkMaxEncoderWrapper(m_leftEncoder);
+  private final TrivialEncoder m_rightTrivialEncoder =
+      new SparkMaxEncoderWrapper(m_rightEncoder);
 
-  private final DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(m_offsetGyro.getRotation2d(),
-      m_leftTrivialEncoder.getPosition(), m_rightTrivialEncoder.getPosition());
+  private final DifferentialDriveOdometry m_odometry =
+      new DifferentialDriveOdometry(m_offsetGyro.getRotation2d(),
+                                    m_leftTrivialEncoder.getPosition(),
+                                    m_rightTrivialEncoder.getPosition());
 
   /**
    * Preferred constructor.
@@ -137,15 +134,16 @@ public class RealDrivebase extends AbstractDrivebase {
    *              device to be driven
    */
   public RealDrivebase(RobotCharacteristics robot) {
-    this(robot.name(), robot.kTrackWidthMeters, robot.gearRatio, robot.kP, robot.kI, robot.kD,
-        robot.kS, robot.kV, robot.kA);
+    this(robot.name(), robot.kTrackWidthMeters, robot.gearRatio, robot.kP,
+         robot.kI, robot.kD, robot.kS, robot.kV, robot.kA);
   }
 
   /**
    * Detailed constructor.
    */
-  private RealDrivebase(String name, double trackWidthMeters, double gearRatio, double kP,
-      double kI, double kD, double kS, double kV, double kA) {
+  private RealDrivebase(String name, double trackWidthMeters, double gearRatio,
+                        double kP, double kI, double kD, double kS, double kV,
+                        double kA) {
     super(trackWidthMeters, kP, kI, kD, kS, kV, kA);
 
     super.setName(getClass().getSimpleName());
@@ -163,13 +161,16 @@ public class RealDrivebase extends AbstractDrivebase {
     ////////////////////////////////////////
     // Configure the encoders.
     System.out.println("Configuring drivebase for " + name);
-    System.out.println("Wheel circumference (m): " + WHEEL_CIRCUMFERENCE_METERS);
+    System.out.println("Wheel circumference (m): " +
+                       WHEEL_CIRCUMFERENCE_METERS);
 
     // Conversion factor from units in rotations (or RPM) to meters (or m/s).
-    final double distanceScalingFactorForGearing = WHEEL_CIRCUMFERENCE_METERS / gearRatio;
+    final double distanceScalingFactorForGearing =
+        WHEEL_CIRCUMFERENCE_METERS / gearRatio;
     final double velocityScalingFactor = distanceScalingFactorForGearing / 60;
     System.out.println("Using gear ratio: " + gearRatio);
-    System.out.println("Adjustment for gearing (m/rotation): " + distanceScalingFactorForGearing);
+    System.out.println("Adjustment for gearing (m/rotation): " +
+                       distanceScalingFactorForGearing);
     System.out.println("Velocity adj.: " + velocityScalingFactor);
 
     m_leftEncoder.setPositionConversionFactor(distanceScalingFactorForGearing);
@@ -196,34 +197,26 @@ public class RealDrivebase extends AbstractDrivebase {
     m_rightRear.setIdleMode(mode);
   }
 
-  protected DifferentialDriveOdometry getOdometry() {
-    return m_odometry;
-  }
+  protected DifferentialDriveOdometry getOdometry() { return m_odometry; }
 
-  protected TrivialEncoder getLeftEncoder() {
-    return m_leftTrivialEncoder;
-  }
+  protected TrivialEncoder getLeftEncoder() { return m_leftTrivialEncoder; }
 
-  protected TrivialEncoder getRightEncoder() {
-    return m_rightTrivialEncoder;
-  }
+  protected TrivialEncoder getRightEncoder() { return m_rightTrivialEncoder; }
 
-  protected IGyro getGyro() {
-    return m_offsetGyro;
-  }
+  protected IGyro getGyro() { return m_offsetGyro; }
 
-  protected double getLeftSpeedPercentage() {
-    return m_leftRear.get();
-  }
+  protected double getLeftSpeedPercentage() { return m_leftRear.get(); }
 
-  protected double getRightSpeedPercentage() {
-    return m_rightRear.get();
-  }
+  protected double getRightSpeedPercentage() { return m_rightRear.get(); }
 
   @Override
   protected void setMotorVoltagesImpl(double leftVoltage, double rightVoltage) {
-    m_leftGroup.setVoltage(leftVoltage);
-    m_rightGroup.setVoltage(rightVoltage);
+    // TODO: Remove one pair, once CAN motors are configured for lead/follow on
+    // drive bases.
+    m_leftFront.setVoltage(leftVoltage);
+    m_rightFront.setVoltage(rightVoltage);
+    m_leftRear.setVoltage(leftVoltage);
+    m_rightRear.setVoltage(rightVoltage);
 
     logValue("Left volts", leftVoltage);
     logValue("Right volts", rightVoltage);

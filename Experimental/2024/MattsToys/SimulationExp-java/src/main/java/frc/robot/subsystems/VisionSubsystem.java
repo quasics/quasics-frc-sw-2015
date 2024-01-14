c79c35c413b624884e7f298c9f16d851595454fc
Matt Healy <mjhealy@alum.rit.edu>
2024-01-14 10:55:29 -0500

    Removed unused import.

--- Experimental/2024/MattsToys/SimulationExp-java/src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ Experimental/2024/MattsToys/SimulationExp-java/src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;

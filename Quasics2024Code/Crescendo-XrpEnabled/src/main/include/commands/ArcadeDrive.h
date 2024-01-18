684ff03d034957ae6938ad286d93172e245ab4c5
Matt Healy <mjhealy@alum.rit.edu>
2024-01-18 13:40:37 -0500

    Copyright info updates.

--- Quasics2024Code/Crescendo-XrpEnabled/src/main/include/commands/ArcadeDrive.h
+++ Quasics2024Code/Crescendo-XrpEnabled/src/main/include/commands/ArcadeDrive.h
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) 2024 Quasics, FIRST, and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -14,8 +14,7 @@
  * directly; this is crucially important, or else the decorator functions in
  * Command will *not* work!
  */
-class ArcadeDrive
-    : public frc2::CommandHelper<frc2::Command, ArcadeDrive> {
+class ArcadeDrive : public frc2::CommandHelper<frc2::Command, ArcadeDrive> {
  public:
   ArcadeDrive();
 

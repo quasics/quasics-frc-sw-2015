5a053dd89914e7d6dae35b1bc9fd28fc81f69670
Matthew Bandos <mat.bandos@gmail.com>
2024-02-03 13:08:24 -0500

    Added a pivot intake auto command

---
+++ Quasics2024Code/Crescendo-XrpEnabled/src/main/cpp/commands/PivotIntakeAuto.cpp
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/PivotIntakeAuto.h"
+
+PivotIntakeAuto::PivotIntakeAuto(IntakeDeployment& IntakeDeployment,
+                                 double speed, bool extend)
+    : m_intakeDeployment(IntakeDeployment),
+      m_intakeDeploymentSpeed(extend ? std::abs(speed) : -std::abs(speed)),
+      m_extending(extend) {
+  // Use addRequirements() here to declare subsystem dependencies.
+  AddRequirements(&m_intakeDeployment);
+}
+
+// Called when the command is initially scheduled.
+void PivotIntakeAuto::Initialize() {
+  m_intakeDeployment.SetMotorSpeed(m_intakeDeploymentSpeed);
+  m_intakeDeployment.EnableBraking(true);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void PivotIntakeAuto::Execute() {
+  m_intakeDeployment.SetMotorSpeed(m_intakeDeploymentSpeed);
+}
+
+// Called once the command ends or is interrupted.
+void PivotIntakeAuto::End(bool interrupted) {
+  m_intakeDeployment.Stop();
+  m_intakeDeployment.EnableBraking(true);
+}
+
+// Returns true when the command should end.
+bool PivotIntakeAuto::IsFinished() {
+  if (m_extending) {
+    if (m_intakeDeployment.ExtendedByRevolutions()) {
+      return true;
+    }
+    return false;
+  } else {
+    if (m_intakeDeployment.RetractedByRevolutions()) {
+      return true;
+    }
+    return false;
+  }
+  return false;
+}

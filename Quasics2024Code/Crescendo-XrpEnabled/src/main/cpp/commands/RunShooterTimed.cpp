ed074000597ff4591b4b1184f4ff80c2fb597bd3
Matthew Bandos <mat.bandos@gmail.com>
2024-02-03 12:39:03 -0500

    Added an autonomus shooter command

---
+++ Quasics2024Code/Crescendo-XrpEnabled/src/main/cpp/commands/RunShooterTimed.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/RunShooterTimed.h"
+
+RunShooterTimed::RunShooterTimed(Shooter* shooter, double shooterSpeed,
+                                 units::second_t time, bool shooting)
+    : m_shooter(shooter),
+      m_shooterSpeed(shooting ? std::abs(shooterSpeed)
+                              : -std::abs(shooterSpeed)),
+      m_time(time),
+      m_shooting(shooting) {
+  // Use addRequirements() here to declare subsystem dependencies.
+  AddRequirements(m_shooter);
+}
+
+// Called when the command is initially scheduled.
+void RunShooterTimed::Initialize() {
+  m_stopWatch.Reset();
+  m_stopWatch.Start();
+  m_shooter->SetFlywheelSpeed(m_shooterSpeed);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void RunShooterTimed::Execute() {
+  m_shooter->SetFlywheelSpeed(m_shooterSpeed);
+}
+
+// Called once the command ends or is interrupted.
+void RunShooterTimed::End(bool interrupted) {
+  m_shooter->Stop();
+}
+
+// Returns true when the command should end.
+bool RunShooterTimed::IsFinished() {
+  if (m_stopWatch.HasElapsed(m_time)) {
+    return true;
+  }
+  return false;
+}

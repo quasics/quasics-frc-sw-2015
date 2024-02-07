8d890f2d2eee9254b66dc578c33024c9621097b4
Matt Healy <mjhealy@alum.rit.edu>
2024-02-07 16:57:10 -0500

    Adding missing boilerplate.

--- Quasics2024Code/Crescendo-XrpEnabled/src/main/include/ConfigSettings.h
+++ Quasics2024Code/Crescendo-XrpEnabled/src/main/include/ConfigSettings.h
@@ -1,3 +1,9 @@
+// Copyright (c) 2024 Quasics, FIRST, and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
 struct ConfigSettings {
   volatile bool normalDriveEngaged = true;
 };

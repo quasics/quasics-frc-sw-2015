d07e48e5080583dcef9b97473fffb15a9746e71d
Matt Healy <mjhealy@alum.rit.edu>
2024-02-10 12:57:35 -0500

    Updates for AndyMark L16 actuator.

--- Experimental/2024/MattsToys/ServoTester/src/main/java/frc/robot/subsystems/ServoHost.java
+++ Experimental/2024/MattsToys/ServoTester/src/main/java/frc/robot/subsystems/ServoHost.java
@@ -9,21 +9,42 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ServoHost extends SubsystemBase {
   static final int SERVO_PWM_ID = 3;
+  static final double FULLY_OPEN = +1.0;
+  static final double FULLY_CLOSED = -1.0;
+
+  /**
+   * Assumed to be an AndyMark Linear Servo Actuator L16-R 140 mm Stroke 35:1 6v.
+   * 
+   * @see https://www.andymark.com/products/actuator-l16-r-140mm-stroke-35-1-6v
+   * @see http://tinyurl.com/45t2ffyc
+   * @see https://www.chiefdelphi.com/t/anyone-have-servo-example-code/155951/9
+   */
   final Servo m_servo = new Servo(SERVO_PWM_ID);
 
   /** Creates a new ServoHost. */
   public ServoHost() {
+    // Configure the boundaries for this device, which are different from a
+    // bog-standard servo.
+    m_servo.setBoundsMicroseconds(
+        2000, // max (per docs): 2.0 msec
+        1800, // deadbandMax (per docs): 1.8 msec
+        1500, // center (per docs): 1.5 msec
+        1200, // deadbandMin (per docs): 1.2 msec
+        1000 // min (per docs): 1.0 msec
+    );
+    m_servo.setSpeed(1.0); // to open
+    m_servo.setSpeed(-1.0); // to close
   }
 
   /**
    * @param pos position for the servo (in the range [0.0,1.0])
    */
   public void setServoPosition(double pos) {
-    m_servo.set(pos);
+    m_servo.setSpeed(Math.min(Math.max(FULLY_CLOSED, pos), FULLY_OPEN));
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    System.out.println("Servo position: " + m_servo.getSpeed());
   }
 }

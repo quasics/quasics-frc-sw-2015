bc9ed3b6644d0082d92aacc4476eec2637e65399
Matt Healy <mjhealy@alum.rit.edu>
2024-02-17 11:26:08 -0500

    Beginning to add BulletinBoard as a central data-sharing node.

---
+++ Experimental/2024/MattsToys/SimulationExp-java/src/main/java/frc/robot/utils/BulletinBoard.java
@@ -0,0 +1,30 @@
+// Copyright (c) 2024, Matthew J. Healy and other Quasics contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/** Add your docs here. */
+public class BulletinBoard {
+  private static Map<String, Object> values = new HashMap<String, Object>();
+
+  public static synchronized Optional<Object> getValue(String key) {
+    if (!values.containsKey(key)) {
+      return Optional.empty();
+    } else {
+      return Optional.of(values.get(key));
+    }
+  }
+
+  public static synchronized void clearValue(String key) {
+    values.remove(key);
+  }
+
+  public static synchronized void updateValue(String key, Object value) {
+    values.put(key, value);
+  }
+}

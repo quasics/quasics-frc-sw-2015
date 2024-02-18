2161f5e7510f5de558532f95f443a5e9151d398e
Matt Healy <mjhealy@alum.rit.edu>
2024-02-17 19:57:56 -0500

    Added conditional definition for ENABLE_XRP.

--- Quasics2024Code/Crescendo-XrpEnabled/src/main/include/ConditionalCompileFlags.h
+++ Quasics2024Code/Crescendo-XrpEnabled/src/main/include/ConditionalCompileFlags.h
@@ -19,3 +19,21 @@
 #undef ENABLE_VISION_TESTING
 
 #undef LEAK_VISION_WORKAROUND
+
+// The XRP classes aren't available when compiling for the RoboRio (only when
+// building for simulation on a host platform).
+#if defined(__linux__)
+// Targeting the RoboRio
+#undef ENABLE_XRP
+#elif defined(__APPLE__)
+// Targeting MacOS (for emulation/unit testing)
+#define ENABLE_XRP
+#elif defined(_WIN32)
+// Targeting Windows (for emulation/unit testing)
+#define ENABLE_XRP
+#else
+// (*shrug*) Assume Windows (for emulation/unit testing), but we could also use
+// the lists at
+// https://github.com/cpredef/predef/blob/master/OperatingSystems.md
+#define ENABLE_XRP
+#endif

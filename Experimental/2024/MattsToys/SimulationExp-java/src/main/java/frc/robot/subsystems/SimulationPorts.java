580e4ac062f5e63c1eec6cf9ec7008ed9accde65
Matt Healy <mjhealy@alum.rit.edu>
2024-03-02 10:36:07 -0500

    Adding ports for simulated climber.

--- Experimental/2024/MattsToys/SimulationExp-java/src/main/java/frc/robot/subsystems/SimulationPorts.java
+++ Experimental/2024/MattsToys/SimulationExp-java/src/main/java/frc/robot/subsystems/SimulationPorts.java
@@ -6,8 +6,16 @@ public interface SimulationPorts {
   public final int RIGHT_FRONT_DRIVE_PWM_ID = 3;
   public final int RIGHT_REAR_DRIVE_PWM_ID = 4;
 
+  public final int LEFT_CLIMBER_PWM_ID = 5;
+  public final int RIGHT_CLIMBER_PWM_ID = 6;
+
   public final int LEFT_DRIVE_ENCODER_PORT_A = 0;
   public final int LEFT_DRIVE_ENCODER_PORT_B = 1;
   public final int RIGHT_DRIVE_ENCODER_PORT_A = 2;
   public final int RIGHT_DRIVE_ENCODER_PORT_B = 3;
+
+  public final int LEFT_CLIMBER_ENCODER_PORT_A = 4;
+  public final int LEFT_CLIMBER_ENCODER_PORT_B = 5;
+  public final int RIGHT_CLIMBER_ENCODER_PORT_A = 6;
+  public final int RIGHT_CLIMBER_ENCODER_PORT_B = 7;
 }

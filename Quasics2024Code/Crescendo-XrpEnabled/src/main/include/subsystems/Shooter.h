ebce5a6aa17d5aa81325c0dd947ccd7e7ea1b282
Matt Healy <mjhealy@alum.rit.edu>
2024-03-10 16:56:25 -0400

    Removing unused include.

--- Quasics2024Code/Crescendo-XrpEnabled/src/main/include/subsystems/Shooter.h
+++ Quasics2024Code/Crescendo-XrpEnabled/src/main/include/subsystems/Shooter.h
@@ -4,15 +4,11 @@
 
 #pragma once
 
-#include <frc/Servo.h>
 #include <frc2/command/SubsystemBase.h>
 #include <rev/CANSparkMax.h>
 
 /**
  * Cargo (ball) shooting subsystem, used to deliver cargo to the hub.
- *
- * Note that the servos are assumed to be an AndyMark Linear Servo Actuator
- * L16-R 140 mm Stroke 35:1 6v, and need to be configured appropriately.
  */
 class Shooter : public frc2::SubsystemBase {
  public:

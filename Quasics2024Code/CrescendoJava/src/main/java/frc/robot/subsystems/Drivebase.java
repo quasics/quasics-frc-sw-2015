b7e90ea55a381837339082204ca6b1823ac07e2a
Quasics FRC <quasics_driverStation@mersgroup.com>
2024-05-02 19:24:07 -0400

    new java project with tank drive volts (untested)

---
+++ Quasics2024Code/CrescendoJava/src/main/java/frc/robot/subsystems/Drivebase.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+import frc.robot.Constants.CanBusIds.SparkMax;
+
+public class Drivebase extends SubsystemBase {
+  final CANSparkMax m_leftLeader = new CANSparkMax(SparkMax.LEFT_LEADER_ID, MotorType.kBrushless);
+  final CANSparkMax m_rightLeader = new CANSparkMax(SparkMax.LEFT_LEADER_ID, MotorType.kBrushless);
+
+  /** Creates a new Drivebase. */
+  public Drivebase() {}
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putData("tank drive 6V", new InstantCommand(() -> tankDriveVolts(6, 6)));
+  }
+
+  public void tankDriveVolts(double leftVoltage, double rightVoltage) {
+    m_leftLeader.setVoltage(leftVoltage);
+    m_rightLeader.setVoltage(rightVoltage);
+  }
+
+}

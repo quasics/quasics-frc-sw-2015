8e71162fff16f40c0a6123751eb0fc07586fe899
Quasics FRC <quasics_driverStation@mersgroup.com>
2024-06-08 10:02:06 -0400

    import

--- Quasics2024Code/CrescendoJava/src/main/java/frc/robot/subsystems/Climbers.java
+++ Quasics2024Code/CrescendoJava/src/main/java/frc/robot/subsystems/Climbers.java
@@ -9,6 +9,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkBase.IdleMode;
 
+import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.ConditionalConstants;
 import frc.robot.Constants.CanBusIds.SparkMax;

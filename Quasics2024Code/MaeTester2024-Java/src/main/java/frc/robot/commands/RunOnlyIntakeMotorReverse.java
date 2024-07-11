b18a799c067bca7a3c41ccca96408f0fd9bf7269
rylieloughran <rylielou8@icloud.com>
2024-07-11 19:15:42 -0400

    added untested cmds RunOnlyConveyorMotor(and reverse), RunOnlyIntakeMotor(and reverse), run shooting motor, and triggerdrivenshooting

--- Quasics2024Code/MaeTester2024-Java/src/main/java/frc/robot/commands/RunOnlyIntakeMotorReverse.java
+++ Quasics2024Code/MaeTester2024-Java/src/main/java/frc/robot/commands/RunOnlyIntakeMotorReverse.java
@@ -5,28 +5,25 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
 
 public class RunOnlyIntakeMotorReverse extends Command {
+  private final Intake m_intake;
   /** Creates a new RunOnlyIntakeMotorReverse. */
-  public RunOnlyIntakeMotorReverse() {
+  public RunOnlyIntakeMotorReverse(Intake intake) {
+    m_intake = intake;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intake);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
+  public void initialize() {
+    m_intake.onlyIntakeReverse();
+  }
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
+  public void end(boolean interrupted) {
+    m_intake.onlyIntakeOff();
   }
 }

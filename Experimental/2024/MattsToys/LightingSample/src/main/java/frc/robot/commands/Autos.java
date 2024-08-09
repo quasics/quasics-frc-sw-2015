e48e9aa0bc22ec95971763c81a87cc9b19ab64b0
Matt Healy <mjhealy@alum.rit.edu>
2024-08-08 20:06:56 -0400

    Minor cleanup.

--- Experimental/2024/MattsToys/LightingSample/src/main/java/frc/robot/commands/Autos.java
+++ Experimental/2024/MattsToys/LightingSample/src/main/java/frc/robot/commands/Autos.java
@@ -4,14 +4,16 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import frc.robot.subsystems.ExampleSubsystem;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
   public static Command exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+    return Commands.sequence(subsystem.exampleMethodCommand(),
+                             new PrintCommand("All done"));
   }
 
   private Autos() {

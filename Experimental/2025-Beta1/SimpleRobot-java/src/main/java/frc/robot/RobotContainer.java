a68c08d57046a3beb3b18429978f52f15071de3b
Matt Healy <mjhealy@alum.rit.edu>
2024-11-07 20:15:41 -0500

    Starting to implement Vision subsystem.
    Simulation support is up and running (tested via browser).

--- Experimental/2025-Beta1/SimpleRobot-java/src/main/java/frc/robot/RobotContainer.java
+++ Experimental/2025-Beta1/SimpleRobot-java/src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Vision;
 
 public class RobotContainer {
+  // Subsystems
+  Vision m_vision = new Vision();
+
   public RobotContainer() {
     configureBindings();
   }

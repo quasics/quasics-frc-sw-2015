79a7e12a0f295fa181572f2dbd121c299866991e
Matt Healy <mjhealy@alum.rit.edu>
2025-01-16 08:38:34 -0500

    Copyright updates.

--- Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/Vision.java
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/Vision.java
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) 2024, Matthew J. Healy and other Quasics contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -7,7 +7,6 @@ package frc.robot.subsystems;
 import java.io.IOException;
 
 import org.photonvision.PhotonCamera;
-import org.photonvision.estimation.TargetModel;
 import org.photonvision.simulation.PhotonCameraSim;
 import org.photonvision.simulation.SimCameraProperties;
 import org.photonvision.simulation.VisionSystemSim;

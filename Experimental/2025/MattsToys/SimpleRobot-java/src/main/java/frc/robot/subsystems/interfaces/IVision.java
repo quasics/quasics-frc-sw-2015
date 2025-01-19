15d6de73546c73f79f0f71aeeee66b61c70f0d0b
Matt Healy <mjhealy@alum.rit.edu>
2025-01-19 16:54:27 -0500

    Defining IVision interface.

---
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/interfaces/IVision.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.interfaces;
+
+public interface IVision {
+
+    public static final String VISION_POSE_KEY = "Vision.Pose";
+    public static final String VISION_TIMESTAMP_KEY = "Vision.Timestamp";
+
+}

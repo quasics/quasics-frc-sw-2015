aaa134d14da82384d0a1ec22f2566a8995f61c4a
Matt Healy <mjhealy@alum.rit.edu>
2025-01-24 15:26:54 -0500

    Adding Logitech constants.  Also updating comments.

--- Experimental/2025/TrainingBot/src/main/java/frc/robot/Constants.java
+++ Experimental/2025/TrainingBot/src/main/java/frc/robot/Constants.java
@@ -5,15 +5,45 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The following is advice from the WPILib project:
+ * "The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class."
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  public static class LogitechGamePad {
+    // Axes - Used with the "getRawAxis()" function to access the data for the
+    // individual sticks on the controller (e.g., for "tank drive" coding).
+    //
+    // Note that the left and right triggers aren't treated as buttons: they
+    // report to the driver's station software as if they're joysticks (with a
+    // range of [0.0, 1.0], unlike regular joysticks).
+    public static final int LeftXAxis = 0;
+    public static final int LeftYAxis = 1;
+    public static final int LeftTriggerAxis = 2;
+    public static final int RightTriggerAxis = 3;
+    public static final int RightXAxis = 2;
+    public static final int RightYAxis = 3;
+
+    // Buttons
+    public static final int AButton = 1;
+    public static final int BButton = 2;
+    public static final int XButton = 3;
+    public static final int YButton = 4;
+    public static final int LeftShoulder = 5;
+    public static final int RightShoulder = 6;
+    public static final int BackButton = 7;
+    public static final int StartButton = 8;
+    public static final int LeftStickPress = 9;
+    public static final int RightStickPress = 10;
   }
 }

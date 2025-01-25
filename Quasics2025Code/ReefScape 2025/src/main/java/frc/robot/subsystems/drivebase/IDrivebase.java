5aabffdda1f80d30fe36d9582055cd572655fc80
Quasics FRC <quasics_driverStation@mersgroup.com>
2025-01-25 12:05:40 -0500

    fixed simulation driving

--- Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/subsystems/drivebase/IDrivebase.java
+++ Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/subsystems/drivebase/IDrivebase.java
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) 2024, Matthew J. Healy and other Quasics contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -112,7 +112,7 @@ public abstract class IDrivebase extends SubsystemBase {
   }
 
   public void setSpeeds(double leftSpeed, double rightSpeed) {
-    this.setSpeeds(leftSpeed, rightSpeed);
+    this.setSpeeds_HAL(leftSpeed, rightSpeed);
   }
 
   public Distance getLengthIncludingBumpers() {

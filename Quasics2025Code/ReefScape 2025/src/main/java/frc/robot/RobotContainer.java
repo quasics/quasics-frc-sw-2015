73e12aa39c3fb3b04b6568d6ff3befc6fc50ccc9
Quasics FRC <quasics_driverStation@mersgroup.com>
2025-02-01 16:07:32 -0500

    small changes

--- Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/RobotContainer.java
+++ Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/RobotContainer.java
@@ -34,6 +34,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -68,7 +69,7 @@ public class RobotContainer {
 
   private final Joystick m_driverController = new Joystick(Constants.DriveTeam.DRIVER_JOYSTICK_ID);
   private final Joystick m_operatorController = new Joystick(Constants.DriveTeam.OPERATOR_JOYSTICK_ID);
-  private final double DEADBAND_CONSTANT = 0.04;
+  private final double DEADBAND_CONSTANT = 0.08;
 
   Trigger switchDriveTrigger;
 
@@ -225,6 +226,7 @@ public class RobotContainer {
 
   private Command intakeThenExtake() {
     return Commands.sequence(new RunKrakenForTime(m_armRoller, true, 0.2),
+    new WaitCommand(0.1),
     new RunKraken(m_armRoller, false)
     );
   }

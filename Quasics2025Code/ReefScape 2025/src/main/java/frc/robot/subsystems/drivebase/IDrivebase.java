3084c1e4ed2fda75728f027eb13332dfa6e26404
Quasics FRC <quasics_driverStation@mersgroup.com>
2025-02-05 20:21:23 -0500

    changed path to a circle. it still doesn't work

--- Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/subsystems/drivebase/IDrivebase.java
+++ Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/subsystems/drivebase/IDrivebase.java
@@ -41,7 +41,7 @@ public abstract class IDrivebase extends SubsystemBase {
   public static final LinearVelocity MAX_SPEED = MetersPerSecond.of(3);
   
   // Max rotational speed is 1/2 rotations per second
-  public static final AngularVelocity MAX_ANGULAR_SPEED = RadiansPerSecond.of(5);
+  public static final AngularVelocity MAX_ANGULAR_SPEED = RadiansPerSecond.of(8.42);
 
   protected static final double NEO_FREE_SPEED = 5676;
 

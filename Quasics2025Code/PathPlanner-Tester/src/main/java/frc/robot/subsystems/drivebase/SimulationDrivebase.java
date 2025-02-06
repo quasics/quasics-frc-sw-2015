03f396cb1f694d5efce0a68cc40ce2179b42aec5
Josh Biehl <joshbiehl94@gmail.com>
2025-02-05 20:24:20 -0500

    started working on sparkmax sim in path planner test

--- Quasics2025Code/PathPlanner-Tester/src/main/java/frc/robot/subsystems/drivebase/SimulationDrivebase.java
+++ Quasics2025Code/PathPlanner-Tester/src/main/java/frc/robot/subsystems/drivebase/SimulationDrivebase.java
@@ -3,10 +3,26 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems.drivebase;
+import frc.robot.Constants.CanBusIds.SparkMaxIds;
 
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.sim.SparkMaxSim;
 
 public class SimulationDrivebase extends AbstractDrivebase {
+
+
+
+  private final Field2d m_fieldSim = new Field2d();
+
+  final SparkMax m_leftLeader = new SparkMax(SparkMaxIds.LEFT_LEADER_ID, MotorType.kBrushless);
+  //final SparkMaxSim m_leftLeaderSim = new SparkMaxSim(m_leftLeader, );
+
   /** Creates a new SimulationDrivebase. */
   public SimulationDrivebase() {}
 
@@ -15,6 +31,8 @@ public class SimulationDrivebase extends AbstractDrivebase {
   protected void setSpeeds_HAL(DifferentialDriveWheelSpeeds speeds) {
       // TODO Auto-generated method stub
       
+
+    SmartDashboard.putData("field", m_fieldSim);
   }
   
   @Override

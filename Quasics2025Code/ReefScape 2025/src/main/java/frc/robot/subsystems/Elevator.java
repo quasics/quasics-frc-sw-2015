89e1339805c22ba53c512d301c7a3bbd537233ab
Matt Healy <mjhealy@alum.rit.edu>
2025-02-12 08:41:55 -0500

    Removed unused imports.

--- Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/subsystems/Elevator.java
+++ Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/subsystems/Elevator.java
@@ -5,10 +5,8 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;

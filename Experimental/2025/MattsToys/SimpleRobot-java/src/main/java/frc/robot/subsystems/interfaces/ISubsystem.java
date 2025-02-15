5889085f28d217558a587e0bef995bb114bd1c93
Matt Healy <mjhealy@alum.rit.edu>
2025-02-15 16:16:27 -0500

    Doc updates.

--- Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/interfaces/ISubsystem.java
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/interfaces/ISubsystem.java
@@ -1,7 +1,15 @@
+// Copyright (c) 2025, Matthew J. Healy and other Quasics contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems.interfaces;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
+/**
+ * Simple interface to be used as a basis for defining subsystem-specific
+ * interfaces.
+ */
 public interface ISubsystem {
   /** Convert the object to a subsystem (for listing in requirements). */
   default Subsystem asSubsystem() {

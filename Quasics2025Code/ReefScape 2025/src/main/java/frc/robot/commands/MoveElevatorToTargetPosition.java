df3237db701a8a13fbc43e455d7832667403c608
Matt Healy <mjhealy@alum.rit.edu>
2025-02-15 12:15:38 -0500

    Finished initial implementation of move to target position command.

--- Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/commands/MoveElevatorToTargetPosition.java
+++ Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/commands/MoveElevatorToTargetPosition.java
@@ -26,21 +26,18 @@ public class MoveElevatorToTargetPosition extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
+    m_elevator.setTargetPosition(m_targetPosition);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_elevator.stop();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return m_elevator.getVelocity() == 0;
   }
 }

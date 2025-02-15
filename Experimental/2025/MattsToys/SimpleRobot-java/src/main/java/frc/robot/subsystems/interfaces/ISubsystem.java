b17cbb1454764b7d68332c10c6c20e00e87c7bdf
Matt Healy <mjhealy@alum.rit.edu>
2025-02-15 16:07:07 -0500

    Formatting.

--- Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/interfaces/ISubsystem.java
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/interfaces/ISubsystem.java
@@ -3,8 +3,8 @@ package frc.robot.subsystems.interfaces;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public interface ISubsystem {
-    /** Convert the object to a subsystem (for listing in requirements). */
-    default Subsystem asSubsystem() {
-        return (Subsystem) this;
-    }
+  /** Convert the object to a subsystem (for listing in requirements). */
+  default Subsystem asSubsystem() {
+    return (Subsystem) this;
+  }
 }

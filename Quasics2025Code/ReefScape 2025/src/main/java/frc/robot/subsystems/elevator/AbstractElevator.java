8ccb7d25a4cb9a93ad7c2f8bd155818af3a35c03
Matt Healy <mjhealy@alum.rit.edu>
2025-02-15 11:52:20 -0500

    Added "Don't care" value for target position.

--- Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/subsystems/elevator/AbstractElevator.java
+++ Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/subsystems/elevator/AbstractElevator.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public abstract class AbstractElevator extends SubsystemBase {
 
   public enum TargetPosition {
+    kDontCare,
     kBottom,
     kL1,
     kL2,

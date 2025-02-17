3857f76321ae0aef3dcdecf38d197d670a7e5d91
Matt Healy <mjhealy@alum.rit.edu>
2025-02-17 10:42:43 -0500

    Doc updates.

--- Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/AbstractElevator.java
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/AbstractElevator.java
@@ -9,12 +9,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 /**
  * Provides pretty basic control for a single-mechanism "elevator" that can be
  * raised/lowered, including a "safety mode" that is intended to prevent it from
- * being wound too far.
+ * being wound too far though this assumes that we'd always be starting up the
+ * code with the elevator fully lowered, so that "0 in code" is "0 in reality").
  * 
  * Note that this is an example of using an "abstract" class as the base for one
  * or more derived types, where the base (a) implements the general
  * functionality and (b) defines the hardware-specific pieces that the derived
  * classes then fill in.
+ * 
+ * Possible improvements would include:
+ * <ul>
+ * <li>Limit switches for safety controls at top and bottom</li>
+ * <li>Assuming that there were (good) hard stops on the elevator, monitoring
+ * the
+ * velocity of the motor to detect when we run up against them</li>
+ * <li>Supporting "known positions" for the elevator, to allow client code to
+ * drive to a predefined height</li>
+ * </ul>
  */
 public abstract class AbstractElevator extends SubsystemBase {
   public static final String NAME = "Elevator";

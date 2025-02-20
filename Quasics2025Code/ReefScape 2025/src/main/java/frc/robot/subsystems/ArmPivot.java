49f12ebe1bc6ebd7ab48a2d2c877a71c71c56701
Matt Healy <mjhealy@alum.rit.edu>
2025-02-20 18:38:41 -0500

    Removed unused import.

--- Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/subsystems/ArmPivot.java
+++ Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/subsystems/ArmPivot.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Constants.CanBusIds.SparkMaxIds;
 import frc.robot.Constants.ArmPIDConstants;
 

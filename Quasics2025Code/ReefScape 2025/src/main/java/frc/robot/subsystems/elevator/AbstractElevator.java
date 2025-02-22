c32760e6b8ae02e0727fac9e0ecea3735ee286aa
Matt Healy <mjhealy@alum.rit.edu>
2025-02-22 10:46:47 -0500

    Removed unused import.

--- Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/subsystems/elevator/AbstractElevator.java
+++ Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/subsystems/elevator/AbstractElevator.java
@@ -4,8 +4,6 @@
 
 package frc.robot.subsystems.elevator;
 
-import com.revrobotics.spark.SparkClosedLoopController;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 

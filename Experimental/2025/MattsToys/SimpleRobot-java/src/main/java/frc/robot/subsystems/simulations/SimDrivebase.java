4f164c920f59120e5d093a3d2546d6954797bc86
Matt Healy <mjhealy@alum.rit.edu>
2025-02-23 01:37:37 -0500

    Removing unused imports.

--- Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/simulations/SimDrivebase.java
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/simulations/SimDrivebase.java
@@ -13,8 +13,6 @@ import edu.wpi.first.math.controller.LTVUnicycleController;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;

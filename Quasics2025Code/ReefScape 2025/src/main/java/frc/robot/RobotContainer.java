8741478f51302d8ffd082765566411dafbc012d0
Quasics FRC <quasics_driverStation@mersgroup.com>
2025-03-01 10:45:51 -0500

    added test velocity and acceleration constraints to all paths

--- Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/RobotContainer.java
+++ Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/RobotContainer.java
@@ -169,6 +169,7 @@ public class RobotContainer {
 
     SmartDashboard.putData("Arm Pivot PID Up", new MoveArmPivotToPosition(m_armPivot, Radians.of(1.5708)));
     SmartDashboard.putData("Arm Pivot PID Down", new MoveArmPivotToPosition(m_armPivot, Radians.of(0)));
+    SmartDashboard.putData("Arm Pivot 45", new MoveArmPivotToPosition(m_armPivot, Degrees.of(45)));
 
     SmartDashboard.putData("Stop arm pivot", new InstantCommand(() -> m_armPivot.stop()));
 

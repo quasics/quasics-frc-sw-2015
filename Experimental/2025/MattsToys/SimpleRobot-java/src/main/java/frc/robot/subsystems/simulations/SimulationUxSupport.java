fa0f246a5adcd303a2cc55194dccf48a12ce1120
Matt Healy <mjhealy@alum.rit.edu>
2025-03-08 12:20:04 -0500

    Moving arm UX into SimulationUxSupport.

--- Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/simulations/SimulationUxSupport.java
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/simulations/SimulationUxSupport.java
@@ -4,8 +4,10 @@
 
 package frc.robot.subsystems.simulations;
 
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Meters;
 
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
@@ -16,47 +18,12 @@ import frc.robot.subsystems.abstracts.AbstractElevator;
 /** Add your docs here. */
 public class SimulationUxSupport {
 
-  public static final SimulationUxSupport instance = new SimulationUxSupport();
-
-  private SimulationUxSupport() {
-    // Simulation rendering setup.
-    Mechanism2d rootMech2d = new Mechanism2d(
-        9,
-        AbstractElevator.MAX_SAFE_HEIGHT.in(Meters) * 1.15 // Leave a little room at the top
-    );
-    m_elevatorMech2d = rootMech2d.getRoot("Root", 3, 0)
-        .append(new MechanismLigament2d("LeftClimber", 0, 90));
-
-    // Publish Mechanism2d to SmartDashboard.
-    // To show the visualization, select Network Tables -> SmartDashboard
-    // -> Elevator Sim
-    SmartDashboard.putData("Elevator Sim", rootMech2d);
-  }
-
-  public enum DeviceStatus {
-    Manual, AtSetpoint, NotAtSetpoint
-  }
-
-  public void updateElevator(Distance height, DeviceStatus status) {
-    m_elevatorMech2d.setLength(height.in(Meters));
-    switch (status) {
-      case Manual:
-        m_elevatorMech2d.setColor(NO_SETPOINT);
-        break;
-      case AtSetpoint:
-        m_elevatorMech2d.setColor(AT_SETPOINT);
-        break;
-      case NotAtSetpoint:
-        m_elevatorMech2d.setColor(NOT_AT_SETPOINT);
-        break;
-    }
-  }
-
   /**
    * Mechanism2d visualization of the hardware (for rendering in SmartDashboard,
    * or the simulator).
    */
   private final MechanismLigament2d m_elevatorMech2d;
+  private final MechanismLigament2d m_armMech2d;
 
   /** Color used to render the elevator when running under manual control. */
   private final static Color8Bit NO_SETPOINT = new Color8Bit(255, 165, 0);
@@ -71,5 +38,58 @@ public class SimulationUxSupport {
    * position .
    */
   private final static Color8Bit NOT_AT_SETPOINT = new Color8Bit(255, 0, 0);
+
+  public static final SimulationUxSupport instance = new SimulationUxSupport();
+
+  private SimulationUxSupport() {
+    // Simulation rendering setup.
+    Mechanism2d rootMech2d = new Mechanism2d(
+        9,
+        AbstractElevator.MAX_SAFE_HEIGHT.in(Meters) * 1.15 // Leave a little room at the top
+    );
+    m_elevatorMech2d = rootMech2d.getRoot("Root", 5, 0)
+        .append(new MechanismLigament2d("LeftClimber", 0, 90));
+
+    m_armMech2d = m_elevatorMech2d.append(
+        new MechanismLigament2d("Arm", 3, -90));
+
+    // Publish Mechanism2d to SmartDashboard.
+    // To show the visualization, select Network Tables -> SmartDashboard
+    // -> Elevator Sim
+    SmartDashboard.putData("Elevator Sim", rootMech2d);
+
+    updateElevator(Meters.of(0), DeviceStatus.Manual);
+    updateArm(Degrees.of(0), DeviceStatus.Manual);
+  }
+
+  public enum DeviceStatus {
+    Manual, AtSetpoint, NotAtSetpoint
+  }
+
+  final static private double ARM_ANGLE_OFFSET = -90;
+
+  public void updateArm(Angle angle, DeviceStatus status) {
+    m_armMech2d.setAngle(angle.in(Degrees) + ARM_ANGLE_OFFSET);
+    updateDeviceStatus(m_armMech2d, status);
+  }
+
+  public void updateElevator(Distance height, DeviceStatus status) {
+    m_elevatorMech2d.setLength(height.in(Meters));
+    updateDeviceStatus(m_elevatorMech2d, status);
+  }
+
+  private static void updateDeviceStatus(MechanismLigament2d mech, DeviceStatus status) {
+    switch (status) {
+      case Manual:
+        mech.setColor(NO_SETPOINT);
+        break;
+      case AtSetpoint:
+        mech.setColor(AT_SETPOINT);
+        break;
+      case NotAtSetpoint:
+        mech.setColor(NOT_AT_SETPOINT);
+        break;
+    }
+  }
 
 }

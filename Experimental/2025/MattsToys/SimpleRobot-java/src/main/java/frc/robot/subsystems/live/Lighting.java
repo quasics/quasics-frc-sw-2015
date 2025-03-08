a253938c613ffc69658a0ed40e21574f05972377
Matt Healy <mjhealy@alum.rit.edu>
2025-03-08 13:09:08 -0500

    Removed unused imports.

--- Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/live/Lighting.java
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/live/Lighting.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems.live;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.interfaces.ILighting;

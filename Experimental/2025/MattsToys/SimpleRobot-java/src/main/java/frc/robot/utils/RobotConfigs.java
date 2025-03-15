509acea483713515d69501d144211c554074aeea
Matt Healy <mjhealy@alum.rit.edu>
2025-03-15 14:46:34 -0400

    Resolving a to-do.

--- Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/utils/RobotConfigs.java
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/utils/RobotConfigs.java
@@ -397,10 +397,9 @@ public class RobotConfigs {
 
   private static RobotConfig generateAmeliaConfig() {
     return new RobotConfig(
-        // TODO: Update DriveConfig data to match Amelia's configuration.
         new DriveConfig(Inches.of(3), // Wheel radius
             Meters.of(0.5628) /* 22 in (from 27Feb2025) */,
-            8.45, // Gearing (from 2024)
+            10.71, // Gearing (from 15Mar2025)
             new PIDConfig(0.046218), // Left (from 27Feb2025)
             new PIDConfig(0.066374), // Right (from 27Feb2025)
             new DriveFeedForwardConfig(

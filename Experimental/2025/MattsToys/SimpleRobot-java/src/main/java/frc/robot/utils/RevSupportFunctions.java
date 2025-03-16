6d7bb0cc1fb1f2a58b1f4a49b5586df0370c8a64
Matt Healy <mjhealy@alum.rit.edu>
2025-03-16 14:21:06 -0400

    Copyright updates.

--- Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/utils/RevSupportFunctions.java
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/utils/RevSupportFunctions.java
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) 2025, Matthew J. Healy and other Quasics contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -7,7 +7,6 @@ package frc.robot.utils;
 import static edu.wpi.first.units.Units.Meters;
 
 import com.revrobotics.spark.config.AbsoluteEncoderConfig;
-import com.revrobotics.spark.config.EncoderConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.units.measure.Distance;

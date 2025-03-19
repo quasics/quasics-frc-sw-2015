72b58bc4aa656c812cc57532569188f2397550c6
Matt Healy <mjhealy@alum.rit.edu>
2025-03-18 20:22:52 -0400

    Adding native Candle configuration.

--- Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/live/Candle.java
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/subsystems/live/Candle.java
@@ -5,6 +5,9 @@
 package frc.robot.subsystems.live;
 
 import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.CANdle.LEDStripType;
+import com.ctre.phoenix.led.CANdleConfiguration;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.interfaces.ICandle;
 import frc.robot.utils.RobotConfigs.RobotConfig;
@@ -25,6 +28,26 @@ public class Candle extends SubsystemBase implements ICandle {
     setName("Candle");
 
     m_candle = new CANdle(config.candle().canId());
+
+    CANdleConfiguration configAll = new CANdleConfiguration();
+
+    // LEDs built into the device are RGB
+    configAll.stripType = LEDStripType.RGB;
+
+    // Turn off Status LED when CANdle is actively being controlled
+    configAll.statusLedOffWhenActive = true;
+
+    // Leave LEDs on when Loss of Signal occurs
+    configAll.disableWhenLOS = false;
+
+    // Dim the LEDs to 50% brightness
+    configAll.brightnessScalar = 0.5;
+
+    // True to turn off the 5V rail. This turns off the on-board LEDs as well.
+    // (So if we're using the on-board LEDs, it should be false.)
+    configAll.v5Enabled = false;
+
+    m_candle.configAllSettings(configAll, 100);
   }
 
   @Override

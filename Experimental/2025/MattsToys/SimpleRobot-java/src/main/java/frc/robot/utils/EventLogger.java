a08def32d85bcd8e85aa77145e929e4096e00f1f
Matt Healy <mjhealy@alum.rit.edu>
2025-03-21 10:25:06 -0400

    Docs.

--- Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/utils/EventLogger.java
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/utils/EventLogger.java
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) 2025, Matthew J. Healy and other Quasics contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -12,7 +12,11 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.time.Instant;
 
-/** Add your docs here. */
+/**
+ * Defines an abstract type to support logging operations/events on the robot.
+ *
+ * TODO: Move this into a subpackage and break out the types (for readability).
+ */
 public abstract class EventLogger {
   //////////////////////////////////////////////////////////////////////
   //

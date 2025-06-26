8b53bbcf2ec330f4b55e578efc4a2b9f6b17b2d8
Nicky Baldy <baldy.nicole@gmail.com>
2025-06-26 19:59:09 -0400

    Kitbot 2025 code added without modifications
    Kitbot code from https://www.firstinspires.org/resource-library/frc/kitbot

---
+++ Quasics2025Code/KitBot2025/Inline Commands/src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
@@ -0,0 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.function.DoubleSupplier;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+
+public class CANDriveSubsystem extends SubsystemBase {
+  private final SparkMax leftLeader;
+  private final SparkMax leftFollower;
+  private final SparkMax rightLeader;
+  private final SparkMax rightFollower;
+
+  private final DifferentialDrive drive;
+
+  public CANDriveSubsystem() {
+    // create brushed motors for drive
+    leftLeader = new SparkMax(DriveConstants.LEFT_LEADER_ID, MotorType.kBrushed);
+    leftFollower = new SparkMax(DriveConstants.LEFT_FOLLOWER_ID, MotorType.kBrushed);
+    rightLeader = new SparkMax(DriveConstants.RIGHT_LEADER_ID, MotorType.kBrushed);
+    rightFollower = new SparkMax(DriveConstants.RIGHT_FOLLOWER_ID, MotorType.kBrushed);
+
+    // set up differential drive class
+    drive = new DifferentialDrive(leftLeader, rightLeader);
+
+    // Set can timeout. Because this project only sets parameters once on
+    // construction, the timeout can be long without blocking robot operation. Code
+    // which sets or gets parameters during operation may need a shorter timeout.
+    leftLeader.setCANTimeout(250);
+    rightLeader.setCANTimeout(250);
+    leftFollower.setCANTimeout(250);
+    rightFollower.setCANTimeout(250);
+
+    // Create the configuration to apply to motors. Voltage compensation
+    // helps the robot perform more similarly on different
+    // battery voltages (at the cost of a little bit of top speed on a fully charged
+    // battery). The current limit helps prevent tripping
+    // breakers.
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.voltageCompensation(12);
+    config.smartCurrentLimit(DriveConstants.DRIVE_MOTOR_CURRENT_LIMIT);
+
+    // Set configuration to follow leader and then apply it to corresponding
+    // follower. Resetting in case a new controller is swapped
+    // in and persisting in case of a controller reset due to breaker trip
+    config.follow(leftLeader);
+    leftFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    config.follow(rightLeader);
+    rightFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    // Remove following, then apply config to right leader
+    config.disableFollowerMode();
+    rightLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // Set conifg to inverted and then apply to left leader. Set Left side inverted
+    // so that postive values drive both sides forward
+    config.inverted(true);
+    leftLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  // Command to drive the robot with joystick inputs
+  public Command driveArcade(
+      CANDriveSubsystem driveSubsystem, DoubleSupplier xSpeed, DoubleSupplier zRotation) {
+    return Commands.run(
+        () -> drive.arcadeDrive(xSpeed.getAsDouble(), zRotation.getAsDouble()), driveSubsystem);
+  }
+}

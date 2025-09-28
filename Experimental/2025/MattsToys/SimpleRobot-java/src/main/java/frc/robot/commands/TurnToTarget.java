d68cf4e7332dfdefa67d2c2e3b8dc7820b86c5e2
Matt Healy <mjhealy@alum.rit.edu>
2025-09-28 12:12:13 -0400

    Beginning to add TurnToTarget cmd.

---
+++ Experimental/2025/MattsToys/SimpleRobot-java/src/main/java/frc/robot/commands/TurnToTarget.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.interfaces.IDrivebase;
+import frc.robot.subsystems.interfaces.IVisionPlus;
+
+/* You should consider using the more terse Command factories API instead
+ * https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands
+ */
+public class TurnToTarget extends Command {
+  private final IVisionPlus vision;
+  private final IDrivebase drivebase;
+  private final int targetId;
+
+  static private final AngularVelocity SEEKING_SPEED = DegreesPerSecond.of(15);
+
+  /** Creates a new TurnToTarget. */
+  public TurnToTarget(IVisionPlus vision, IDrivebase drivebase, int targetId) {
+    this.vision = vision;
+    this.drivebase = drivebase;
+    this.targetId = targetId;
+
+    addRequirements(vision.asSubsystem(), drivebase.asSubsystem());
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (!vision.isTargetVisible(targetId)) {
+      drivebase.arcadeDrive(null, SEEKING_SPEED);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    drivebase.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return vision.isTargetVisible(targetId);
+  }
+}

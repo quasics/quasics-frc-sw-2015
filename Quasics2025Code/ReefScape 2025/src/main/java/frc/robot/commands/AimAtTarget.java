03bae0fec5073c35a7f6bc3fb894de0dbcd0bcb2
Quasics FRC <quasics_driverStation@mersgroup.com>
2025-11-01 14:57:43 -0400

    functionality to aimattarget command for 2 modes: target in sight and pointing at target, tested

--- Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/commands/AimAtTarget.java
+++ Quasics2025Code/ReefScape 2025/src/main/java/frc/robot/commands/AimAtTarget.java
@@ -15,18 +15,24 @@ import java.util.function.Supplier;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class AimAtTarget extends Command {
+  public enum Mode {
+    TargetInSight, PointAtTarget
+  }
 
   private final AbstractDrivebase m_drivebase;
   private final Vision m_vision;
-  private boolean hasTarget = false;
-  private boolean m_aligned = false;
+  private boolean m_inSight = false;
   private int m_fiducialId;
+  private boolean m_aligned = false;
+  private boolean m_finished = false;
+  private final Mode m_mode;
 
   /** Creates a new AimAtTarget. */
-  public AimAtTarget(AbstractDrivebase drivebase, Vision vision, int fiducialId) {
+  public AimAtTarget(AbstractDrivebase drivebase, Vision vision, int fiducialId, Mode mode) {
     m_drivebase = drivebase;
     m_vision = vision;
     m_fiducialId = fiducialId;
+    m_mode = mode;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(drivebase, vision);
   }
@@ -34,7 +40,7 @@ public class AimAtTarget extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_aligned = false;
+    m_finished = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -42,14 +48,29 @@ public class AimAtTarget extends Command {
   public void execute() {
     // if target is in view just align, if target is not in view then turn towards
     // it
+    AngularVelocity av = DegreesPerSecond.of(10);
     Angle angle = m_vision.getTargetAngleDegrees(m_fiducialId);
     if (angle == null) {
       System.out.println("cannot see target");
-    } else {
-      System.out.println("tag is in sight");
-      m_aligned = true;
+      m_drivebase.arcadeDrive(MetersPerSecond.of(0), DegreesPerSecond.of(40));
+      return;
     }
-
+    if (angle != null && m_mode == Mode.TargetInSight) {
+      System.out.println("tag is in sight");
+      m_finished = true;
+    }
+    if (angle != null && m_mode == Mode.PointAtTarget) {
+      if (angle.gt(Degrees.of(1))) {
+        m_drivebase.arcadeDrive(MetersPerSecond.of(0), DegreesPerSecond.of(8));
+      }
+      if (angle.lt(Degrees.of(1))) {
+        av = DegreesPerSecond.of(-8);
+        m_drivebase.arcadeDrive(MetersPerSecond.of(0), av);
+      } else {
+        m_drivebase.stop();
+        m_finished = true;
+      }
+    }
   }
 
   // Called once the command ends or is interrupted.
@@ -61,6 +82,6 @@ public class AimAtTarget extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_aligned;
+    return m_finished;
   }
 }

7ec7279d6389edebdf6d10257fab84c06e2b695f
Matt Healy <mjhealy@alum.rit.edu>
2025-11-20 18:33:58 -0500

    Doc updates

--- Experimental/2025/TrainingBot/src/main/java/frc/robot/commands/TankDrive.java
+++ Experimental/2025/TrainingBot/src/main/java/frc/robot/commands/TankDrive.java
@@ -18,11 +18,34 @@ import java.util.function.Supplier;
  *      https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands
  */
 public class TankDrive extends Command {
+  /**
+   * The drive base we're working with. (Abstract, so that we can work with either
+   * the real hardware or simulation.)
+   */
   private final AbstractDrivebase m_drivebase;
+
+  /**
+   * Used to get a "double" value, to use in controlling left-side wheel speeds.
+   * 
+   * @see https://docs.oracle.com/javase/8/docs/api/java/util/function/Supplier.html
+   */
   private final Supplier<Double> m_leftSupplier;
+
+  /**
+   * Used to get a "double" value, to use in controlling right-side wheel speeds.
+   * 
+   * @see https://docs.oracle.com/javase/8/docs/api/java/util/function/Supplier.html
+   */
   private final Supplier<Double> m_rightSupplier;
 
-  /** Creates a new TankDrive. */
+  /**
+   * Creates a new TankDrive command.
+   * 
+   * @param drivebase     the drive base object we're actually using to control
+   *                      the robot
+   * @param leftSupplier  the Supplier object providing left-side speed values
+   * @param rightSupplier the Supplier object providing right-side speed values
+   */
   public TankDrive(
       AbstractDrivebase drivebase, Supplier<Double> leftSupplier, Supplier<Double> rightSupplier) {
     m_drivebase = drivebase;
@@ -32,34 +55,45 @@ public class TankDrive extends Command {
     addRequirements(m_drivebase);
   }
 
+  /** Helper method to set the left/right speed values. */
   private void updateSpeeds() {
     m_drivebase.tankDrive(m_leftSupplier.get(), m_rightSupplier.get());
   }
 
-  // Called when the command is initially scheduled.
+  // Called when the command is initially scheduled. ("Start whatever the command
+  // does.")
   @Override
   public void initialize() {
     updateSpeeds();
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
+  // Called every time the scheduler runs while the command is scheduled. ("Make
+  // any changes that are needed.")
   @Override
   public void execute() {
     updateSpeeds();
   }
 
-  // Called once the command ends or is interrupted.
+  // Called once the command ends or is interrupted. ("Do any shutdown/cleanup
+  // that's needed when the command is done.")
   @Override
   public void end(boolean interrupted) {
     m_drivebase.stop();
   }
 
-  // Returns true when the command should end.
-  //
-  // Note that this command never ends, unless it's interrupted because some other
-  // command requires the subsystem. (This is a common pattern for default
-  // commands; we could leave this function out, since the Command base class
-  // already does exactly this, but it's left here to be explicit as an example.)
+  /**
+   * Standard "Command" class method that signals when the command has finished
+   * running.
+   * 
+   * Note that this particular command *never* ends, unless it's interrupted
+   * because some other command requires the subsystem.
+   * 
+   * This is a common pattern for default commands; we could leave this function
+   * out, since the Command base class already does exactly this, but it's left
+   * here to be an explicit example.)
+   * 
+   * @return true if the command has finished doing its job
+   */
   @Override
   public boolean isFinished() {
     return false;

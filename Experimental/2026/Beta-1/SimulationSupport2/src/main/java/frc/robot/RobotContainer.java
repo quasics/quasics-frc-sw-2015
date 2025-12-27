6066ad021e61ff6fb632a326fcd578aec36b8716
Matt Healy <mjhealy@alum.rit.edu>
2025-12-27 14:04:40 -0500

    [SimSupport] Added drive controller selection.

--- Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/RobotContainer.java
+++ Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,14 @@
 
 package frc.robot;
 
+import java.util.function.Consumer;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.simulation.SendableChooserSim;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.ArcadeDrive;
@@ -24,7 +29,52 @@ public class RobotContainer {
     configureBindings();
   }
 
+  private enum DriveControl {
+    KEYBOARD1,
+    ALT_KEYBOARD1,
+    LOGITECH_CONTROLLER;
+
+    public String getControlSchemeName() {
+      switch (this) {
+        case KEYBOARD1:
+          return "Keyboard1";
+        case ALT_KEYBOARD1:
+          return "Alt-Keyboard1";
+        case LOGITECH_CONTROLLER:
+          return "Logitech Controller";
+        default:
+          return "Unknown";
+      }
+    }
+  }
+
+  DriveControl currentControlScheme = DriveControl.KEYBOARD1;
+
+  private void updateControlScheme(DriveControl controlScheme) {
+    currentControlScheme = controlScheme;
+    System.out.println("Control scheme set to: " + controlScheme.getControlSchemeName());
+  }
+
   private void configureBindings() {
+    SendableChooser<DriveControl> driveInputChooser = new SendableChooser<DriveControl>();
+    for (var option : DriveControl.values()) {
+      if (option.ordinal() == 0) {
+        driveInputChooser.setDefaultOption(option.getControlSchemeName(), option);
+      } else {
+        driveInputChooser.addOption(option.getControlSchemeName(), option);
+      }
+    }
+    driveInputChooser.onChange(this::updateControlScheme);
+    currentControlScheme = driveInputChooser.getSelected();
+    SmartDashboard.putData("Drive control", driveInputChooser);
+
+    SendableChooser<Command> chooser = new SendableChooser<Command>();
+    chooser.setDefaultOption("No Auto", Commands.print("No autonomous command configured"));
+    chooser.addOption("Do something", Commands.print("Do something"));
+    SmartDashboard.putData("Autonomous Command", chooser);
+
+    // SendableChooserSim chooserSim = new SendableChooserSim("choosersim");
+    // SmartDashboard.putData("Chooser sim", chooserSim);
   }
 
   public Command getAutonomousCommand() {
@@ -35,12 +85,20 @@ public class RobotContainer {
   private final SlewRateLimiter rotationSlewRateLimiter = new SlewRateLimiter(OperatorConstants.MAX_SLEW_RATE);
 
   public Double getArcadeForward() {
-    final double forward = m_driveController.getRawAxis(LogitechConstants.Dualshock.LeftXAxis);
+    final double forward = switch (currentControlScheme) {
+      case KEYBOARD1 -> m_driveController.getRawAxis(0); // Mapped to D/A keys
+      case ALT_KEYBOARD1 -> -m_driveController.getRawAxis(1); // Mapped to W/S keys
+      case LOGITECH_CONTROLLER -> -m_driveController.getRawAxis(LogitechConstants.Dualshock.LeftYAxis);
+    };
     return forwardSlewRateLimiter.calculate(MathUtil.applyDeadband(forward, OperatorConstants.DEADBAND_THRESHOLD));
   }
 
   public Double getArcadeRotation() {
-    final double rotation = -m_driveController.getRawAxis(LogitechConstants.Dualshock.LeftYAxis);
+    final double rotation = switch (currentControlScheme) {
+      case ALT_KEYBOARD1 -> m_driveController.getRawAxis(0); // Mapped to D/A keys
+      case KEYBOARD1 -> -m_driveController.getRawAxis(1); // Mapped to W/S keys
+      case LOGITECH_CONTROLLER -> -m_driveController.getRawAxis(LogitechConstants.Dualshock.RightXAxis);
+    };
     return rotationSlewRateLimiter.calculate(MathUtil.applyDeadband(rotation, OperatorConstants.DEADBAND_THRESHOLD));
   }
 }

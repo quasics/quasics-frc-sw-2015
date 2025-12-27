7f2f81abd27e062ddb2f0f3d8382b14e804913d3
Matt Healy <mjhealy@alum.rit.edu>
2025-12-27 12:21:57 -0500

    [SimSupport] Cloning code to work around RevLib crash.

---
+++ Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/subsystems/interfaces/IDrivebase.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.interfaces;
+
+/** Interface for a drivebase subsystem. */
+public interface IDrivebase extends ISubsystem {
+  String SUBSYSTEM_NAME = "Drivebase";
+
+  /**
+   * "Classic" arcade-style driving, based on percentages. (Note: operates
+   * directly; no PID.)
+   * 
+   * @param forward  forward speed (-1.0 to 1.0)
+   * @param rotation rotation rate (-1.0 to 1.0)
+   */
+  void driveArcade(double forward, double rotation);
+
+  /**
+   * "Classic" tank-style driving, based on percentages. (Note: operates directly;
+   * no PID.)
+   * 
+   * @param leftSpeed  speed for the left side (-1.0 to 1.0)
+   * @param rightSpeed speed for the right side (-1.0 to 1.0)
+   */
+  void driveTank(double leftSpeed, double rightSpeed);
+
+  /** Stops all motion of the drivebase. */
+  default void stop() {
+    driveTank(0.0, 0.0);
+  }
+}
---
+++ Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/subsystems/interfaces/ISubsystem.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.interfaces;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public interface ISubsystem {
+  default Subsystem asSubsystem() {
+    return (Subsystem) this;
+  }
+}

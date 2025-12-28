450ff104ae9bb857e951a32abbefb7d4f0f2eccc
Matt Healy <mjhealy@alum.rit.edu>
2025-12-27 22:39:28 -0500

    [SimSupport] More doc updates/cleanup.

--- Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/util/DriverJoystickWrapper.java
+++ Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/util/DriverJoystickWrapper.java
@@ -1,7 +1,10 @@
+// Copyright (c) 2025, Matthew J. Healy and other Quasics contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.util;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;

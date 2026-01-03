2bd2b6775d431a9b17888d953b565bbb20093d0c
Matt Healy <mjhealy@alum.rit.edu>
2026-01-03 13:29:27 -0500

    Doc updates.

--- Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/RobotContainer.java
+++ Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/RobotContainer.java
@@ -210,7 +210,7 @@ public class RobotContainer {
             new Pose2d(0, 0, new Rotation2d(0)),
             // Pass through these two interior waypoints, making an 's' curve path
             List.of(new Translation2d(2, 1), new Translation2d(4, -1)),
-            // End 3 meters straight ahead of where we started, facing forward
+            // End 6 meters straight ahead of where we started, facing forward
             new Pose2d(6, 0, new Rotation2d(0)),
             // Pass config
             config);

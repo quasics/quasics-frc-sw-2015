5ec88aeb7c2f003d5cca73d012242811c842665d
Matt Healy <mjhealy@alum.rit.edu>
2026-01-04 10:31:30 -0500

    [SimSupport] Removed unused import.

--- Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/RobotContainer.java
+++ Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/RobotContainer.java
@@ -22,7 +22,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;

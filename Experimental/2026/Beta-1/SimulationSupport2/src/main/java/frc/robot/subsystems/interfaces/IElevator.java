dc1ab76a05f0b6327f3668f7d36c020c94824917
Matt Healy <mjhealy@alum.rit.edu>
2026-01-04 10:29:19 -0500

    Doc updates.

--- Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/subsystems/interfaces/IElevator.java
+++ Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/subsystems/interfaces/IElevator.java
@@ -10,10 +10,30 @@ public interface IElevator extends ISubsystem {
   final String SUBSYSTEM_NAME = "Elevator";
 
   /** The state of the elevator. */
-  enum ElevatorState { IDLE, MOVING_TO_POSITION, MANUAL_CONTROL }
+  enum ElevatorState {
+    /** The elevator is idle. */
+    IDLE,
+    /** The elevator is moving to a position under PID control. */
+    MOVING_TO_POSITION,
+    /** The elevator is under manual control. */
+    MANUAL_CONTROL,
+  }
 
   /** The predefined positions for the elevator. */
-  enum ElevatorPosition { BOTTOM, LOW, MEDIUM, HIGH, TOP, MANUAL_CONTROL }
+  enum ElevatorPosition {
+    /** The elevator is at the bottom (lower limit). */
+    BOTTOM,
+    /** The elevator is at the low position. */
+    LOW,
+    /** The elevator is at the medium position. */
+    MEDIUM,
+    /** The elevator is at the high position. */
+    HIGH,
+    /** The elevator is at the top (upper limit). */
+    TOP,
+    /** The elevator is under manual control (so, it could be anywhere right now). */
+    MANUAL_CONTROL,
+  }
 
   /** Stops the elevator. */
   void stop();
@@ -35,10 +55,16 @@ public interface IElevator extends ISubsystem {
    */
   ElevatorState getElevatorState();
 
-  /** Moves the elevator down (under manual control). */
+  /**
+   * Moves the elevator down (under manual control).  It will automatically stop when it reaches
+   * its bottom limit.
+   */
   void down();
 
-  /** Moves the elevator up (under manual control). */
+  /**
+   * Moves the elevator up (under manual control). It will automatically stop when it reaches its
+   * top limit.
+   */
   void up();
 
   /** Gets the current height of the elevator. */

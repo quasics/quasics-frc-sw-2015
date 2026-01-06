eaa04d3b9cb223b3798412bf69608822191f8f57
Matt Healy <mjhealy@alum.rit.edu>
2026-01-06 16:56:12 -0500

    [SimSupport] Adding basic vision interface definitions.

---
+++ Experimental/2026/Beta-1/SimulationSupport2/src/main/java/frc/robot/subsystems/interfaces/IPhotonVision.java
@@ -0,0 +1,44 @@
+package frc.robot.subsystems.interfaces;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Transform3d;
+import java.util.List;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+
+/**
+ * Interface for an explicitly PhotonVision-based vision subsystem.
+ */
+public interface IPhotonVision extends IVision {
+  /**
+   * Camera data set.
+   *
+   * @param camera      connection to the camera
+   * @param transform3d defines the conversion from the robot's position, to the
+   *                    cameras's
+   * @param estimator   pose estimator associated with this camera. Note that (per
+   *                    docs) the estimated poses can have a lot of
+   *                    uncertainty/error baked into them when you are further
+   *                    away from the targets.
+   */
+  public record
+      CameraData(PhotonCamera camera, Transform3d transform3d, PhotonPoseEstimator estimator) {}
+
+  /**
+   * Returns the list of CameraData records being used by this object.
+   *
+   * Note: this is exposed as public only for use with SimVisionWrapper.
+   *
+   * @return a list of CameraData objects
+   */
+  List<CameraData> getCameraDataForSimulation();
+
+  /**
+   * The AprilTagFieldLayout being used by this object.
+   *
+   * Note: this is exposed as public only for use with SimVisionWrapper.
+   *
+   * @return an AprilTagFieldLayout
+   */
+  AprilTagFieldLayout getFieldLayoutForSimulation();
+}

8dba7daeb8f0f0625dc8b987f59afa2aa9a43e2a
Matt Healy <mjhealy@alum.rit.edu>
2026-01-10 12:12:10 -0500

    Doc updates

--- Experimental/2026/MattsToys/MultiHardwareExample/src/main/java/frc/robot/subsystems/interfaces/ISingleMotorThing.java
+++ Experimental/2026/MattsToys/MultiHardwareExample/src/main/java/frc/robot/subsystems/interfaces/ISingleMotorThing.java
@@ -2,9 +2,28 @@ package frc.robot.subsystems.interfaces;
 
 import edu.wpi.first.units.measure.Distance;
 
+/**
+ * Defines a simple interface to something running on a single motor.
+ *
+ * This is intended to be an example of an interface to some aspect of the robot, which would then
+ * be implemented for specific hardware variants.
+ */
 public interface ISingleMotorThing {
-    void setSpeed(double percent);
-    double getSpeed();
-    Distance getPosition();
-    default void stop() { setSpeed(0); }
+  /**
+   * Sets the speed of the motor.
+   *
+   * @param percent percent speed (a value in the range [-1.0, +1.0])
+   */
+  void setSpeed(double percent);
+
+  /** Returns the motor speed. */
+  double getSpeed();
+
+  /** Returns the current position reading for the encoder on the "single motor thing". */
+  Distance getPosition();
+
+  /** Utility method to stop the motor. */
+  default void stop() {
+    setSpeed(0);
+  }
 }
--- Experimental/2026/MattsToys/MultiHardwareExample/src/main/java/frc/robot/subsystems/simulation/SingleMotorThingSim.java
+++ Experimental/2026/MattsToys/MultiHardwareExample/src/main/java/frc/robot/subsystems/simulation/SingleMotorThingSim.java
@@ -9,43 +9,51 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.interfaces.ISingleMotorThing;
 
+/**
+ * An example of how the interface to the subsystem could be directly implemented.
+ */
 public class SingleMotorThingSim extends SubsystemBase implements ISingleMotorThing {
+  /** Encoder ticks per revolution. */
+  public static final int ENCODER_TICKS_PER_REVOLUTION = -4096;
 
-    /** Encoder ticks per revolution. */
-    public static final int ENCODER_TICKS_PER_REVOLUTION = -4096;
+  /** Wheel diameter in inches. */
+  public static final Distance WHEEL_DIAMETER = Inches.of(6);
 
-    /** Wheel diameter in inches. */
-    public static final Distance WHEEL_DIAMETER = Inches.of(6);
+  private final PWMSparkMax m_motor = new PWMSparkMax(1);
+  private final Encoder m_encoder = new Encoder(0, 1);
 
-    private final PWMSparkMax m_motor = new PWMSparkMax(1);
-    private final Encoder m_encoder = new Encoder(0, 1);
+  protected static void configureEncoderForDistance(Encoder encoder, Distance outerDiameter) {
+    encoder.setDistancePerPulse(Math.PI * WHEEL_DIAMETER.in(Meters) / ENCODER_TICKS_PER_REVOLUTION);
+  }
 
-    protected static void configureEncoderForDistance(Encoder encoder,
-            Distance outerDiameter) {
-        encoder.setDistancePerPulse(Math.PI * WHEEL_DIAMETER.in(Meters) /
-                ENCODER_TICKS_PER_REVOLUTION);
-    }
+  public SingleMotorThingSim() {
+  }
 
-    public SingleMotorThingSim() {
-    }
+  //
+  // Methods from ISingleMotorThing
+  //
 
-    @Override
-    public void setSpeed(double percent) {
-        m_motor.set(percent);
-    }
+  @Override
+  public void setSpeed(double percent) {
+    m_motor.set(percent);
+  }
 
-    @Override
-    public double getSpeed() {
-        return m_motor.get();
-    }
+  @Override
+  public double getSpeed() {
+    return m_motor.get();
+  }
 
-    @Override
-    public Distance getPosition() {
-        return Meters.of(m_encoder.getDistance());
-    }
+  @Override
+  public Distance getPosition() {
+    return Meters.of(m_encoder.getDistance());
+  }
 
-    @Override
-    public void simulationPeriodic() {
-        // Do simulation things here...
-    }
+  //
+  // Methods from SubsystemBase
+  //
+
+  @Override
+  public void simulationPeriodic() {
+    // Do simulation things here...
+  }
 }

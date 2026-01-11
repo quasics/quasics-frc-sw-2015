e16e3d4bb9d1eef7fafdc3050b11d5d52874c060
Matt Healy <mjhealy@alum.rit.edu>
2026-01-10 20:00:57 -0500

    Cleaned up warnings in KitBot code.

--- Experimental/2026/MattsToys/2026KitBotInline/src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
+++ Experimental/2026/MattsToys/2026KitBotInline/src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
@@ -6,15 +6,14 @@ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.DriveConstants.*;
 
--- Experimental/2026/MattsToys/2026KitBotInline/src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
+++ Experimental/2026/MattsToys/2026KitBotInline/src/main/java/frc/robot/subsystems/CANFuelSubsystem.java
@@ -4,8 +4,8 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkMax;

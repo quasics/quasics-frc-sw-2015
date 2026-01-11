c267fbff17d8f42cfdfc9f4d3121f2d243a48e96
Matt Healy <mjhealy@alum.rit.edu>
2026-01-11 14:17:55 -0500

    Adding power control buttons.

--- Experimental/2026/MattsToys/SingleSparkRunner/src/main/java/frc/robot/RobotContainer.java
+++ Experimental/2026/MattsToys/SingleSparkRunner/src/main/java/frc/robot/RobotContainer.java
@@ -4,18 +4,36 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import frc.robot.subsystems.SingleMotor;
 
 public class RobotContainer {
   SingleMotor m_singleMotor = new SingleMotor();
-  
+
   public RobotContainer() {
-    configureBindings();
+    addPowerButton("Stop!", 0);
+    addPowerButton("-25% power", -.25);
+    addPowerButton("-50% power", -.5);
+    addPowerButton("-100% power", -1.0);
+    addPowerButton("+25% power", +.25);
+    addPowerButton("+50% power", +.5);
+    addPowerButton("+100% power", +1.0);
   }
 
-  private void configureBindings() {}
+  private void addPowerButton(String label, double percent) {
+    SmartDashboard.putData(label, new FunctionalCommand(
+      ()->{m_singleMotor.setSpeed(percent);},
+      null,
+      // end
+      (Boolean b) -> { m_singleMotor.stop(); },
+      // isFinished
+      null,
+      // Dependency
+      m_singleMotor));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");

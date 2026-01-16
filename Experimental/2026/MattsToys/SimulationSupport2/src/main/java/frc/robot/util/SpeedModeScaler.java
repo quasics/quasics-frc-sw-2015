b192908aa06923a15920878405e43f99f5236acd
Matt Healy <mjhealy@alum.rit.edu>
2026-01-16 15:44:13 -0500

    [SimSupport] Doc updates.

--- Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/util/SpeedModeScaler.java
+++ Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/util/SpeedModeScaler.java
@@ -1,14 +1,40 @@
+// Copyright (c) 2026, Matthew J. Healy and other Quasics contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.util;
 
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
+/**
+ * Implements a reusable "add-on" for handling speed scaling
+ * (normal/turbo/turtle) for robot driving.
+ */
 public class SpeedModeScaler implements UnaryOperator<Double> {
+    /** Supplies the current "driving mode" for the robot. */
     final Supplier<SpeedMode> m_modeSupplier;
+
+    /** Scaling factor applied in "normal" mode. */
     final double m_normalScaling;
+
+    /** Scaling factor applied in "turbo" mode. */
     final double m_turboScaling;
+
+    /** Scaling factor applied in "turtle" mode. */
     final double m_turtleScaling;
 
+    /**
+     * Constructor.
+     * 
+     * @param modeSupplier  supplies the current "driving mode" for the robot
+     * @param normalScaling scaling factor applied in "normal" mode
+     * @param turboScaling  scaling factor applied in "turbo" mode
+     * @param turtleScaling scaling factor applied in "turtle" mode
+     * 
+     * @throws IllegalArgumentException if normalScaling > turboScaling, or
+     *                                  turtleScaling > normalScaling
+     */
     public SpeedModeScaler(Supplier<SpeedMode> modeSupplier, double normalScaling, double turboScaling,
             double turtleScaling) {
         if (modeSupplier == null) {
--- Experimental/2026/MattsToys/SimulationSupport2/src/test/java/SpeedModeScalerTest.java
+++ Experimental/2026/MattsToys/SimulationSupport2/src/test/java/SpeedModeScalerTest.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2026, Matthew J. Healy and other Quasics contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 

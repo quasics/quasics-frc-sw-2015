01c35a1f6e49693031515fd194cb6a3b6607c2e4
Matt Healy <mjhealy@alum.rit.edu>
2026-01-17 13:06:57 -0500

    Doc updates.

--- Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/commands/TargetingSupportCommand.java
+++ Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/commands/TargetingSupportCommand.java
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) 2026, Matthew J. Healy and other Quasics contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -24,7 +24,17 @@ import frc.robot.util.PoseHelpers;
  * Some notes:
  * <ul>
  * <li>This code assumes that there are no other factors limiting our ability to
- * take a shot (e.g., obstacles on the field such as the net in )
+ * take a shot (e.g., obstacles on the field such as the net in "Rebuilt").
+ * 
+ * <li>This code only provides a simple "good/distance bad/angle bad/both
+ * bad/don't know" signal, rather than something more nuanced (e.g., "too close
+ * and rotated too far clockwise").
+ * 
+ * <li>We are only using the vision system's estimate for position, which means
+ * that if there isn't at least one AprilTag in sight, we can't figure anything
+ * out. Using a "fused" estimate (based on both vision and odometry) would be
+ * significantly better for a real implementation.
+ * 
  * </ul>
  */
 public class TargetingSupportCommand extends Command {
@@ -34,7 +44,25 @@ public class TargetingSupportCommand extends Command {
   final Distance m_minRange;
   final Angle m_angleRange;
 
-  /** Creates a new TargetingSupportCommand. */
+  /**
+   * Creates a new TargetingSupportCommand.
+   * 
+   * Note: note that for the "targetPos", we're only using its position, and not
+   * the "rotation"/heading. This could be a point for improving the solution in a
+   * real-world scenario, where the rotation could indicate the centerline along
+   * which it's approachable (e.g., facing blue or red alliance), and then take
+   * another Angle measurement to indicate from how broad a range it can be
+   * approached. (So that, for example, if you're coming up from behind it, the
+   * lights will give different signals than if you're coming in where it's open.)
+   * 
+   * @param lighting   lighting subsystem
+   * @param targetPos  center of the target we're tring to align with, in
+   *                   field-relative terms
+   * @param maxRange   maximum distance from which we can (likely) make a shot
+   * @param minRange   minimum distance from which we can (likely) make a shot
+   * @param angleRange margin of error (+/-) that we're willing to accept for
+   *                   being "headed for" the target's center
+   */
   public TargetingSupportCommand(ILighting lighting, Pose2d targetPos, Distance maxRange, Distance minRange,
       Angle angleRange) {
     m_lighting = lighting;
@@ -45,6 +73,10 @@ public class TargetingSupportCommand extends Command {
     addRequirements(m_lighting.asSubsystem());
   }
 
+  /**
+   * Updates the lights on the robot, based on the robot's pose vs. the target's
+   * location.
+   */
   private void update() {
     var positionOpt = BulletinBoard.common.getValue(IVisionPlus.ESTIMATED_POSE_KEY,
         IVisionPlus.EstimatedPoseData.class);

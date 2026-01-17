acb2b0f2a8d5b7d16920bab9804157289d292111
Quasics FRC <quasics_driverStation@mersgroup.com>
2026-01-17 13:16:30 -0500

    Began building motor controller recognition

--- Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/RealDrivebase.java
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/RealDrivebase.java
@@ -4,11 +4,29 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class RealDrivebase extends SubsystemBase {
+public class RealDrivebase extends AbstractDrivebase {
+  private final SparkMax m_leftleader;
+  private final SparkMax m_leftfollower;
+  private final SparkMax m_rightleader;
+  private final SparkMax m_rightfollower;
+
   /** Creates a new RealDrivebase. */
-  public RealDrivebase() {}
+  public RealDrivebase() {
+    // TODO: find actual SparkMax IDs, currents are placeholders.
+    m_leftleader = new SparkMax(1, MotorType.kBrushless);
+    m_leftfollower = new SparkMax(2, MotorType.kBrushless);
+    m_rightleader = new SparkMax(3, MotorType.kBrushless);
+    m_rightfollower = new SparkMax(4, MotorType.kBrushless);
+    // add thriftynova support
+  }
+
+  public void arcadeDrive(float forwardspeed, float turnspeed) {
+    // TODO: use diffDrive class to drive w arcade
+  }
 
   @Override
   public void periodic() {

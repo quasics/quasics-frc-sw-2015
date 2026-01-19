c1f55405b3c76a4aedecece7f2f7a41257c9de9a
Matt Healy <mjhealy@alum.rit.edu>
2026-01-19 15:11:26 -0500

    Doc updates.

--- Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/hardware/actuators/ThriftyNovaMotorControllerPlus.java
+++ Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/hardware/actuators/ThriftyNovaMotorControllerPlus.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2026, Matthew J. Healy and other Quasics contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.hardware.actuators;
 
 import static edu.wpi.first.units.Units.Volts;
@@ -6,8 +10,14 @@ import com.thethriftybot.devices.ThriftyNova;
 import java.io.Closeable;
 import java.io.IOException;
 
+/** IMotorControllerPlus specialization for Thrifty Nova motors. */
 public class ThriftyNovaMotorControllerPlus
     extends IMotorControllerPlus.MotorControllerPlus {
+  /**
+   * Constructor.
+   * 
+   * @param controller the motor controller being wrapped
+   */
   public ThriftyNovaMotorControllerPlus(ThriftyNova controller) {
     super(controller, () -> Volts.of(controller.getVoltage()), new Closeable() {
       public void close() throws IOException {

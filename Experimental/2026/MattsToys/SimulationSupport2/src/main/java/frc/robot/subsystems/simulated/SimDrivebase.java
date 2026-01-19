dcf3ecd6bb4ffe1ea9c866dc66602bc3093c7619
Matt Healy <mjhealy@alum.rit.edu>
2026-01-19 13:32:38 -0500

    Doc updates.

--- Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/subsystems/simulated/SimDrivebase.java
+++ Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/subsystems/simulated/SimDrivebase.java
@@ -95,8 +95,8 @@ public class SimDrivebase extends DrivebaseBase {
               new Rotation2d(ReefscapeConstants.FACING_RED));
 
         case Reefscape__Extra1 ->
-          new Pose2d(ReefscapeConstants.BLUE_STARTING_LINE.in(Meters) -.5,
-              ReefscapeConstants.TOP_BALL_HEIGHT.in(Meters) -.5,
+          new Pose2d(ReefscapeConstants.BLUE_STARTING_LINE.in(Meters) - .5,
+              ReefscapeConstants.TOP_BALL_HEIGHT.in(Meters) - .5,
               new Rotation2d(
                   ReefscapeConstants.FACING_BLUE.minus(Degrees.of(5))));
         case Reefscape__Extra2 ->
@@ -134,23 +134,25 @@ public class SimDrivebase extends DrivebaseBase {
    *
    * @see frc.robot.utils.RobotConfigs.DriveFeedForwardConfig
    */
-  final LinearSystem<N2, N2, N2> m_drivetrainSystem =
-      LinearSystemId.identifyDrivetrainSystem(
-          // Linear components (velocity, acceleration)
-          1.98, 0.2,
-          // Angular components (velocity, acceleration)
-          1.5, 0.3);
+  final LinearSystem<N2, N2, N2> m_drivetrainSystem = LinearSystemId.identifyDrivetrainSystem(
+      // Linear components (velocity, acceleration)
+      1.98, 0.2,
+      // Angular components (velocity, acceleration)
+      1.5, 0.3);
 
   /** Simulation driver for the overall drive train. */
-  final DifferentialDrivetrainSim m_drivetrainSimulator =
-      new DifferentialDrivetrainSim(m_drivetrainSystem,
-          // Drive motor type and count
-          DCMotor.getNEO(4), GEAR_RATIO, TRACK_WIDTH.in(Meters),
-          WHEEL_DIAMETER.in(Meters),
-          // configure for no noise in measurements
-          null);
+  final DifferentialDrivetrainSim m_drivetrainSimulator = new DifferentialDrivetrainSim(m_drivetrainSystem,
+      // Drive motor type and count
+      DCMotor.getNEO(4), GEAR_RATIO, TRACK_WIDTH.in(Meters),
+      WHEEL_DIAMETER.in(Meters),
+      // configure for no noise in measurements
+      null);
 
-  /** Constructor. */
+  /**
+   * Constructor.
+   * 
+   * @param config the configuration for this drive base
+   */
   public SimDrivebase(DriveConfig config) {
     this(config,
         // Left encoder
@@ -168,7 +170,7 @@ public class SimDrivebase extends DrivebaseBase {
   /**
    * Actual constructor for this class.
    *
-   * @param config  drive configuration
+   * @param config  drivebase configuration data
    * @param left    left encoder/simulator pair
    * @param right   right encoder/simulator pair
    * @param rawGyro gyro
@@ -188,8 +190,7 @@ public class SimDrivebase extends DrivebaseBase {
     m_rightEncoderSim = right.encoderSim;
     m_gyroSim = new AnalogGyroSim(rawGyro);
 
-    SendableChooser<StartingPosition> positionChooser =
-        new SendableChooser<StartingPosition>();
+    SendableChooser<StartingPosition> positionChooser = new SendableChooser<StartingPosition>();
     for (var pos : StartingPosition.values()) {
       if (pos == StartingPosition.Default) {
         positionChooser.setDefaultOption(pos.getName(), pos);
@@ -201,8 +202,18 @@ public class SimDrivebase extends DrivebaseBase {
     positionChooser.onChange(this::updateStartingPoint);
   }
 
-  private record SimulatedEncoderPair(Encoder encoder, EncoderSim encoderSim) {}
+  /** Used to hold a WPI encoder and its paired EncoderSim object. */
+  private record SimulatedEncoderPair(Encoder encoder, EncoderSim encoderSim) {
+  }
 
+  /**
+   * Allocates an encoder and a paired simulator object.
+   * 
+   * @param portId1  "A" port for the encoder
+   * @param portId2  "B" port for the encoder
+   * @param inverted if true, the motor is in an inverted configuration
+   * @return the simulated encoder pair
+   */
   protected static SimulatedEncoderPair getSimulatedEncoderPair(
       int portId1, int portId2, boolean inverted) {
     Encoder e = getConfiguredEncoder(portId1, portId2, inverted);
@@ -279,10 +290,8 @@ public class SimDrivebase extends DrivebaseBase {
     // Angular velocity is not computed by the drive train simulator, but it's
     // just the derivative of heading (change in heading over time), so we can
     // compute it here.
-    final var deltaHeading =
-        m_drivetrainSimulator.getHeading().minus(oldHeading);
-    final double angularVelocity =
-        deltaHeading.getDegrees() / dtSeconds; // degrees per second
+    final var deltaHeading = m_drivetrainSimulator.getHeading().minus(oldHeading);
+    final double angularVelocity = deltaHeading.getDegrees() / dtSeconds; // degrees per second
     m_gyroSim.setRate(angularVelocity);
   }
 }

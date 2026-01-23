0f788df5ef3822c977f045e89c45af21694e0b8b
Quasics FRC <quasics_driverStation@mersgroup.com>
2026-01-22 19:00:51 -0500

    getTargetData for best target only

--- Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/IVision.java
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/IVision.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems;
 
 public interface IVision {
+  record TargetData(int id, double yaw, double pitch, double skew) {
+
+  }
+
   boolean canSeeTargets();
 }
--- Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/Vision.java
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/Vision.java
@@ -4,12 +4,35 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
 import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.PhotonUtils;
 
 public class Vision extends SubsystemBase implements IVision {
   /** Creates a new Vision. */
+  private static final AprilTagFields FIELD_LAYOUT = AprilTagFields.kDefaultField;
+  private final AprilTagFieldLayout m_tagLayout;
+
   public Vision() {
+    AprilTagFieldLayout tagLayout = null;
+    try {
+      tagLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.kDefaultField.m_resourceFile);
+    } catch (IOException ioe) {
+      System.err.println("Warning: failed to load April Tags layout (" + FIELD_LAYOUT + ")");
+      ioe.printStackTrace();
+    }
+    m_tagLayout = tagLayout;
   }
 
   protected PhotonCamera camera = new PhotonCamera("camera1");
@@ -25,5 +48,20 @@ public class Vision extends SubsystemBase implements IVision {
     var result = camera.getLatestResult();
     hasTargets = result.hasTargets();
     return hasTargets;
+  }
+
+  public TargetData getTargetData() {
+    var results = camera.getLatestResult();
+    TargetData targetData = new TargetData(0, 0.0, 0.0, 0.0);
+    if (!results.hasTargets()) {
+      System.out.println("NO TARGET DATA RETRIEVED");
+      return targetData;
+    }
+    if (results.hasTargets()) {
+      PhotonTrackedTarget target = results.getBestTarget();
+      targetData = new TargetData(target.getFiducialId(), target.getYaw(), target.getPitch(), target.getSkew());
+    }
+    System.out.println(targetData);
+    return targetData;
   }
 }

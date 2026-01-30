4e96458ccd8030c5389193706077291d5870ec67
Matt Healy <mjhealy@alum.rit.edu>
2026-01-30 10:00:17 -0500

    [SimSupport] Updated handling of getName().

--- Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/constants/games/StartingPosition.java
+++ Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/constants/games/StartingPosition.java
@@ -14,7 +14,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
  * Supported (pre-defined) starting positions for the robot.
  *
  * Note that game-specific starting positions are pre-fixed with the game name
- * (e.g., "Reefscape_").
+ * (e.g., "Reefscape__").
  */
 public enum StartingPosition {
   /** Default robot position (0, 0, 0). */
@@ -25,30 +25,27 @@ public enum StartingPosition {
   //
 
   /** Facing Blue and aligned with the middle of the lower trench in Rebuilt. */
-  Rebuilt_BlueStart1,
+  Rebuilt__BlueStart1,
 
   /** Facing Red and aligned with the middle of the lower trench in Rebuilt. */
-  Rebuilt_RedStart1,
+  Rebuilt__RedStart1,
 
   //
   // Sample starting positions for Reefscape (2025).
   //
 
   /** Facing Blue and aligned with starting game element 1 in Reefscape. */
-  Reefscape_Blue1,
+  Reefscape__Blue1,
   /** Facing Blue and aligned with starting game element 2 in Reefscape. */
-  Reefscape_Blue2,
+  Reefscape__Blue2,
   /** Facing Blue and aligned with starting game element 3 in Reefscape. */
-  Reefscape_Blue3,
+  Reefscape__Blue3,
   /** Facing Red and aligned with starting game element 1 in Reefscape. */
-  Reefscape_Red1,
+  Reefscape__Red1,
   /** Facing Red and aligned with starting game element 2 in Reefscape. */
-  Reefscape_Red2,
+  Reefscape__Red2,
   /** Facing Red and aligned with starting game element 3 in Reefscape. */
-  Reefscape_Red3,
-  Reefscape_Extra1,
-  Reefscape_Extra2,
-  ;
+  Reefscape__Red3, Reefscape__Extra1, Reefscape__Extra2,;
 
   /** Default starting pose for the robot. */
   public static final Pose2d DEFAULT_STARTING_POSE = new Pose2d(0, 0, new Rotation2d());
@@ -59,49 +56,49 @@ public enum StartingPosition {
     return switch (this) {
       case Default -> DEFAULT_STARTING_POSE;
 
-      case Rebuilt_BlueStart1 ->
+      case Rebuilt__BlueStart1 ->
         new Pose2d(
             RebuiltConstants.BLUE_STARTING_LINE.in(Meters),
             RebuiltConstants.TRENCH_WIDTH.div(2).in(Meters),
             new Rotation2d(RebuiltConstants.FACING_BLUE));
 
-      case Rebuilt_RedStart1 ->
+      case Rebuilt__RedStart1 ->
         new Pose2d(
             RebuiltConstants.RED_STARTING_LINE.in(Meters),
             RebuiltConstants.TRENCH_WIDTH.div(2).in(Meters),
             new Rotation2d(RebuiltConstants.FACING_RED));
 
-      case Reefscape_Blue1 ->
+      case Reefscape__Blue1 ->
         new Pose2d(ReefscapeConstants.BLUE_STARTING_LINE.in(Meters),
             ReefscapeConstants.TOP_BALL_HEIGHT.in(Meters),
             new Rotation2d(ReefscapeConstants.FACING_BLUE));
-      case Reefscape_Blue2 ->
+      case Reefscape__Blue2 ->
         new Pose2d(ReefscapeConstants.BLUE_STARTING_LINE.in(Meters),
             ReefscapeConstants.MIDDLE_BALL_HEIGHT.in(Meters),
             new Rotation2d(ReefscapeConstants.FACING_BLUE));
-      case Reefscape_Blue3 ->
+      case Reefscape__Blue3 ->
         new Pose2d(ReefscapeConstants.BLUE_STARTING_LINE.in(Meters),
             ReefscapeConstants.BOTTOM_BALL_HEIGHT.in(Meters),
             new Rotation2d(ReefscapeConstants.FACING_BLUE));
-      case Reefscape_Red1 ->
+      case Reefscape__Red1 ->
         new Pose2d(ReefscapeConstants.RED_STARTING_LINE.in(Meters),
             ReefscapeConstants.BOTTOM_BALL_HEIGHT.in(Meters),
             new Rotation2d(ReefscapeConstants.FACING_RED));
-      case Reefscape_Red2 ->
+      case Reefscape__Red2 ->
         new Pose2d(ReefscapeConstants.RED_STARTING_LINE.in(Meters),
             ReefscapeConstants.MIDDLE_BALL_HEIGHT.in(Meters),
             new Rotation2d(ReefscapeConstants.FACING_RED));
-      case Reefscape_Red3 ->
+      case Reefscape__Red3 ->
         new Pose2d(ReefscapeConstants.RED_STARTING_LINE.in(Meters),
             ReefscapeConstants.TOP_BALL_HEIGHT.in(Meters),
             new Rotation2d(ReefscapeConstants.FACING_RED));
 
-      case Reefscape_Extra1 ->
+      case Reefscape__Extra1 ->
         new Pose2d(ReefscapeConstants.BLUE_STARTING_LINE.in(Meters) - .5,
             ReefscapeConstants.TOP_BALL_HEIGHT.in(Meters) - .5,
             new Rotation2d(
                 ReefscapeConstants.FACING_BLUE.minus(Degrees.of(5))));
-      case Reefscape_Extra2 ->
+      case Reefscape__Extra2 ->
         new Pose2d(ReefscapeConstants.BLUE_STARTING_LINE.in(Meters) + .5,
             ReefscapeConstants.TOP_BALL_HEIGHT.in(Meters) + .5,
             new Rotation2d(
@@ -112,8 +109,25 @@ public enum StartingPosition {
   /**
    * Returns the user-facing name of this starting position (e.g., for use in
    * a chooser).
+   * 
+   * @see #getName(boolean)
    */
   public String getName() {
-    return this.toString().replaceFirst(".*__", "");
+    return getName(true);
+  }
+
+  /**
+   * Returns the user-facing name of this starting position (e.g., for use in
+   * a chooser).
+   * 
+   * @param stripGameFromName if true, remove any game-specific prefix (e.g.,
+   *                          "Reefscape__") from the name
+   */
+  public String getName(boolean stripGameFromName) {
+    if (stripGameFromName) {
+      return toString().replaceFirst(".*__", "");
+    } else {
+      return toString();
+    }
   }
 }

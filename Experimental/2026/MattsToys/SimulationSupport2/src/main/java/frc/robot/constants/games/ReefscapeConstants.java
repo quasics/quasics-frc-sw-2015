331a5a3461a03bdd88cd008792d4d390cb783853
Matt Healy <mjhealy@alum.rit.edu>
2026-01-29 19:35:04 -0500

    [SimSupport] Updated ordering of "public static final".

--- Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/constants/games/ReefscapeConstants.java
+++ Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/constants/games/ReefscapeConstants.java
@@ -11,54 +11,52 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 
 /** Defines various constants for the 2025 game, "Reefscape". */
-final public class ReefscapeConstants {
+public final class ReefscapeConstants {
   /** Robot heading when facing the Blue alliance. */
-  static final public Angle FACING_BLUE = Degrees.of(180);
+  public static final Angle FACING_BLUE = Degrees.of(180);
 
   /** Robot heading when facing the Red alliance. */
-  static final public Angle FACING_RED = Degrees.of(0);
+  public static final Angle FACING_RED = Degrees.of(0);
 
   /** Length (x) of the field. */
-  static final public Distance FIELD_LENGTH = Meters.of(17.55);
+  public static final Distance FIELD_LENGTH = Meters.of(17.55);
 
   /** Width (y) of the field. */
-  static final public Distance FIELD_WIDTH = Meters.of(8.05);
+  public static final Distance FIELD_WIDTH = Meters.of(8.05);
 
   /** Field midline (length-wise), where the barge is located. */
-  static final public Distance MIDLINE = FIELD_LENGTH.div(2);
+  public static final Distance MIDLINE = FIELD_LENGTH.div(2);
 
   /** Offset from the field midline where the robots are supposed to start. */
-  static final public Distance STARTING_LINE_OFFSET = Meters.of(1.2);
+  public static final Distance STARTING_LINE_OFFSET = Meters.of(1.2);
 
   /**
    * Robot distance from east side of the field when on the Blue alliance's
    * starting line.
    */
-  static final public Distance BLUE_STARTING_LINE =
-      FIELD_LENGTH.div(2).minus(STARTING_LINE_OFFSET);
+  public static final Distance BLUE_STARTING_LINE = FIELD_LENGTH.div(2).minus(STARTING_LINE_OFFSET);
 
   /**
    * Robot distance from east side of the field when on the Red alliance's
    * starting line.
    */
-  static final public Distance RED_STARTING_LINE =
-      FIELD_LENGTH.div(2).plus(STARTING_LINE_OFFSET);
+  public static final Distance RED_STARTING_LINE = FIELD_LENGTH.div(2).plus(STARTING_LINE_OFFSET);
 
   /**
    * Robot distance from south side of the field when in front of the
    * "top-most" ball.
    */
-  static final public Distance TOP_BALL_HEIGHT = Meters.of(5.9);
+  public static final Distance TOP_BALL_HEIGHT = Meters.of(5.9);
 
   /**
    * Robot distance from south side of the field when in front of the "middle"
    * ball.
    */
-  static final public Distance MIDDLE_BALL_HEIGHT = Meters.of(4);
+  public static final Distance MIDDLE_BALL_HEIGHT = Meters.of(4);
 
   /**
    * Robot distance from south side of the field when in front of the
    * "bottom-most" ball.
    */
-  static final public Distance BOTTOM_BALL_HEIGHT = Meters.of(2.25);
+  public static final Distance BOTTOM_BALL_HEIGHT = Meters.of(2.25);
 }

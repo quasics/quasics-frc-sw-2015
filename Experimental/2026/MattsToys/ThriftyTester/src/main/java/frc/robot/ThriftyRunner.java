f1864c809320ebaf92d027fd6e892fbaaf2a5b28
Quasics FRC <quasics_driverStation@mersgroup.com>
2026-01-30 20:17:16 -0500

    Trying Thrifty's sample code. It doesn't work.

---
+++ Experimental/2026/MattsToys/ThriftyTester/src/main/java/frc/robot/ThriftyRunner.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.thethriftybot.devices.ThriftyNova;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ThriftyRunner extends SubsystemBase {
+  private ThriftyNova motor; // motor instance
+
+  /** Creates a new ThriftyRunner. */
+  public ThriftyRunner() {
+    motor = new ThriftyNova(1);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    motor.setPercent(1); // set motor to output full forward 
+  }
+}

856de7334aba75f9dfc74a72ffa9fadef8fae236
Matt Healy <mjhealy@alum.rit.edu>
2026-01-30 20:21:40 -0500

    Added printing of errors when constructing ThriftyNova.

--- Experimental/2026/MattsToys/ThriftyTester/src/main/java/frc/robot/ThriftyRunner.java
+++ Experimental/2026/MattsToys/ThriftyTester/src/main/java/frc/robot/ThriftyRunner.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import java.util.List;
+
 import com.thethriftybot.devices.ThriftyNova;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,11 +16,16 @@ public class ThriftyRunner extends SubsystemBase {
   /** Creates a new ThriftyRunner. */
   public ThriftyRunner() {
     motor = new ThriftyNova(1);
+    List<ThriftyNova.Error> errors = motor.getErrors();
+    for (var err : errors) {
+      System.out.println(err.toString());
+    }
+    motor.clearErrors();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    motor.setPercent(1); // set motor to output full forward 
+    motor.setPercent(1); // set motor to output full forward
   }
 }

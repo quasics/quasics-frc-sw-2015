540aa6d4aadf7b591189fec0cdab4621d911a96a
Matt Healy <mjhealy@alum.rit.edu>
2026-01-31 23:16:18 -0500

    Working on flywheel/shooter simulation.

---
+++ Experimental/2026/MattsToys/SimpleFlywheel/src/main/java/frc/robot/subsystems/BangBangFlywheel.java
@@ -0,0 +1,109 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Radian;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RevolutionsPerSecond;
+
+import edu.wpi.first.math.controller.BangBangController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class BangBangFlywheel extends SubsystemBase {
+  private static final int kMotorPort = 0;
+  private static final int kEncoderAChannel = 0;
+  private static final int kEncoderBChannel = 1;
+
+  private double m_setpointRadiansPerSec = 0.0;
+
+  // Max setpoint for joystick control in RPM
+  private static final AngularVelocity kMaxSetpointValue = RevolutionsPerSecond.of(6000.0);
+
+  private final PWMSparkMax m_flywheelMotor = new PWMSparkMax(kMotorPort);
+  private final Encoder m_encoder = new Encoder(kEncoderAChannel, kEncoderBChannel);
+
+  private final BangBangController m_bangBangController = new BangBangController();
+
+  // Gains are for example purposes only - must be determined for your own robot!
+  public static final double kFlywheelKs = 0.0001; // V
+  public static final double kFlywheelKv = 0.000195; // V/RPM
+  public static final double kFlywheelKa = 0.0003; // V/(RPM/s)
+  private final SimpleMotorFeedforward m_feedforward = new SimpleMotorFeedforward(kFlywheelKs, kFlywheelKv,
+      kFlywheelKa);
+
+  // Simulation classes help us simulate our robot
+
+  // Reduction between motors and encoder, as output over input. If the flywheel
+  // spins slower than the motors, this number should be greater than one.
+  private static final double kFlywheelGearing = 1.0 / 8.45;
+
+  // 1/2 MR²
+  private static final double kFlywheelMomentOfInertia = 0.008;
+
+  private final DCMotor m_gearbox = DCMotor.getNEO(1);
+
+  private final LinearSystem<N1, N1, N1> m_plant = LinearSystemId.createFlywheelSystem(m_gearbox, kFlywheelGearing,
+      kFlywheelMomentOfInertia);
+
+  private final FlywheelSim m_flywheelSim = new FlywheelSim(m_plant, m_gearbox);
+  private final EncoderSim m_encoderSim = new EncoderSim(m_encoder);
+
+  /** Creates a new BangBangFlywheel. */
+  public BangBangFlywheel() {
+    // Add bang-bang controller to SmartDashboard and networktables.
+    SmartDashboard.putData(m_bangBangController);
+  }
+
+  public void setSpeed(AngularVelocity velocity) {
+    m_setpointRadiansPerSec = velocity.in(RadiansPerSecond);
+  }
+
+  public AngularVelocity getSpeed() {
+    return RadiansPerSecond.of(m_encoder.getRate());
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    // Set setpoint and measurement of the bang-bang controller
+    double bangOutput = m_bangBangController.calculate(m_encoder.getRate(), m_setpointRadiansPerSec) * 12.0;
+
+    // Controls a motor with the output of the BangBang controller and a
+    // feedforward. The feedforward is reduced slightly to avoid overspeeding
+    // the shooter.
+    final double volts = bangOutput + 0.9 * m_feedforward.calculate(m_setpointRadiansPerSec);
+    m_flywheelMotor.setVoltage(volts);
+
+    SmartDashboard.putNumber(
+        "Flywheel speed (RPM)",
+        Units.radiansPerSecondToRotationsPerMinute(m_encoder.getRate()));
+    SmartDashboard.putNumber(
+        "Flywheel setpoint (RPM)",
+        Units.radiansPerSecondToRotationsPerMinute(m_setpointRadiansPerSec));
+    SmartDashboard.putNumber("Flywheel volts", volts);
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // To update our simulation, we set motor voltage inputs, update the
+    // simulation, and write the simulated velocities to our simulated encoder
+    m_flywheelSim.setInputVoltage(m_flywheelMotor.get() * RobotController.getInputVoltage());
+    m_flywheelSim.update(0.02);
+    m_encoderSim.setRate(m_flywheelSim.getAngularVelocityRadPerSec());
+  }
+}
---
+++ Experimental/2026/MattsToys/SimpleFlywheel/src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,84 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.sim.SparkMaxSim; // REV-specific sim
+
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim; // WPILib physics sim
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class Shooter extends SubsystemBase {
+  private final SparkMax motor;
+  private final SparkClosedLoopController pidController;
+
+  // Simulation Objects
+  private SparkMaxSim motorSim;
+  private FlywheelSim flywheelSim;
+
+  public Shooter(int canID) {
+    motor = new SparkMax(canID, MotorType.kBrushless);
+    pidController = motor.getClosedLoopController();
+
+    // Configure the motor controller
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.closedLoop.p(0.0005);
+    // config.closedLoop.velocityFF(0.00017);
+    config.closedLoop.feedForward.kV(0.00017);
+    motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    // Initialize Simulation only if running on desktop
+    if (RobotBase.isSimulation()) {
+      // Define the physics of your shooter
+      final DCMotor gearbox = DCMotor.getNEO(1); // Motor type and count
+      final LinearSystem<N1, N1, N1> plant = LinearSystemId.createFlywheelSystem(
+          gearbox,
+          0.008, // Moment of Inertia (kg*m^2) - adjust to your wheel size);
+          1.0 // Gearing (1:1),
+      );
+
+      flywheelSim = new FlywheelSim(plant, gearbox);
+      motorSim = new SparkMaxSim(motor, gearbox); // Connect sim to motor
+    }
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Shooter Actual RPM", getActualRPM());
+  }
+
+  // Call this in the robot's simulationPeriodic() method
+  @Override
+  public void simulationPeriodic() {
+    final double timeDeltaSecs = 0.020; // Standard 20ms loop
+
+    // 1. Get applied voltage from the SparkMax
+    double motorVoltage = motorSim.getAppliedOutput() * motorSim.getBusVoltage();
+
+    // 2. Update physics model with that voltage
+    flywheelSim.setInputVoltage(motorVoltage);
+    flywheelSim.update(timeDeltaSecs);
+
+    // 3. Feed the calculated velocity back into the SparkMax encoder
+    motorSim.iterate(flywheelSim.getAngularVelocityRPM(), motorSim.getBusVoltage(), timeDeltaSecs);
+  }
+
+  public void setRPM(double targetRPM) {
+    pidController.setSetpoint(targetRPM, ControlType.kVelocity);
+    SmartDashboard.putNumber("Shooter Target RPM", targetRPM);
+  }
+
+  public double getActualRPM() {
+    return motor.getEncoder().getVelocity();
+  }
+}

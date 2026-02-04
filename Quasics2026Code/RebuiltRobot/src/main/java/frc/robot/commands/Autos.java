7de7a08302bf2ac4e6359f494431f5fd725f8550
Matt Healy <mjhealy@alum.rit.edu>
2026-02-04 09:03:01 -0500

    [Rebuilt] Removing ExampleSubsystem.

--- Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/commands/Autos.java
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/commands/Autos.java
@@ -4,14 +4,13 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
-  public static Command exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), Commands.print("Do something else now...."));
+  public static Command exampleAuto() {
+    return Commands.sequence(Commands.print("Do something...."), Commands.print("Do something else now...."));
   }
 
   private Autos() {

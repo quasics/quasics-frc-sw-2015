5b1d5748f2af378d1c0059a43db119eae5becab3
Matt Healy <mjhealy@alum.rit.edu>
2026-02-04 20:08:32 -0500

    [Rebuilt] Doc updates.

--- Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/Lighting.java
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/Lighting.java
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) 2026, Quasics Robotics and other contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -8,7 +8,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Lighting extends SubsystemBase {
   /** Creates a new Lighting. */
-  public Lighting() {}
+  public Lighting() {
+  }
 
   @Override
   public void periodic() {
--- Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/interfaces/IDrivebase.java
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/interfaces/IDrivebase.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2026, Quasics Robotics and other contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems.interfaces;
 
 import edu.wpi.first.math.geometry.Pose2d;

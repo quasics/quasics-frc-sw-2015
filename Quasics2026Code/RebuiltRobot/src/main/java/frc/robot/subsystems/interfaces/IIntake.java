41bfde060961d161e72a7d192c1ff3cd9fa442f2
Matt Healy <mjhealy@alum.rit.edu>
2026-02-11 09:49:56 -0500

    [Rebuilt] Creating stubbed interfaces for remaining (known) subsystems.

---
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/interfaces/IIntake.java
@@ -0,0 +1,13 @@
+// Copyright (c) 2026, Quasics Robotics and other contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.interfaces;
+
+/**
+ * Interface for controlling the intake mechanism, used to pick up balls from
+ * the floor.
+ */
+public interface IIntake {
+
+}

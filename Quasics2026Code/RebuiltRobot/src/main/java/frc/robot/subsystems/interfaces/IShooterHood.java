f286ca66d9c5b6df763dc706c04267d04a1e7028
Matt Healy <mjhealy@alum.rit.edu>
2026-02-13 13:51:30 -0500

    [Rebuilt] Doc updates.

--- Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/interfaces/IShooterHood.java
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/interfaces/IShooterHood.java
@@ -9,10 +9,26 @@ public interface IShooterHood {
    * (1 motor for hood angle; rotary (absolute) encoder for hood position)
    * Adjust hood angle (with limits) - forward and backward
    * 
+   * Note that while we may not need to use PID control for the hood, doing so
+   * (and setting up the motor controller to use closed-loop control via the
+   * absolute encoder with the through-bore encoder wired to it) would be a
+   * good idea to ensure that the hood is always at the correct angle. And this
+   * would mean you'd need to do comparatively little work to implement it, since
+   * the SparkMax controllers have built-in support for closed-loop control with
+   * an absolute encoder (and the through-bore encoder can be wired to the
+   * SparkMax's encoder port, so you wouldn't need to do any custom wiring or
+   * anything like that).
+   * 
+   * And, yeah: this would also allow us to easily set the hood to specific angles
+   * for different shots, which could be useful for consistency and accuracy.
+   * 
    * FINDME(Daniel): Some suggested reading for this subsystem:
    * * SparkMax controllers with different encoders:
    * https://docs.revrobotics.com/brushless/spark-max/encoders
    * * Through Bore encoder application notes: https://shorturl.at/wm3tF
+   * * Closed-loop control with SparkMax:
+   * https://docs.revrobotics.com/revlib/spark/closed-loop
+   * 
    */
   // TODO: Add methods for controlling the hood (and then implement them).
 }

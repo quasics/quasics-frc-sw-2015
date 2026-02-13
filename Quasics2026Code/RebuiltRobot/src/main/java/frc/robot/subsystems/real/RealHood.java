914a65d412d5829c4f7ea295e5c9a9a6cedd4533
Matt Healy <mjhealy@alum.rit.edu>
2026-02-12 21:22:52 -0500

    [Rebuilt] Added stubbed subsystems, based on interfaces.

---
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/subsystems/real/RealHood.java
@@ -0,0 +1,20 @@
+// Copyright (c) 2026, Quasics Robotics and other contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.real;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.interfaces.IHood;
+
+public class RealHood extends SubsystemBase implements IHood {
+
+  /** Creates a new RealHood. */
+  public RealHood() {
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}

9d34d30c0780b87f8fb6ccfc4eba62d866161333
Matt Healy <mjhealy@alum.rit.edu>
2026-02-13 19:44:11 -0500

    Doc updates.

--- Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/hardware/sensors/TalonEncoderWrapper.java
+++ Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/hardware/sensors/TalonEncoderWrapper.java
@@ -16,9 +16,27 @@ import edu.wpi.first.units.measure.LinearVelocity;
 /**
  * Convenience wrapper, allowing a TalonFX to be read in the same (general)
  * way as a normal WPLib Encoder.
+ * 
+ * Note: Phoenix 6 moved away from "native units" (ticks) coming back from the
+ * encoder, and uses "rotations" as the standard unit for everything it does.
+ * CTRE's philosophy now is that the scaling should happen in your Java code,
+ * with the intention that this will prevent "magic numbers" from being hidden
+ * inside the motor controller's memory/configuration, making your code much
+ * easier to debug.
+ * 
+ * However, this means that the TalonFX's "position" and "velocity" values are
+ * now in "rotations" and "rotations per second", respectively, and thus need to
+ * be converted to linear distance and velocity using the wheel circumference,
+ * which this wrapper will do.
  */
 public class TalonEncoderWrapper implements TrivialEncoder {
+  /** Wrapped TalonFX motor controller, providing access to encoder data. */
   final TalonFX m_motorController;
+
+  /**
+   * Distance travelled per turn of the outer wheel. (Used to convert
+   * "revolutions" to linear distance, etc.)
+   */
   final Distance m_outerCircumference;
 
   /**
@@ -35,10 +53,9 @@ public class TalonEncoderWrapper implements TrivialEncoder {
     m_outerCircumference = outerDiameter.times(Math.PI);
   }
 
-  @Override
-  public void close() throws IOException {
-    // No-op: close the motor controller, which will also close the encoder.
-  }
+  //
+  // TrivialEncoder implementation
+  //
 
   @Override
   public Distance getPosition() {
@@ -55,5 +72,14 @@ public class TalonEncoderWrapper implements TrivialEncoder {
   @Override
   public void reset() {
     m_motorController.setPosition(0);
+  }
+
+  //
+  // Closeable implementation
+  //
+
+  @Override
+  public void close() throws IOException {
+    // No-op: close the motor controller, which will also close the encoder.
   }
 }

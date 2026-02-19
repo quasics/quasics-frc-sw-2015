d7c6ab51b153bdac7c8fe955d1993320321d8d5e
Matt Healy <mjhealy@alum.rit.edu>
2026-02-18 19:19:59 -0500

    [SimSupport] Doc updates.

--- Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/subsystems/interfaces/IFlywheel.java
+++ Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/subsystems/interfaces/IFlywheel.java
@@ -1,12 +1,26 @@
+// Copyright (c) 2026, Matthew J. Healy and other Quasics contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems.interfaces;
 
+/**
+ * An interface for a flywheel subsystem. This is meant to be a simple
+ * abstraction that can be implemented by different types of flywheel
+ * subsystems (e.g., SparkMax, TalonFX, etc.) without exposing the underlying
+ * hardware details.
+ */
 public interface IFlywheel {
+  /** Sets the target RPM for the flywheel. */
   public void setRPM(double targetRPM);
 
+  /** Returns the current RPM of the flywheel. */
   public double getCurrentRPM();
 
+  /** Returns the setpoint RPM of the flywheel. */
   public double getSetpointRPM();
 
+  /** Stops the flywheel by setting the target RPM to 0. */
   default void stop() {
     setRPM(0);
   }

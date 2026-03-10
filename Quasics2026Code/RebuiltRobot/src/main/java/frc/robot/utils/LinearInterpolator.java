6639991f07c5bf1c6be336ef698386cc7b40d657
Matt Healy <mjhealy@alum.rit.edu>
2026-03-10 12:39:09 -0400

    [Rebuilt] Carrying in changes to LinearInterpolator.

--- Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/utils/LinearInterpolator.java
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/utils/LinearInterpolator.java
@@ -4,18 +4,51 @@
 
 package frc.robot.utils;
 
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.RPM;
+
 import java.util.TreeMap;
 
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Distance;
+
 /**
- * Simple class for handling linear interpolation (e.g., calculating estimated
- * shooter velocity, given a range of "good values" at known distances).
+ * Simple class for handling linear interpolation.
+ * 
+ * You can think of this as implementing some sort of an algebraic function
+ * ("f(x) -> y"), where one of these objects can be given a (relatively)
+ * arbitrary "x" (key), and estimate the corresponding "y".
+ * 
+ * One obvious application is to try to estimate shooter speeds required to hit
+ * a target from different distances.
+ * 
+ * Note that this is *linear* interpolation, and thus it's assumed that only
+ * *one* value is being varied. (For the shooter speed example, this would be
+ * the distance, and we'd assume that other things like the angle of a hood on
+ * the shooter, etc., were not being changed.)
  */
 public class LinearInterpolator {
   private final TreeMap<Double, Double> m_dataLookupTable = new TreeMap<>();
 
+  /**
+   * Constructor.
+   * 
+   * Note that data points will need to be added to the class before we can
+   * generate estimates.
+   * 
+   * @see #addDataPoint(double, double)
+   */
   public LinearInterpolator() {
   }
 
+  /**
+   * Adds a data point for use in estimation. (If you think of this class as
+   * providing a function that maps "x" keys to "y" values, this adds a point on
+   * the curve.)
+   * 
+   * @param key   a new known input ("x") value for the function
+   * @param value a new known output ("y") value for the function
+   */
   public void addDataPoint(double key, double value) {
     m_dataLookupTable.put(key, value);
   }
@@ -24,6 +57,15 @@ public class LinearInterpolator {
     return m_dataLookupTable.toString();
   }
 
+  /**
+   * Given a key (e.g., distance), generates an estimate of the corresponding
+   * value (e.g., shooter speed) associated with it.
+   * 
+   * @param key value for which a corresponding estimate should be generated
+   * @return an estimated value for the key
+   * @throws IllegalStateException when we don't have any data points to use for
+   *                               estimation
+   */
   public double getTargetApproximationForKey(double key) {
     if (m_dataLookupTable.isEmpty()) {
       throw new IllegalStateException("No data to use in performing interpolation");
@@ -50,5 +92,55 @@ public class LinearInterpolator {
     final double ceilValue = ceilPair.getValue();
     double speed = floorValue + (ceilValue - floorValue) * (key - floorKey) / (ceilKey - floorKey);
     return speed;
+  }
+
+  /**
+   * Sample (though useful) class, demonstrating how the LinearInterpolator class
+   * can be used in a "unitized" (unit-safe) form.
+   */
+  static public final class DistanceToShooterSpeedInterpolator {
+    /**
+     * Linear interpolation object we'll use.
+     * 
+     * Note that this will internally map distances to inches and speeds to RPMs, in
+     * order to ensure data consistency. (This also means that we can *accept*
+     * distances in any unit that the client code has handy. and they'll be able to
+     * safely convert results into whatever is *needed*.)
+     */
+    private LinearInterpolator m_interpolator = new LinearInterpolator();
+
+    /**
+     * Constructor.
+     * 
+     * Note that data points will need to be added to the class before we can
+     * generate estimates.
+     * 
+     * @see #addDataPoint(Distance, AngularVelocity)
+     */
+    public DistanceToShooterSpeedInterpolator() {
+    }
+
+    /**
+     * Adds a data point for use in estimating needed speeds.
+     * 
+     * @param distance distance to the target
+     * @param speed    (known) speed required at that distance
+     */
+    public void addDataPoint(Distance distance, AngularVelocity speed) {
+      m_interpolator.addDataPoint(distance.in(Inches), speed.in(RPM));
+    }
+
+    /**
+     * Returns an estimate of the speed needed to hit the target from the specified
+     * distance.
+     * 
+     * @param distance current distance from the target
+     * @return estimated speed for the shooter, in order to hit the target
+     * @throws IllegalStateException when we don't have any data points to use for
+     *                               estimation
+     */
+    public AngularVelocity getEstimatedShooterSpeed(Distance distance) {
+      return RPM.of(m_interpolator.getTargetApproximationForKey(distance.in(Inches)));
+    }
   }
 }

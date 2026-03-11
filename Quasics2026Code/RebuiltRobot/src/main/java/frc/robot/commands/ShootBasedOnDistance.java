e891ea84a353bfeb928288e909d506e409dddeb5
Matt Healy <mjhealy@alum.rit.edu>
2026-03-10 21:59:01 -0400

    [Rebuilt] Minor cleanup + code review feedback.

--- Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/commands/ShootBasedOnDistance.java
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/commands/ShootBasedOnDistance.java
@@ -1,8 +1,11 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) 2026, Quasics Robotics and other contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
+
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.RPM;
 
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj.Timer;
@@ -12,17 +15,14 @@ import frc.robot.subsystems.interfaces.IDrivebase;
 import frc.robot.subsystems.interfaces.IShooter;
 import frc.robot.utils.ShooterCalculator;
 
-/* You should consider using the more terse Command factories API instead
- * https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands
- */
 public class ShootBasedOnDistance extends Command {
-  IShooter m_shooter;
-  private AngularVelocity m_velocityRPM;
-  private double m_kickSpeed;
-  private double m_kickerDelay;
-  Timer m_timer;
-  ShooterCalculator m_calculator;
-  IDrivebase m_drivebase;
+  private final IShooter m_shooter;
+  private final IDrivebase m_drivebase;
+
+  private final double m_kickSpeed;
+  private final double m_kickerDelay;
+  private final Timer m_timer = new Timer();;
+  private final ShooterCalculator m_calculator = new ShooterCalculator();
 
   /** Creates a new RunShooter. */
   public ShootBasedOnDistance(IShooter shooter, IDrivebase drivebase, double kickSpeed, double kickerDelay) {
@@ -30,30 +30,34 @@ public class ShootBasedOnDistance extends Command {
     m_drivebase = drivebase;
     m_kickSpeed = kickSpeed;
     m_kickerDelay = kickerDelay;
-    m_timer = new Timer();
-    m_timer.start();
-    m_calculator = new ShooterCalculator();
-    m_calculator.addDataPoint(70, 2700);
-    m_calculator.addDataPoint(105, 3050); // half from tower to hub ish
-    m_calculator.addDataPoint(140, 3300); // trench shot
-    m_calculator.addDataPoint(160, 3700); // from tower/back wall
-    // Use addRequirements() here to declare subsystem dependencies.
+
+    // Load our spot measurements into the shot calculator.
+    m_calculator.addDataPoint(Inches.of(70), RPM.of(2700));
+    m_calculator.addDataPoint(Inches.of(105), RPM.of(3050)); // half from tower to hub ish
+    m_calculator.addDataPoint(Inches.of(140), RPM.of(3300)); // trench shot
+    m_calculator.addDataPoint(Inches.of(160), RPM.of(3700)); // from tower/back wall
+
+    // FINDME(Rylie): Note that you can actually get away without declaring that you
+    // use the drivebase, since you're only treating it as a read-only source of
+    // data (i.e., pose estimates), and not trying to tell it to *do* anything. It's
+    // not *wrong* to list it here, but it means that you'd be unable to have code
+    // that can shoot *while moving*.
     addRequirements((Subsystem) shooter, (Subsystem) drivebase);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_velocityRPM = m_calculator.getSpeedToHitHubCenter(m_drivebase.getEstimatedPose());
-    m_shooter.setFlywheelRPM(m_velocityRPM);
-    m_timer.reset();
+    final AngularVelocity velocityRPM = m_calculator.getSpeedToHitHubCenter(m_drivebase.getEstimatedPose());
+    m_shooter.setFlywheelRPM(velocityRPM);
+    m_timer.restart();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_velocityRPM = m_calculator.getSpeedToHitHubCenter(m_drivebase.getEstimatedPose());
-    m_shooter.setFlywheelRPM(m_velocityRPM);
+    final AngularVelocity velocityRPM = m_calculator.getSpeedToHitHubCenter(m_drivebase.getEstimatedPose());
+    m_shooter.setFlywheelRPM(velocityRPM);
     if (m_timer.hasElapsed(m_kickerDelay)) {
       m_shooter.setKickerSpeed(m_kickSpeed);
     }

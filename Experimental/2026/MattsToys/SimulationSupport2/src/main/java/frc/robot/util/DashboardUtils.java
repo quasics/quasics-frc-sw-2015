ab281e1990033628a5fc328b6e8bb8ac7fb1c43a
Matt Healy <mjhealy@alum.rit.edu>
2026-03-14 10:37:18 -0400

    [SimSupport] Doc updates.

--- Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/util/DashboardUtils.java
+++ Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/util/DashboardUtils.java
@@ -7,15 +7,37 @@ import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+/**
+ * Some utility functions for working with the SmartDashboard/Shuffleboard,
+ * including tab management.
+ */
 public class DashboardUtils {
   public static boolean USE_SUB_TABS = true;
   public static boolean USE_SUB_TAB_FOR_DRIVE_TEAM = false;
 
   private static Set<String> m_addedEntities = new HashSet<String>();
 
+  private static String getLookupLabel(String tabName, String label) {
+    return tabName + "____" + label;
+  }
+
+  /**
+   * Helper function, providing some filtering.
+   * 
+   * Unlike with the SmartDashboard.put() call, you can only add an object with a
+   * given name to a tab *once*; trying to do so again (even with the same object)
+   * will cause an exception to be thrown. We could catch this, but that's
+   * potentially expensive, so we'll instead just maintain a list that we can
+   * cross-check.
+   * 
+   * @param tabName
+   * @param label
+   * @param object
+   */
   private static void addToTab(String tabName, String label, Sendable object) {
-    String lookup = tabName + "____" + label;
+    final String lookup = getLookupLabel(tabName, label);
     if (m_addedEntities.contains(lookup)) {
+      // Tab already contains something with this name: not gonna add it again.
       return;
     }
 
@@ -25,8 +47,8 @@ public class DashboardUtils {
   }
 
   public static boolean isPublished(String tabName, String label) {
-    String lookup = tabName + "____" + label;
-    return m_addedEntities.contains(label);
+    final String lookup = getLookupLabel(tabName, label);
+    return m_addedEntities.contains(lookup);
   }
 
   public static void publishForDriveTeam(String label, Sendable object) {

639fec3e98e8531259a3e750046afa73d0dc00f5
Matt Healy <mjhealy@alum.rit.edu>
2026-03-14 11:02:22 -0400

    [Rebuilt] Updating AlignToHub to allow PID values to be adjusted (real vs sim).

--- Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/commands/AlignToHub.java
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/commands/AlignToHub.java
@@ -14,6 +14,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Robot;
 import frc.robot.logging.Logger;
 import frc.robot.subsystems.interfaces.IDrivebase;
 import frc.robot.utils.TargetPositioningUtils;
@@ -31,7 +32,14 @@ public class AlignToHub extends Command {
   /** Creates a new AlignToHub. */
   public AlignToHub(IDrivebase drivebase) {
     m_drivebase = drivebase;
-    m_pid = new PIDController(0.004, 0.002, 0);
+    if (Robot.isSimulation()) {
+      m_pid = new PIDController(0.004, 0.002, 0);
+    } else {
+      // TODO: Add tuning values for Lizzie, based on its real behavior. (This can
+      // also be done by performing characterization of angular movement, and feeding
+      // the data into SysId.)
+      m_pid = new PIDController(0.004, 0.0, 0);
+    }
     m_pid.enableContinuousInput(-180, 180);
 
     m_supplier = () -> {

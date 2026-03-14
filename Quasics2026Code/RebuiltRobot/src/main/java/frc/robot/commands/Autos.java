5049548e6308ec9d145bf7bebb16e838a1df16bf
Quasics FRC <quasics_driverStation@mersgroup.com>
2026-03-14 10:33:49 -0400

    Climb for Time

--- Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/commands/Autos.java
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/commands/Autos.java
@@ -7,9 +7,12 @@ package frc.robot.commands;
 import static edu.wpi.first.units.Units.Feet;
 import static edu.wpi.first.units.Units.Seconds;
 
+import com.fasterxml.jackson.databind.introspect.ClassIntrospector;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.PrintCommand;

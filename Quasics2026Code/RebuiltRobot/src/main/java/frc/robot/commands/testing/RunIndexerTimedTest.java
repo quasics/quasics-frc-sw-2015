f23c4e7d54cb05154f53b271e9cf9dad4b59aaa3
Matt Healy <mjhealy@alum.rit.edu>
2026-03-21 09:54:26 -0400

    [Rebuilt] Doc updates.

--- Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/commands/testing/RunIndexerTimedTest.java
+++ Quasics2026Code/RebuiltRobot/src/main/java/frc/robot/commands/testing/RunIndexerTimedTest.java
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) 2026, Quasics Robotics and other contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 

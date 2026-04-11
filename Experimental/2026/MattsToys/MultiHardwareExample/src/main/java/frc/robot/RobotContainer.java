7be8425dd2017c7d940f9a6ace68dbdb385108b1
Matt Healy <mjhealy@alum.rit.edu>
2026-04-11 13:17:48 -0400

    [MultiHW] Refactor: moved static SMT allocation into separate (demo) class.

--- Experimental/2026/MattsToys/MultiHardwareExample/src/main/java/frc/robot/RobotContainer.java
+++ Experimental/2026/MattsToys/MultiHardwareExample/src/main/java/frc/robot/RobotContainer.java
@@ -1,40 +1,26 @@
-// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) 2026, Matthew J. Healy and other Quasics contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Inches;
-
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import frc.robot.commands.VariableSpeedCommand;
-import frc.robot.hardware.actuators.IMotorControllerPlus;
-import frc.robot.hardware.actuators.TalonMotorControllerPlus;
-import frc.robot.hardware.sensors.TalonEncoderWrapper;
-import frc.robot.hardware.sensors.TrivialEncoder;
-import frc.robot.subsystems.implementation.SingleMotorThing;
-import frc.robot.subsystems.implementation.SingleMotorThingGroup;
 import frc.robot.subsystems.interfaces.ISingleMotorThing;
 import frc.robot.subsystems.real.SingleMotorThingNova;
 import frc.robot.subsystems.real.SingleMotorThingSpark;
 import frc.robot.subsystems.real.SingleMotorThingTalonPwm;
-import frc.robot.subsystems.simulation.SingleMotorThingSim;
 import frc.robot.util.DashboardUtils;
 
 /**
@@ -98,66 +84,6 @@ public class RobotContainer {
     };
   }
 
-  /** Supported (fixed) hardware configurations. */
-  enum HardwareConfig {
-    /** Simulated motor only. */
-    Simulated,
-    /** SparkMax motor controller (live). */
-    Spark,
-    /** Paired SparkMax motor controllers (live). */
-    SparkPair,
-    /** TalonFX motor controller (live). */
-    TalonPwm,
-    /**
-     * TalonFX motor controller, constructed directly from the basic
-     * SingleMotorThing class (live).
-     */
-    TalonCanDirect,
-    /** Thrifty Nova motor controller (live). */
-    Thrifty,
-    /** Victor motor controller (live). */
-    Victor,
-  }
-
-  /**
-   * Demonstrates how to set up a "single motor thing" in a number of ways, based
-   * on the selected hardware configuration.
-   * 
-   * @param HardwareConfig the (fixed/template) hardware configuration to use
-   */
-  static ISingleMotorThing allocateFixedSingleMotorThing(HardwareConfig config) {
-    return switch (config) {
-      case Simulated -> new SingleMotorThingSim();
-      case Spark -> new SingleMotorThingSpark(5, true);
-      case SparkPair -> {
-        // Generate an example of a pair of motors being treated as a single
-        // (logical) entity (e.g., paired motors responsible for
-        // extending/retracting an intake, mounted in opposite directions from
-        // each other).
-        yield new SingleMotorThingGroup(
-            new SingleMotorThingSpark(RIGHT_INTAKE_DEPLOYMENT_ID, true),
-            new SingleMotorThingSpark(LEFT_INTAKE_DEPLOYMENT_ID, false));
-      }
-      case TalonPwm -> new SingleMotorThingTalonPwm(6);
-      case TalonCanDirect -> {
-        final TalonFX talon = new TalonFX(11);
-        final IMotorControllerPlus motorController = new TalonMotorControllerPlus(talon);
-        final TrivialEncoder encoder = new TalonEncoderWrapper(talon, Inches.of(6));
-        yield new SingleMotorThing(
-            new SingleMotorThing.ConstructionData(motorController, encoder));
-      }
-      case Thrifty -> new SingleMotorThingNova(5);
-      case Victor ->
-        // Sample of how to use the SingleMotorThing class without needing
-        // to derive a class for hardware-specific setup.
-        new SingleMotorThing(new SingleMotorThing.ConstructionData(
-            // Use a Victor motor controller...
-            new VictorSP(8),
-            // ...and a bog-standard WPILib encoder.
-            TrivialEncoder.forWpiLibEncoder(new Encoder(1, 2))));
-    };
-  }
-
   private static final int DEFAULT_MOTOR_ID = 1;
   private static final MotorType DEFAULT_MOTOR_TYPE = MotorType.SparkMax;
   private static final boolean DEFAULT_MOTOR_INVERTED = false;
@@ -165,14 +91,6 @@ public class RobotContainer {
   private static final MotorType SELECTED_MOTOR_TYPE = getMotorTypeFromPreferences(DEFAULT_MOTOR_TYPE);
   private static final int SELECTED_MOTOR_ID = getMotorIdFromPreferences(DEFAULT_MOTOR_ID);
   private static final boolean SELECTED_MOTOR_INVERTED = getInvertedFromPreferences(DEFAULT_MOTOR_INVERTED);
-
-  public static final int RIGHT_INTAKE_DEPLOYMENT_ID = 6;
-  public static final int LEFT_INTAKE_DEPLOYMENT_ID = 7;
-
-  /** Selected hardware configuration. */
-  final HardwareConfig m_hardware = Robot.isSimulation()
-      ? HardwareConfig.Simulated
-      : HardwareConfig.TalonCanDirect;
 
   // Sets up a "single motor thing", based on the selected hardware
   // configuration.
@@ -183,8 +101,6 @@ public class RobotContainer {
 
   /** Constructor. */
   public RobotContainer() {
-    System.out.println("Hardware configuration: " + m_hardware);
-
     Shuffleboard.getTab(VariableSpeedCommand.TAB_NAME)
         .add("Variable Speed", new VariableSpeedCommand(m_singleMotorThing))
         .withWidget(BuiltInWidgets.kCommand);

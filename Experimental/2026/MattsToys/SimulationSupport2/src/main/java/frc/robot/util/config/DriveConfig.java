3b64ce0ba57c46ffc53944cbd7b2e54df49028f7
Matt Healy <mjhealy@alum.rit.edu>
2026-04-23 19:25:01 -0400

    [SimSupport] Starting to add support for motor plan definitions in DriveConfig.

--- Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/util/config/DriveConfig.java
+++ Experimental/2026/MattsToys/SimulationSupport2/src/main/java/frc/robot/util/config/DriveConfig.java
@@ -1,6 +1,10 @@
 package frc.robot.util.config;
 
+import java.util.Map;
+
 import edu.wpi.first.units.measure.Distance;
+import frc.robot.constants.robots.QuasicsSparkMaxConstants;
+import frc.robot.constants.robots.QuasicsThriftyNovaConstants;
 
 /**
  * Drive base configuration data.
@@ -13,13 +17,57 @@ import edu.wpi.first.units.measure.Distance;
  * @param leftPid     PID configuration for the drivebase's left motors
  * @param rightPid    PID configuration for the drivebase's right motors
  * @param feedForward feedforward configuration for the drivebase
+ * @param motorIdMap  mapping of drive motors to their corresponding IDs (CAN,
+ *                    PWM, etc.)
  */
 public record DriveConfig(DriveType driveType, Distance wheelRadius,
     Distance trackWidth, double gearing, DriveOrientation orientation,
-    PIDConfig leftPid, PIDConfig rightPid, DriveFeedForwardConfig feedForward) {
+    PIDConfig leftPid, PIDConfig rightPid, DriveFeedForwardConfig feedForward, Map<MotorUnit, Integer> motorIdMap) {
+  public enum MotorUnit {
+    LeftLeader, RightLeader, LeftFollower, RightFollower
+  }
+
+  private static final Map<MotorUnit, Integer> DEFAULT_NOVA_MOTOR_ID_MAP = Map.of(
+      MotorUnit.LeftLeader, QuasicsThriftyNovaConstants.QuasicsDrivebaseCanIds.LEFT_LEADER_ID,
+      MotorUnit.RightLeader, QuasicsThriftyNovaConstants.QuasicsDrivebaseCanIds.RIGHT_LEADER_ID,
+      MotorUnit.LeftFollower, QuasicsThriftyNovaConstants.QuasicsDrivebaseCanIds.LEFT_FOLLOWER_ID,
+      MotorUnit.RightFollower, QuasicsThriftyNovaConstants.QuasicsDrivebaseCanIds.RIGHT_FOLLOWER_ID);
+
+  private static final Map<MotorUnit, Integer> DEFAULT_SPARK_MOTOR_ID_MAP = Map.of(
+      MotorUnit.LeftLeader, QuasicsSparkMaxConstants.QuasicsDrivebaseCanIds.LEFT_LEADER_ID,
+      MotorUnit.RightLeader, QuasicsSparkMaxConstants.QuasicsDrivebaseCanIds.RIGHT_LEADER_ID,
+      MotorUnit.LeftFollower, QuasicsSparkMaxConstants.QuasicsDrivebaseCanIds.LEFT_FOLLOWER_ID,
+      MotorUnit.RightFollower, QuasicsSparkMaxConstants.QuasicsDrivebaseCanIds.RIGHT_FOLLOWER_ID);
+
+  static Map<MotorUnit, Integer> getDefaultMotorIdMap(DriveType driveType) {
+    return switch (driveType) {
+      case Simulated -> null;
+      case CanSparkMax -> DEFAULT_SPARK_MOTOR_ID_MAP;
+      case ThriftyNova -> DEFAULT_NOVA_MOTOR_ID_MAP;
+    };
+  }
+
+  /**
+   * Convenience constructor, using a default (canonical) motor ID mapping.
+   *
+   * @param driveType   drive type (simulated, CAN-based SparkMax)
+   * @param wheelRadius radius of the drive base wheels
+   * @param trackWidth  maximum width between drive base wheels
+   * @param gearing     gearing between motor and wheel axel (>=1)
+   * @param orientation orientation of the drivebase's motors
+   * @param commonPid   shared PID configuration for the drivebase
+   * @param feedForward feedforward configuration for the drivebase
+   */
+  public DriveConfig(DriveType driveType, Distance wheelRadius,
+      Distance trackWidth, double gearing, DriveOrientation orientation,
+      PIDConfig leftPid, PIDConfig rightPid, DriveFeedForwardConfig feedForward) {
+    this(driveType, wheelRadius, trackWidth, gearing, orientation, leftPid,
+        rightPid, feedForward, getDefaultMotorIdMap(driveType));
+  }
+
   /**
    * Convenience constructor, using a single set of PID values for both left
-   * and right.
+   * and right, and a default (canonical) motor ID mapping.
    *
    * @param driveType   drive type (simulated, CAN-based SparkMax)
    * @param wheelRadius radius of the drive base wheels
@@ -33,6 +81,6 @@ public record DriveConfig(DriveType driveType, Distance wheelRadius,
       Distance trackWidth, double gearing, DriveOrientation orientation,
       PIDConfig commonPid, DriveFeedForwardConfig feedForward) {
     this(driveType, wheelRadius, trackWidth, gearing, orientation, commonPid,
-        commonPid, feedForward);
+        commonPid, feedForward, getDefaultMotorIdMap(driveType));
   }
 }

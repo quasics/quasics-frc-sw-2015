be1469fe7affe5e698be831d6734f49ee1d0b1fd
Matt Healy <mjhealy@alum.rit.edu>
2026-04-30 19:57:32 -0400

    [TrainingBot] Starting to frame Nova drivebase.

---
+++ Experimental/2026/TrainingBot_Base/src/main/java/frc/robot/subsystems/RealNovaDrivebase.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+public class RealNovaDrivebase extends AbstractDrivebase {
+  public RealNovaDrivebase() {
+  }
+
+  @Override
+  public void tankDrive(double leftPercentage, double rightPercentage) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'tankDrive'");
+  }
+
+  @Override
+  public double getLeftDistanceMeters() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getLeftDistanceMeters'");
+  }
+
+  @Override
+  public double getRightDistanceMeters() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getRightDistanceMeters'");
+  }
+
+  @Override
+  public double getHeadingInDegrees() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getHeadingInDegrees'");
+  }
+
+}
